fc19df9f6b14702cfe624abdb576f9f9a552a1ea
Serge Rider <serge@jkiss.org>
2020-04-16 14:30:26 +0000

    Server start script fix (linux)

--- deploy/scripts/run-server.sh
+++ deploy/scripts/run-server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-cd server
-launcherJar=( plugins/org.eclipse.equinox.launcher*.jar )
+launcherJar=( server/plugins/org.eclipse.equinox.launcher*.jar )
 
 echo "Starting Cloudbeaver Server"
-export CLOUDBEAVER_HOME=$(pwd)
-
-java -jar ${launcherJar} -product io.cloudbeaver.server.product -web-config ./../conf/cloudbeaver.conf -nl en -registryMultiLanguage -vmargs -Xmx2048M
+java -jar ${launcherJar} -product io.cloudbeaver.server.product -web-config conf/cloudbeaver.conf -nl en -registryMultiLanguage -vmargs -Xmx2048M
