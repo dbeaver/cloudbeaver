0039df8a97879a3f50fe10e322398c9ecf2696d2
Wroud <wrouds@gmail.com>
2020-05-13 18:34:44 +0300

    feat(core): add usePermission hook

--- webapp/packages/core/src/app/NavigationTree/NavigationTree.tsx
+++ webapp/packages/core/src/app/NavigationTree/NavigationTree.tsx
@@ -9,6 +9,9 @@
 import { observer } from 'mobx-react';
 import styled, { css } from 'reshadow';
 
+import { usePermission } from '@dbeaver/core/root';
+
+import { PUBLIC_PERMISSION } from '../shared/NodesManager/NodesManagerService';
 import { useChildren } from '../shared/useChildren';
 import { NavigationTreeNode } from './NavigationTreeNode/NavigationTreeNode';
 
@@ -38,6 +41,11 @@ const navigationTreeStyles = css`
 
 export const NavigationTree = observer(function NavigationTree() {
   const nodeChildren = useChildren();
+  const isEnabled = usePermission(PUBLIC_PERMISSION);
+
+  if (!isEnabled) {
+    return null;
+  }
 
   if (!nodeChildren || nodeChildren.children.length === 0) {
     return styled(navigationTreeStyles)(
--- webapp/packages/core/src/root/index.ts
+++ webapp/packages/core/src/root/index.ts
@@ -3,3 +3,4 @@ export * from './ServerService';
 export * from './PermissionsService';
 export * from './ServerSettingsService';
 export * from './SessionSettingsService';
+export * from './usePermission';
