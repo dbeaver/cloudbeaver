45329334f3b0f33c1b21a1e954f12f8516ce42bf
Wroud <wrouds@gmail.com>
2020-07-16 19:47:07 +0300

    chore: plugins assets extraction
    CB-211

--- webapp/configs/webpack.plugin.config.js
+++ webapp/configs/webpack.plugin.config.js
@@ -6,6 +6,7 @@ const commonConfig = require('./webpack.config.js');
 var main = resolve('src/index.ts');
 var outputDir = resolve('lib');
 var package = require(resolve("package.json"));
+
 module.exports = (env, argv) => merge(commonConfig(env, argv), {
   entry: main,
   output: {
--- webapp/configs/webpack.product.config.js
+++ webapp/configs/webpack.product.config.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
-const { resolve } = require('path');
+const fs = require('fs');
+const { resolve, join } = require('path');
 const commonConfig = require('./webpack.config.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -8,6 +9,44 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 var main = resolve('src/index.ts');
 var outputDir = resolve('lib');
 var package = require(resolve("package.json"))
+
+function getCloudbeaverDeps(package) {
+    if (!package.dependencies) {
+        return [];
+    }
+
+    return Object.keys(package.dependencies)
+        .filter(dependency => /@cloudbeaver\/(.*?)/.test(dependency))
+}
+
+function scanCloudbeaverDeps() {
+    const deps = new Set();
+    const list = getCloudbeaverDeps(package);
+
+    while (list.length) {
+        const dependency = list.shift();
+
+        if (!deps.has(dependency)) {
+            list.push(...getCloudbeaverDeps(require(join(__dirname, '../node_modules', dependency, 'package.json'))))
+        }
+
+        deps.add(dependency);
+    }
+
+    return Array.from(deps.keys())
+}
+
+function getAssets(to) {
+    const patterns = scanCloudbeaverDeps()
+        .map(dependency => ({ from: join(__dirname, '../node_modules', dependency, 'public'), to, force: true }))
+        .reverse();
+
+    patterns.push({ from: './public', to, force: true });
+
+    return patterns.filter(pattern => fs.existsSync(pattern.from))
+}
+
+scanCloudbeaverDeps()
 
 module.exports = (env, argv) => merge(commonConfig(env, argv), {
     entry: main,
@@ -30,9 +69,7 @@ module.exports = (env, argv) => merge(commonConfig(env, argv), {
     },
     plugins: [
         new CopyWebpackPlugin({
-            patterns: [
-                { from: './public', to: '', force: true },
-            ]
+            patterns: getAssets('')
         }),
         new webpack.DefinePlugin({
             version: JSON.stringify(package.buildVersion),
