ca19142bd3d1e0a4de0c0257562b9e08860120c4
Wroud <wrouds@gmail.com>
2020-08-07 19:07:23 +0300

    fix(core-app): logo navigation

--- webapp/packages/core-app/src/TopNavBar/Logo.tsx
+++ webapp/packages/core-app/src/TopNavBar/Logo.tsx
@@ -11,16 +11,15 @@ import { observer } from 'mobx-react';
 import { AppLogo } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
 import { ServerService } from '@cloudbeaver/core-root';
-
-import { AppScreenService } from '../AppScreen/AppScreenService';
+import { ScreenService } from '@cloudbeaver/core-routing';
 
 declare const version: string; // declared in webpack DefinePlugin // todo move to enviroment?
 
 export const Logo = observer(function Logo() {
   const serverService = useService(ServerService);
-  const appScreenService = useService(AppScreenService);
+  const screenService = useService(ScreenService);
 
   const title = `Frontend: ${version}\nBackend: ${serverService.config.data?.version}`;
 
-  return <AppLogo title={title} onClick={() => appScreenService.navigateToRoot()}/>;
+  return <AppLogo title={title} onClick={() => screenService.navigateToRoot()}/>;
 });
