d382d74d449949a281635c00ce66cce27e5f9ff7
Wroud <wrouds@gmail.com>
2020-09-10 22:13:55 +0300

    chore: formatting

--- webapp/packages/product-default/src/index.ts
+++ webapp/packages/product-default/src/index.ts
@@ -1,17 +1,16 @@
-/* eslint-disable import-helpers/order-imports, import/order, import/no-duplicates */
-/* this eslint-disable required for webpack inject */
 import 'reflect-metadata';
 
 import core, { bootstrap } from '@cloudbeaver/core-bootstrap';
-import spreadsheet from '@cloudbeaver/plugin-data-spreadsheet';
 import authentication from '@cloudbeaver/plugin-authentication';
+import connectionCustom from '@cloudbeaver/plugin-connection-custom';
+import connectionTemplate from '@cloudbeaver/plugin-connection-template';
 import dataExport from '@cloudbeaver/plugin-data-export';
+import spreadsheet from '@cloudbeaver/plugin-data-spreadsheet';
 import dataViewer from '@cloudbeaver/plugin-data-viewer';
 import ddlViewer from '@cloudbeaver/plugin-ddl-viewer';
 import objectViewer from '@cloudbeaver/plugin-object-viewer';
 import sqlEditor from '@cloudbeaver/plugin-sql-editor';
-import connectionCustom from '@cloudbeaver/plugin-connection-custom';
-import connectionTemplate from '@cloudbeaver/plugin-connection-template';
+
 import { defaultProductManifest } from './manifest';
 
 const PLUGINS = [
