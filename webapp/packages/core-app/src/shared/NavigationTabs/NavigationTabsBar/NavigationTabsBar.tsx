c68daac931ec79cde55a09a0941a8af00dffc1b6
Naumov Alexey <Iamemptyhuh@gmail.com>
2020-09-29 19:26:11 +0300

    fix(core-localization): changed placeholder text(ru)
    CB-227

--- webapp/packages/core-app/src/shared/NavigationTabs/NavigationTabsBar/NavigationTabsBar.tsx
+++ webapp/packages/core-app/src/shared/NavigationTabs/NavigationTabsBar/NavigationTabsBar.tsx
@@ -10,17 +10,15 @@ import { observer } from 'mobx-react';
 import { useCallback } from 'react';
 import styled, { css } from 'reshadow';
 
-import {
-  TextPlaceholder, TabsBox, TabPanel, useFocus
-} from '@cloudbeaver/core-blocks';
+import { TextPlaceholder, TabsBox, TabPanel, useFocus } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
+import { useTranslate } from '@cloudbeaver/core-localization';
 import { useStyles, composes } from '@cloudbeaver/core-theming';
 import { useActiveView } from '@cloudbeaver/core-view';
 
 import { NavigationTabsService } from '../NavigationTabsService';
 import { TabHandlerPanel } from './Tabs/TabHandlerPanel';
 import { TabHandlerTab } from './Tabs/TabHandlerTab';
-import { useTranslate } from '@cloudbeaver/core-localization';
 
 const styles = composes(
   css`
