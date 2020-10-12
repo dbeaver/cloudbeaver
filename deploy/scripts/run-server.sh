ffcfd1f39bc1283d6de31e417d71b031e6a815aa
Serge Rider <serge@jkiss.org>
2020-10-12 20:32:35 +0300

    CB-404 Server configuration re-layout

--- deploy/scripts/run-server.sh
+++ deploy/scripts/run-server.sh
@@ -3,6 +3,6 @@ launcherJar=( server/plugins/org.eclipse.equinox.launcher*.jar )
 
 echo "Starting Cloudbeaver Server"
 
-[ ! -d "workspace/.metadata" ] && mkdir workspace/GlobalConfiguration/.dbeaver && cp conf/initial-data-sources.conf workspace/GlobalConfiguration/.dbeaver/data-sources.json
+[ ! -d "workspace/.metadata" ] && mkdir -p workspace/GlobalConfiguration/.dbeaver && cp conf/initial-data-sources.conf workspace/GlobalConfiguration/.dbeaver/data-sources.json
 
 java -jar ${launcherJar} -product io.cloudbeaver.product.ce.product -web-config conf/cloudbeaver.conf -nl en -registryMultiLanguage -vmargs -Xmx2048M
