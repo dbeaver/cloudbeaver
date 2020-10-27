f4435a1eb87f7d4078912845f81848e90fd6c447
Wroud <wrouds@gmail.com>
2020-10-27 19:04:10 +0300

    chore(plugin-administration): CB-450 improve localization

--- webapp/packages/plugin-administration/src/locales/ru.ts
+++ webapp/packages/plugin-administration/src/locales/ru.ts
@@ -1,4 +1,5 @@
 export default [
+  ['administration_configuration_wizard_configuration_server_session_lifetime', 'Время сессии'],
   ['administration_configuration_wizard_configuration_custom_connections', 'Разрешить пользовательские подключения'],
   ['administration_configuration_wizard_configuration_custom_connections_description', 'Позволяет пользователям создавать собственные подключения. Иначе, любые подключения могут быть добавлены только со страницы администрирования'],
   ['administration_server_configuration_save_confirmation_title', 'Изменение настроек сервера'],
