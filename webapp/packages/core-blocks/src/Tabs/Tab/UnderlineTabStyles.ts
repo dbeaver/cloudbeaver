49248c0e2a8ec950f42c040a3f7495b1d7a88f7f
Naumov Alexey <Iamemptyhuh@gmail.com>
2020-11-23 20:39:45 +0300

    fix(plugin-aws-administration): CB-488 change styles, add theme support

--- webapp/packages/core-blocks/src/Tabs/Tab/UnderlineTabStyles.ts
+++ webapp/packages/core-blocks/src/Tabs/Tab/UnderlineTabStyles.ts
@@ -8,38 +8,42 @@
 
 import { css } from 'reshadow';
 
-export const UNDERLINE_TAB_STYLES = css`
-  TabList {
-    display: flex;
-    & tab-outer:not(:last-child) {
-      margin-right: 15px;
+import { composes } from '@cloudbeaver/core-theming';
+
+export const UNDERLINE_TAB_STYLES = composes(
+  css`
+    Tab {
+      composes: theme-border-color-primary from global;
     }
-  }
-  Tab {
-    composes: theme-typography--body2 from global;
-    position: relative;
-    background: none;
-    color: inherit;
-    border: none;
-    outline: none;
-    opacity: 0.8;
-    &:hover {
-      cursor: pointer;
-      opacity: 1;
-    }
-    &:global([aria-selected="true"]) {
-      opacity: 1;
-      &:after {
-        content: '';
-        display: block;
-        position: absolute;
-        left: 0;
-        bottom: -5px;
-        width: 100%;
-        height: 2px;
-        border-radius: 2px;
-        background: #3f8ae0;
+  `,
+  css`
+    TabList {
+      display: flex;
+      & tab-outer:not(:last-child) {
+        margin-right: 16px;
       }
     }
-  }
-`;
+    Tab {
+      composes: theme-typography--body2 from global;
+      position: relative;
+      background: none;
+      color: inherit;
+      border: none;
+      border-bottom: 2px solid;
+      outline: none;
+      box-sizing: border-box;
+      opacity: 1;
+      height: 30px !important;
+      padding: 0 5px !important;
+      border-top: none !important;
+      font-weight: normal !important;
+      &:global([aria-selected="false"]) {
+        opacity: 0.8;
+        border-bottom: 2px solid transparent !important;
+      }
+      &:hover {
+        cursor: pointer;
+        opacity: 1;
+      }
+    }
+`);
