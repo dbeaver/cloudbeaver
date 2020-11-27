1f707bf6c9f3eb23d193892450bfc26a1a367f57
Wroud <wrouds@gmail.com>
2020-11-27 16:42:01 +0300

    fix(core-app): CB-559 version displayment

--- webapp/packages/core-app/src/useAppVersion.tsx
+++ webapp/packages/core-app/src/useAppVersion.tsx
@@ -5,15 +5,18 @@ interface IAppVersion {
   backendVersion: string;
   frontendVersion: string;
 }
+
 declare const _VERSION_: string | undefined;
+const VERSION_REGEX = /(\d+\.\d+\.\d+)/;
+
 export function useAppVersion(short = false): IAppVersion {
   const serverService = useService(ServerService);
   let backendVersion = serverService.config.data?.version || '';
   let frontendVersion = _VERSION_ || '';
 
   if (short) {
-    backendVersion = backendVersion.slice(0, 5);
-    frontendVersion = frontendVersion.slice(0, 5);
+    backendVersion = VERSION_REGEX.exec(backendVersion)?.[1] ?? backendVersion;
+    frontendVersion = VERSION_REGEX.exec(frontendVersion)?.[1] ?? frontendVersion;
   }
   return { backendVersion, frontendVersion };
 }
