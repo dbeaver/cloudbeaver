248eb4d8385dd1ee560ce4bb3df27f2985c1be23
Wroud <wrouds@gmail.com>
2021-05-14 15:23:06 +0300

    fix(core-blocks): CB-991 pass div props through containers

---
+++ webapp/packages/core-blocks/src/Containers/filterContainerFakeProps.ts
@@ -0,0 +1,25 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2021 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
+import { filterLayoutFakeProps } from './filterLayoutFakeProps';
+import type { IContainerProps } from './IContainerProps';
+
+export function filterContainerFakeProps<T extends IContainerProps>(props: T): Omit<T, keyof IContainerProps> {
+  const {
+    baseline,
+    center,
+    vertical,
+    wrap,
+    overflow,
+    parent,
+    gap,
+    ...rest
+  } = filterLayoutFakeProps(props);
+
+  return rest as Omit<T, keyof IContainerProps>;
+}
