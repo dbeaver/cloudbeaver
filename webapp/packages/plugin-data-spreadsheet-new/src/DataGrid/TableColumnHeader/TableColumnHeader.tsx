ed963b670028bde989468081b3a0f1256cfba119
Naumov Alexey <Iamemptyhuh@gmail.com>
2021-05-27 23:54:13 +0300

    feat(plugin-data-spreadsheet-new): CB-764 show readonlyStatus if presented

--- webapp/packages/plugin-data-spreadsheet-new/src/DataGrid/TableColumnHeader/TableColumnHeader.tsx
+++ webapp/packages/plugin-data-spreadsheet-new/src/DataGrid/TableColumnHeader/TableColumnHeader.tsx
@@ -84,7 +84,7 @@ export const TableColumnHeader: React.FC<HeaderRendererProps<any>> = observer(fu
   // TODO we want to get "sortable" property from SqlResultColumn data
   const sortable = model.source.results.length === 1;
   const readOnly = !tableDataContext.isReadOnly() && column?.readOnly;
-  const readOnlyStatus = readOnly && column?.readOnlyStatus ? `(Read-only: ${column.readOnlyStatus})` : '';
+  const readOnlyStatus = column?.readOnlyStatus ? `(Read-only: ${column.readOnlyStatus})` : '';
   const columnTooltip = `${columnName}${column?.fullTypeName ? ': ' + column.fullTypeName : ''} ${readOnlyStatus}`;
 
   const handleColumnSelection = (e: React.MouseEvent<HTMLDivElement>) => {
