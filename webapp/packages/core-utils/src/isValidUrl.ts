571461e1e545038e57bbbbf40f4a4a5849e329fa
Wroud <wrouds@gmail.com>
2021-07-13 18:46:31 +0300

    fix(core-utils): CB-1119 regexp freez

--- webapp/packages/core-utils/src/isValidUrl.ts
+++ webapp/packages/core-utils/src/isValidUrl.ts
@@ -6,6 +6,7 @@
  * you may not use this file except in compliance with the License.
  */
 
+// test freezes with '5999999999999000000000000000000000000000000'
 const urlPattern = new RegExp('^(https?:\\/\\/)?' // protocol
 + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' // domain name
 + '((\\d{1,3}\\.){3}\\d{1,3}))' // OR ip (v4) address
@@ -14,5 +15,5 @@ const urlPattern = new RegExp('^(https?:\\/\\/)?' // protocol
 + '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
 
 export function isValidUrl(value: string): boolean {
-  return urlPattern.test(value);
+  return value.startsWith('https://') || value.startsWith('http://');
 }
