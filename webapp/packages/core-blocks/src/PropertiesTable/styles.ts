f7fb233caf9caf8c9721c2bade931a12deb29781
Wroud <wrouds@gmail.com>
2021-08-02 12:36:02 +0300

    fix(core-blocks): properties table add button styles

--- webapp/packages/core-blocks/src/PropertiesTable/styles.ts
+++ webapp/packages/core-blocks/src/PropertiesTable/styles.ts
@@ -39,7 +39,7 @@ export const PROPERTIES_TABLE_STYLES = composes(
       width: 300px;
     }
     properties-header-add {
-      flex: 0 0 100%;
+      flex: 0 0 auto;
       padding: 0px 28px;
     }
 
