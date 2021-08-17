6ccba5c07a42e6908c1c25cc511e94b68f00c1aa
Riccardo Messineo <riccardomessineo@gmail.com>
2021-08-17 11:40:37 +0200

    Create it.ts

---
+++ webapp/packages/plugin-connection-template/src/locales/it.ts
@@ -0,0 +1,12 @@
+export default [
+  ['basicConnection_connectionDialog_newConnection', 'Nuova connessione'],
+  ['basicConnection_connectionDialog_title', 'Collegati al database'],
+  ['basicConnection_connectionDialog_listTitle', 'Database:'],
+  ['basicConnection_connectionDialog_username', 'Database Username:'],
+  ['basicConnection_connectionDialog_usernamePlaceholder', 'utente'],
+  ['basicConnection_connectionDialog_password', 'Password Utente del Database:'],
+  ['basicConnection_connectionDialog_passwordPlaceholder', 'password'],
+  ['basicConnection_connectionDialog_connecting', 'Collegamento...'],
+  ['basicConnection_connectionDialog_connecting_message', 'Collegamento al database...'],
+  ['basicConnection_main_menu_item', 'Dal Template'],
+];
