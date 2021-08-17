389ffb5504332aa503519d3edf430c7fea9b98af
Riccardo Messineo <riccardomessineo@gmail.com>
2021-08-17 12:31:54 +0200

    Create it.ts

---
+++ webapp/packages/plugin-sql-editor/src/locales/it.ts
@@ -0,0 +1,16 @@
+export default [
+  ['sql_editor_placeholder', 'Esegui la query con Ctrl+Enter per vedere i risultati'],
+  ['sql_editor_execution_plan_button_tooltip', 'Mostra il piano di esecuzione (Shift + Ctrl + E)'],
+  ['sql_editor_sql_execution_button_tooltip', 'Esegui l\'istruzione SQL (Ctrl + Enter)'],
+  ['sql_editor_sql_execution_new_tab_button_tooltip', 'Esegui l\'istruzione SQL in una nuova tab (Ctrl + \\)(Shift + Ctrl + Enter)'],
+  ['sql_editor_sql_execution_script_button_tooltip', 'Esegui lo script SQL (Alt + X)'],
+  ['sql_editor_sql_statistics_unavailable', 'Statistiche non disponibili'],
+  ['sql_editor_sql_execution_queries', 'Queries:'],
+  ['sql_editor_sql_execution_executing', 'Esecuzione in corso...'],
+  ['sql_execution_plan_placeholder', 'Piano di esecuzione vuoto'],
+  ['sql_execution_plan_properties_panel_placeholder', 'Non ci sono elementi da mostrare'],
+  ['sql_execution_plan_properties_panel_name', 'Nome'],
+  ['sql_execution_plan_properties_panel_value', 'Valore'],
+  ['sql_execution_plan_properties_panel_general', 'Generale'],
+  ['sql_execution_plan_properties_panel_details', 'Dettagli'],
+];
