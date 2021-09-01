719c523d122ad8d1137a2d6b1d57f24429b028f6
Wroud <wrouds@gmail.com>
2021-09-01 14:53:46 +0300

    fix(core-app): CB-1211 empty groups in nav-tree

--- webapp/packages/core-app/src/NavigationTree/navigationTreeConnectionGroupFilter.ts
+++ webapp/packages/core-app/src/NavigationTree/navigationTreeConnectionGroupFilter.ts
@@ -40,12 +40,12 @@ function sortManageable(connectionInfoResource: ConnectionInfoResource): (nodeA:
     const connectionA = connectionInfoResource.get(NodeManagerUtils.connectionNodeIdToConnectionId(nodeA.id));
     const connectionB = connectionInfoResource.get(NodeManagerUtils.connectionNodeIdToConnectionId(nodeB.id));
 
-    // if (!connectionA || !connectionB) {
-    //   if (connectionA === connectionB) {
-    //     return 0;
-    //   }
-    //   return connectionA ? 1 : -1;
-    // }
+    if (!connectionA || !connectionB) {
+      if (connectionA === connectionB) {
+        return 0;
+      }
+      return connectionA ? 1 : -1;
+    }
 
     const nodeAManageable = connectionA?.features.includes(EConnectionFeature.manageable);
     const nodeBManageable = connectionB?.features.includes(EConnectionFeature.manageable);
@@ -80,8 +80,6 @@ export function navigationTreeConnectionGroupFilter(
       const connection = connectionInfoResource.get(NodeManagerUtils.connectionNodeIdToConnectionId(node.id));
       const manageable = !!connection?.features.includes(EConnectionFeature.manageable);
 
-      console.log(!!connection, manageable);
-
       let nextGroup = NAVIGATION_TREE_CONNECTION_GROUPS.unsorted;
 
       if (manageable) {
@@ -109,8 +107,6 @@ export function navigationTreeConnectionGroupFilter(
         && id !== NAVIGATION_TREE_CONNECTION_GROUPS.unmanageable
       );
     }
-
-    console.log(groupedChildren);
 
     return groupedChildren;
   };
