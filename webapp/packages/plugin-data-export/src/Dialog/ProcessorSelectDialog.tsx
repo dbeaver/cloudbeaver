7cfc0b017b32d15a7421fb22bf9166f410411e32
Wroud <wrouds@gmail.com>
2021-09-07 22:52:49 +0300

    fix(plugin-data-export): dialog styles

--- webapp/packages/plugin-data-export/src/Dialog/ProcessorSelectDialog.tsx
+++ webapp/packages/plugin-data-export/src/Dialog/ProcessorSelectDialog.tsx
@@ -25,7 +25,7 @@ const styles = css`
   export-object {
     composes: theme-typography--body2 from global;
     flex-shrink: 0;
-    padding: 16px;
+    padding: 16px 24px;
     max-height: 50px;
     overflow: hidden;
 
