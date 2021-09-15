324252a47ef3162d7e922fd93086d7fd48a31ec9
Wroud <wrouds@gmail.com>
2021-09-15 11:14:16 +0300

    feat: CB-1236 getGQLResponse

---
+++ webapp/packages/core-sdk/src/getGQLResponse.ts
@@ -0,0 +1,29 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2021 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
+import { GQLError } from './GQLError';
+
+interface IGraphQLResponse<T>{
+  data: T | null;
+  error?: Error;
+}
+
+export async function getGQLResponse<T>(query: Promise<T>): Promise<IGraphQLResponse<T>> {
+  try {
+    const data = await query;
+
+    return { data };
+  } catch (exception) {
+    console.log(exception.response);
+    const data = exception instanceof GQLError ? exception.response.data : null;
+    return {
+      data,
+      error: exception,
+    };
+  }
+}
--- webapp/packages/core-sdk/src/index.ts
+++ webapp/packages/core-sdk/src/index.ts
@@ -8,6 +8,7 @@ export * from './DetailsError';
 export * from './EnvironmentService';
 export * from './EServerErrorCode';
 export * from './getErrorDetails';
+export * from './getGQLResponse';
 export * from './GQLError';
 export * from './GQLErrorCatcher';
 export * from './GraphQLService';
