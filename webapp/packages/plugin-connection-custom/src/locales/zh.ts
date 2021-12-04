45c5d58da6774189068eff22d94106571187b29a
ianzhi <ianzhi@126.com>
2021-12-04 15:54:28 +0800

    add zh.ts

---
+++ webapp/packages/plugin-connection-custom/src/locales/zh.ts
@@ -0,0 +1,18 @@
+export default [
+    ['customConnection_connectionType_custom', '参数'],
+    ['customConnection_connectionType_url', 'URL'],
+    ['customConnection_options', '选项'],
+    ['customConnection_properties', '驱动属性'],
+    ['customConnection_custom_name', '名称'],
+    ['customConnection_custom_host', '主机'],
+    ['customConnection_custom_obligatory', '（强制性的）'],
+    ['customConnection_custom_port', '端口'],
+    ['customConnection_custom_database', '数据库'],
+    ['customConnection_custom_server', '服务器'],
+    ['customConnection_url_JDBC', 'JDBC URL'],
+    ['customConnection_userName', '用户名称'],
+    ['customConnection_Password', '密码'],
+    ['customConnection_test', '测试连接'],
+    ['customConnection_create', '创建'],
+    ['customConnection_create_error', '创建连接失败'],
+  ];
---
+++ webapp/packages/plugin-connection-template/src/locales/zh.ts
@@ -0,0 +1,12 @@
+export default [
+    ['basicConnection_connectionDialog_newConnection', '新连接'],
+    ['basicConnection_connectionDialog_title', '连接数据库'],
+    ['basicConnection_connectionDialog_listTitle', '数据库：'],
+    ['basicConnection_connectionDialog_username', '用户库用户名：'],
+    ['basicConnection_connectionDialog_usernamePlaceholder', '用户'],
+    ['basicConnection_connectionDialog_password', '数据库用户密码：'],
+    ['basicConnection_connectionDialog_passwordPlaceholder', '密码'],
+    ['basicConnection_connectionDialog_connecting', '连接中...'],
+    ['basicConnection_connectionDialog_connecting_message', '连接数据库...'],
+    ['basicConnection_main_menu_item', '从模板创建'],
+  ];
