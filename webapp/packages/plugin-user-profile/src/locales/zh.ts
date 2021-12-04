3172b0f55fcde85066dda4c70781bdc426447f82
ianzhi <ianzhi@126.com>
2021-12-04 16:22:33 +0800

    add zh.ts

---
+++ webapp/packages/plugin-user-profile/src/locales/zh.ts
@@ -0,0 +1,18 @@
+export default [
+    ['plugin_user_profile_menu', '简介'],
+    ['plugin_user_profile_info', '信息'],
+    ['plugin_user_profile_info_id', '用户ID'],
+    ['plugin_user_profile_info_displayName', '显示名称'],
+    ['plugin_user_profile_auth_providers', '认证提供者'],
+    ['plugin_user_profile_info', '用户信息'],
+    ['plugin_user_profile_auth_tokens', '认证令牌'],
+    ['plugin_user_profile_auth_providers_active', '认证处于活跃状态'],
+
+    ['plugin_user_profile_authentication_change_password', '更改密码'],
+    ['plugin_user_profile_authentication_change_password_current_password', '当前密码'],
+    ['plugin_user_profile_authentication_change_password_new_password', '新密码'],
+    ['plugin_user_profile_authentication_change_password_repeat_password', '重复密码'],
+    ['plugin_user_profile_authentication_change_password_success', '密码更改成功'],
+    ['plugin_user_profile_authentication_change_password_submit_label', '更改'],
+    ['plugin_user_profile_authentication_change_password_passwords_not_match', "密码不匹配"],
+  ];
