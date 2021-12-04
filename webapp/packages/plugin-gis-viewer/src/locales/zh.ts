a4f9a1eb279e91a6b8850a626dd790c470a56895
ianzhi <ianzhi@126.com>
2021-12-04 16:12:40 +0800

    add zh.ts

---
+++ webapp/packages/plugin-gis-viewer/src/locales/zh.ts
@@ -0,0 +1,6 @@
+export default [
+    ['gis_presentation_title', '地图'],
+    ['gis_presentation_placeholder', '无可用数据'],
+    ['gis_presentation_base_tile_street_name', '街道'],
+    ['gis_presentation_base_tile_topography_name', '地形'],
+  ];
