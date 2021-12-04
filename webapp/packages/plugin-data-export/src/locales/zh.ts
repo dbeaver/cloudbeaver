1d8adc5a34f04536a66447a4e3ce702a9e43ec66
ianzhi <ianzhi@126.com>
2021-12-04 16:02:10 +0800

    add zh.ts

---
+++ webapp/packages/plugin-data-export/src/locales/zh.ts
@@ -0,0 +1,15 @@
+export default [
+    ['data_transfer_dialog_title', '导出数据'],
+    ['data_transfer_dialog_export', '导出'],
+    ['data_transfer_dialog_export_tooltip', '将结果集导出为文件'],
+    ['data_transfer_dialog_configuration_title', '导出配置'],
+    ['data_transfer_dialog_preparation', '我们准备您的文件以供导出。请稍等...'],
+    ['data_transfer_notification_preparation', '我们准备您的文件以供下载。请稍等...'],
+    ['data_transfer_notification_ready', '文件已经准备好下载'],
+    ['data_transfer_notification_error', '数据导出失败'],
+    ['data_transfer_notification_cancelled', '数据导出已取消'],
+    ['data_transfer_notification_download', '下载'],
+    ['data_transfer_notification_delete', '删除'],
+    ['data_transfer_exporting_table', '表：'],
+    ['data_transfer_exporting_sql', 'SQL：'],
+  ];
