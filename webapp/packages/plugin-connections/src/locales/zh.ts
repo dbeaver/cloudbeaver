02a7e017502d20eed49250ea0652e9b363337e9d
ianzhi <ianzhi@126.com>
2021-12-04 15:56:38 +0800

    add zh.ts

---
+++ webapp/packages/plugin-connections/src/locales/zh.ts
@@ -0,0 +1,8 @@
+export default [
+    ['connections_public_connection_edit_menu_item_title', '编辑连接'],
+    ['connections_public_connection_edit_cancel_title', '取消确认'],
+    ['connections_public_connection_edit_cancel_message', '您将取消连接更改。未保存的更改将丢失。您确定吗？'],
+    ['connections_public_connection_edit_reconnect_title', '连接已更新'],
+    ['connections_public_connection_edit_reconnect_message', '连接已更新。您想重新连接吗？'],
+    ['connections_public_connection_edit_reconnect_failed', '重新连接失败'],
+  ];
