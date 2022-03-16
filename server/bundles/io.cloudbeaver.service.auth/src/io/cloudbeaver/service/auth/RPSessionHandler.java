eb8fcea583623ddbd37f6f3ccc89ba4ee0bd6fa5
Ainur <59531286+yagudin10@users.noreply.github.com>
2022-03-16 14:30:18 +0300

    CB-1546 fix name of variable (#715)
    * CB-1546 fix name of variable
    
    * CB-1546 add a field to the runtime conf

--- server/bundles/io.cloudbeaver.service.auth/src/io/cloudbeaver/service/auth/RPSessionHandler.java
+++ server/bundles/io.cloudbeaver.service.auth/src/io/cloudbeaver/service/auth/RPSessionHandler.java
@@ -1,3 +1,19 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2022 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cloudbeaver.service.auth;
 
 import io.cloudbeaver.DBWUserIdentity;
@@ -34,12 +50,12 @@ public class RPSessionHandler implements DBWSessionHandler {
     public boolean handleSessionOpen(WebSession webSession, HttpServletRequest request, HttpServletResponse response) throws DBException, IOException {
         boolean configMode = CBApplication.getInstance().isConfigurationMode();
         //checks if the app is not in configuration mode and reverse proxy auth is enabled in the config file
-        if (!configMode && CBApplication.getInstance().getAppConfiguration().isSupportsReverseProxyAuth()) {
+        if (!configMode && CBApplication.getInstance().getAppConfiguration().isEnabledReverseProxyAuth()) {
             reverseProxyAuthentication(request, webSession);
         }
         return false;
     }
-    public boolean reverseProxyAuthentication(@NotNull HttpServletRequest request, @NotNull WebSession webSession) throws DBWebException {
+    public void reverseProxyAuthentication(@NotNull HttpServletRequest request, @NotNull WebSession webSession) throws DBWebException {
         SMController<WebUser, ?, ?> securityController = CBPlatform.getInstance().getApplication().getSecurityController();
         AuthProviderDescriptor authProvider = AuthProviderRegistry.getInstance().getAuthProvider(AUTH_PROVIDER);
 
@@ -92,7 +108,6 @@ public class RPSessionHandler implements DBWSessionHandler {
                 throw new DBWebException("Error", e);
             }
         }
-        return false;
     }
     @Override
     public boolean handleSessionClose(WebSession webSession) throws DBException, IOException {
