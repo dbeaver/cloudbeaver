ac2515fb59a3515a8b2fb5ed07f845e2d7f71d40
Cheng Pan <chengpan@apache.org>
2022-05-27 01:10:26 +0800

    Update zh locales

--- webapp/packages/plugin-resource-manager/src/locales/zh.ts
+++ webapp/packages/plugin-resource-manager/src/locales/zh.ts
@@ -12,9 +12,9 @@ export default [
   ['plugin_resource_manager_save_script_error', '打开脚本出错'],
   ['plugin_resource_manager_update_script_error', '更新脚本出错'],
   ['plugin_resource_manager_sync_script_error', '同步查询失败'],
-  ['plugin_resource_manager_delete_script_error', 'Error occurred while trying to delete the script'],
-  ['plugin_resource_manager_script_delete_confirmation', 'You are going to delete "{args.name}". Are you sure?'],
+  ['plugin_resource_manager_delete_script_error', '删除脚本时发生错误'],
+  ['plugin_resource_manager_script_delete_confirmation', '你确定要删除"{args.name}"吗？'],
   ['plugin_resource_manager_script_not_found_title', '找不到关联的脚本'],
   ['plugin_resource_manager_script_not_found_message', '脚本可能被删除或重命名，将使用最后的有效值'],
-  ['plugin_resource_manager_script_name_invalid_characters_message', 'Script\'s name may contain the following symbols "_-$.()@" and can\'t start with a dot'],
+  ['plugin_resource_manager_script_name_invalid_characters_message', '脚本名称中可以使用字符"_-$." 但不能以"."开头'],
 ];
