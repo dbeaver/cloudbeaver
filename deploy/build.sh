5e184ee384e3ed2f68b5119c5ecfe75ee6282f37
serge-rider <serge@jkiss.org>
2022-06-24 17:06:12 +0200

    Use verify instead package

--- deploy/build.sh
+++ deploy/build.sh
@@ -28,7 +28,7 @@ cd cloudbeaver/deploy
 echo "Build CloudBeaver server"
 
 cd ../server/product/aggregate
-mvn clean package -Dheadless-platform
+mvn clean postgres -Dheadless-platform
 if [[ "$?" -ne 0 ]] ; then
   echo 'Could not perform package'; exit $rc
 fi
