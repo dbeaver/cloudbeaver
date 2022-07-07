856bfffb81fd33b1839f708450e70c98c81411bc
Naumov Alexey <Iamemptyhuh@gmail.com>
2022-07-07 15:32:18 +0200

    fix(plugin-resource-manager): add missing symbols to the zh locale token

--- webapp/packages/plugin-resource-manager/src/locales/zh.ts
+++ webapp/packages/plugin-resource-manager/src/locales/zh.ts
@@ -16,5 +16,5 @@ export default [
   ['plugin_resource_manager_script_delete_confirmation', '你确定要删除"{args.name}"吗？'],
   ['plugin_resource_manager_script_not_found_title', '找不到关联的脚本'],
   ['plugin_resource_manager_script_not_found_message', '脚本可能被删除或重命名，将使用最后的有效值'],
-  ['plugin_resource_manager_script_name_invalid_characters_message', '脚本名称中可以使用字符"_-$." 但不能以"."开头'],
+  ['plugin_resource_manager_script_name_invalid_characters_message', '脚本名称中可以使用字符"_-$.()@" 但不能以"."开头'],
 ];
