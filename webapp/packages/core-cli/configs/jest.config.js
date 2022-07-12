37b61a04843b18e938379d6dc334542c23ccb446
Wroud <wroud@yandex.ru>
2022-07-13 02:28:37 +0300

    feat(test): CB-2288 configure jsdom testing

--- webapp/packages/core-cli/configs/jest.config.js
+++ webapp/packages/core-cli/configs/jest.config.js
@@ -1,10 +1,13 @@
 // https://jestjs.io/docs/configuration
 
 module.exports = {
-  // collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/types.ts'],
-  // coverageReporters: ['json'],
+  transformIgnorePatterns: [
+    "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"
+  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/types.ts'],
+  coverageReporters: ['json'],
   // restoreMocks: true,
-  // testEnvironment: 'jsdom',
+  testEnvironment: 'jsdom',
   setupFiles: ['../tests/setup.ts'],
   transform: {"\\.[jt]sx?$": ['babel-jest', { configFile: require.resolve('./babel.config.js') }]}
 };
