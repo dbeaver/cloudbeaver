e8293ff91564bd46fc6d7c5e4934cb694579052b
Wroud <wroud@yandex.ru>
2022-07-15 03:54:13 +0300

    feat(test): CB-2288 testing component in initialized app

---
+++ webapp/packages/core-authentication/mocks/mockAuthentication.ts
@@ -0,0 +1,17 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2022 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
+import type { graphql } from 'msw';
+
+import { mockGetActiveUser } from './resolvers/mockGetActiveUser';
+
+export function mockAuthentication(endpoint: ReturnType<typeof graphql.link>) {
+  return [
+    endpoint.query('getActiveUser', mockGetActiveUser),
+  ];
+}
