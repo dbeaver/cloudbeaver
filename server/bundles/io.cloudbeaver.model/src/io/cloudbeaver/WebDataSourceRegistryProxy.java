9290ed1f8cfc805ee69a77724f157cd2f4203e18
serge-rider <serge@jkiss.org>
2022-09-18 23:41:15 +0200

    dbeaver/dbeaver-ee#1965 Auth profiles secrets load/save

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/WebDataSourceRegistryProxy.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/WebDataSourceRegistryProxy.java
@@ -12,6 +12,7 @@ import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
 import org.jkiss.dbeaver.model.connection.DBPDriver;
 import org.jkiss.dbeaver.model.net.DBWNetworkProfile;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.secret.DBSSecretController;
 import org.jkiss.dbeaver.model.struct.DBSObjectFilter;
 import org.jkiss.dbeaver.registry.DataSourceConfigurationManager;
 import org.jkiss.dbeaver.registry.DataSourcePersistentRegistry;
@@ -301,4 +302,13 @@ public class WebDataSourceRegistryProxy implements DBPDataSourceRegistry, DataSo
         dataSourceRegistry.saveConfigurationToManager(monitor, configurationManager, filter);
     }
 
+    @Override
+    public void persistSecrets(DBSSecretController secretController) throws DBException {
+        dataSourceRegistry.persistSecrets(secretController);
+    }
+
+    @Override
+    public void resolveSecrets(DBSSecretController secretController) throws DBException {
+        dataSourceRegistry.resolveSecrets(secretController);
+    }
 }
