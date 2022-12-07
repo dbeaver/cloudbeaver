59159804806f6e405f8b924e623f843a8913e1d8
Roman Kunitskiy <roman.kunitskiy@gmail.com>
2022-12-07 16:24:52 +0100

    dbeaver/dbeaver-devops#417 chown volume and run server as non root user

--- deploy/scripts/run-server.sh
+++ deploy/scripts/run-server.sh
@@ -4,6 +4,9 @@ launcherJar=( server/plugins/org.eclipse.equinox.launcher*.jar )
 
 echo "Starting Cloudbeaver Server"
 
+chown -R cloudbeaver:cloudbeaver /opt/cloudbeaver 
+
 [ ! -d "workspace/.metadata" ] && mkdir -p workspace/.metadata && mkdir -p workspace/GlobalConfiguration/.dbeaver && cp conf/initial-data-sources.conf workspace/GlobalConfiguration/.dbeaver/data-sources.json
 
-java ${JAVA_OPTS} -jar ${launcherJar} -product io.cloudbeaver.product.ce.product -web-config conf/cloudbeaver.conf -nl en -registryMultiLanguage
+su cloudbeaver -c "/opt/java/openjdk/bin/java ${JAVA_OPTS} -jar ${launcherJar} -product io.cloudbeaver.product.ce.product -web-config conf/cloudbeaver.conf -nl en -registryMultiLanguage"
+# java ${JAVA_OPTS} -jar ${launcherJar} -product io.cloudbeaver.product.ce.product -web-config conf/cloudbeaver.conf -nl en -registryMultiLanguage
