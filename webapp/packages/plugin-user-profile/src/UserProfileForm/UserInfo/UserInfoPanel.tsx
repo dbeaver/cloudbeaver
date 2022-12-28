be1ad7a766049b48a66b4e61c7051619d804064a
alex <48489896+devnaumov@users.noreply.github.com>
2022-12-28 15:49:36 +0100

    Cb 3005 show auth role (#1374)
    * CB-3005 show auth role in user profile
    
    * CB-3005 code style
    
    * CB-3005 pass authRole to current user
    
    Co-authored-by: kseniaguzeeva <112612526+kseniaguzeeva@users.noreply.github.com>

--- webapp/packages/plugin-user-profile/src/UserProfileForm/UserInfo/UserInfoPanel.tsx
+++ webapp/packages/plugin-user-profile/src/UserProfileForm/UserInfo/UserInfoPanel.tsx
@@ -40,8 +40,8 @@ export const UserInfoPanel = observer<Props>(function UserInfoPanel({
             <GroupTitle>{translate('plugin_user_profile_info')}</GroupTitle>
             <Container wrap gap>
               <InputField
-                type="text"
-                name="userId"
+                type='text'
+                name='userId'
                 minLength={1}
                 state={user}
                 mod='surface'
@@ -53,8 +53,8 @@ export const UserInfoPanel = observer<Props>(function UserInfoPanel({
                 {translate('plugin_user_profile_info_id')}
               </InputField>
               <InputField
-                type="text"
-                name="displayName"
+                type='text'
+                name='displayName'
                 minLength={1}
                 state={user}
                 mod='surface'
@@ -64,6 +64,18 @@ export const UserInfoPanel = observer<Props>(function UserInfoPanel({
                 fill
               >
                 {translate('plugin_user_profile_info_displayName')}
+              </InputField>
+              <InputField
+                type='text'
+                name='authRole'
+                mod='surface'
+                state={user}
+                autoHide
+                readOnly
+                tiny
+                fill
+              >
+                {translate('authentication_user_role')}
               </InputField>
             </Container>
             <Loader state={userMetaParameters} inline>
