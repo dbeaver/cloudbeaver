d3232c22e90426072b604e636d575860fe7ed9d6
Ainur <yagudin10@yandex.ru>
2023-01-19 16:32:20 +0100

    CB-2881 remove user teams validation

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/session/WebSessionAuthProcessor.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/session/WebSessionAuthProcessor.java
@@ -161,9 +161,6 @@ public class WebSessionAuthProcessor {
                     providerConfig,
                     authAttrs);
 
-                if (!configMode && securityController.getCurrentUserTeams().length == 0) {
-                    throw new DBWebException("Access denied (no permissions)");
-                }
                 if (!configMode && !securityController.getCurrentUser().isEnabled()) {
                     throw new DBWebException("User account is locked");
                 }
