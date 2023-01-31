bd9d85bb88ae0873301ac0166ee482bb254fc14e
Ainur <yagudin10@yandex.ru>
2023-01-31 19:28:38 +0200

    CB-3100 rm event for creating project

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/rm/local/LocalResourceController.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/rm/local/LocalResourceController.java
@@ -251,6 +251,7 @@ public class LocalResourceController implements RMController {
         }
         try {
             Files.createDirectories(getProjectPath(project.getId()));
+            fireRmProjectAddEvent(project);
             return project;
         } catch (IOException e) {
             throw new DBException("Error creating project path", e);
@@ -814,6 +815,14 @@ public class LocalResourceController implements RMController {
             new RMEvent(RMEvent.Action.RESOURCE_DELETE,
                 makeProjectFromId(projectId, false),
                 resourcePath
+            )
+        );
+    }
+
+    private void fireRmProjectAddEvent(@NotNull RMProject project) throws DBException {
+        RMEventManager.fireEvent(
+            new RMEvent(RMEvent.Action.RESOURCE_ADD,
+                project
             )
         );
     }
