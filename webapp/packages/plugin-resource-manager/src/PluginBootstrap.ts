331ebf1b1251876ea060b0ed1d9cfd99bafa45fe
naumov <iamemptyhuh@gmail.com>
2023-02-10 14:14:53 +0100

    CB-3042 subscribe via component

--- webapp/packages/plugin-resource-manager/src/PluginBootstrap.ts
+++ webapp/packages/plugin-resource-manager/src/PluginBootstrap.ts
@@ -7,42 +7,17 @@
  */
 
 import { Bootstrap, injectable } from '@cloudbeaver/core-di';
-import { ResourceManagerResource } from '@cloudbeaver/core-resource-manager';
-import { ServerConfigResource } from '@cloudbeaver/core-root';
-import { CachedResourceParamKey } from '@cloudbeaver/core-sdk';
-
-import { ResourceManagerService } from './ResourceManagerService';
 
 @injectable()
 export class PluginBootstrap extends Bootstrap {
-  private subscriptionKey: string | null;
-
   constructor(
-    private readonly resourceManagerResource: ResourceManagerResource,
-    private readonly resourceManagerService: ResourceManagerService,
-    private readonly serverConfigResource: ServerConfigResource
   ) {
     super();
-
-    this.subscriptionKey = null;
   }
 
   register(): void | Promise<void> {
-    this.serverConfigResource.onDataUpdate.addHandler(this.subscriptionHandler.bind(this));
   }
 
   async load(): Promise<void> {
-    this.subscriptionHandler();
-  }
-
-  private subscriptionHandler() {
-    if (this.resourceManagerService.enabled) {
-      this.subscriptionKey = this.resourceManagerResource.use(CachedResourceParamKey);
-    } else {
-      if (this.subscriptionKey) {
-        this.resourceManagerResource.free(CachedResourceParamKey, this.subscriptionKey);
-        this.subscriptionKey = null;
-      }
-    }
   }
 }
