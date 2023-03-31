e1f9f1858b0e396ea1330b5f0fa2b481ad90f7fc
Alexey <wrouds@gmail.com>
2023-03-31 16:54:30 +0300

    chore: update copyright year to 2023 (#1579)

--- webapp/packages/core-blocks/src/layout/AppLogo.tsx
+++ webapp/packages/core-blocks/src/layout/AppLogo.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2022 DBeaver Corp and others
+ * Copyright (C) 2020-2023 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -49,7 +49,7 @@ export const AppLogo: React.FC<Props> = function AppLogo({ title, onClick }) {
   return styled(styles)(
     <logo title={title} onClick={onClick}>
       <IconOrImage icon="/icons/logo.svg" {...use({ primary: true })} />
-      <IconOrImage icon="/icons/logo_sm.svg" {...use({ small: true })}/>
+      <IconOrImage icon="/icons/logo_sm.svg" {...use({ small: true })} />
     </logo>
   );
 };
--- webapp/packages/plugin-data-viewer/src/DatabaseDataModel/Actions/IDatabaseDataResultAction.ts
+++ webapp/packages/plugin-data-viewer/src/DatabaseDataModel/Actions/IDatabaseDataResultAction.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2022 DBeaver Corp and others
+ * Copyright (C) 2020-2023 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -9,6 +9,4 @@
 import type { IDatabaseDataAction } from '../IDatabaseDataAction';
 import type { IDatabaseDataResult } from '../IDatabaseDataResult';
 
-export interface IDatabaseDataResultAction<TResult extends IDatabaseDataResult>
-  extends IDatabaseDataAction<any, TResult> {
-}
+export type IDatabaseDataResultAction<TResult extends IDatabaseDataResult> = IDatabaseDataAction<any, TResult>;
