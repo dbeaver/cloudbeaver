a571eeef8e85c8a4422684f4ca1121c214846a4f
Tatiana Fokina <123386439+tfokina@users.noreply.github.com>
2023-07-05 14:44:35 +0600

    CB-3615 change notification for app update, remove dialog (#1796)
    Co-authored-by: dariamarutkina <125263541+dariamarutkina@users.noreply.github.com>

--- webapp/packages/plugin-browser/src/locales/en.ts
+++ webapp/packages/plugin-browser/src/locales/en.ts
@@ -1,4 +1,4 @@
 export default [
-  ['plugin_browser_update_dialog_title', 'Update available'],
+  ['plugin_browser_update_dialog_title', 'Application is updating...'],
   ['plugin_browser_update_dialog_message', 'New version of CloudBeaver is available. Do you want to update?'],
 ];
