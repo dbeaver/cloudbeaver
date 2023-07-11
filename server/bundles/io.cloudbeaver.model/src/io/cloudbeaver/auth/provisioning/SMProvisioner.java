a38d0f89ed6d4bdccec4b23c95e3577d9795a71e
Ainur <yagudin10@yandex.ru>
2023-07-11 17:07:26 +0200

    CB-3612 get resource token only after calling gql func

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/auth/provisioning/SMProvisioner.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/auth/provisioning/SMProvisioner.java
@@ -1,7 +1,9 @@
 package io.cloudbeaver.auth.provisioning;
 
+import io.cloudbeaver.DBWebException;
 import io.cloudbeaver.model.session.WebSession;
 import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.model.security.SMAuthProviderCustomConfiguration;
 import org.jkiss.dbeaver.model.security.user.SMUserProvisioning;
 
 import java.util.List;
@@ -10,6 +12,7 @@ public interface SMProvisioner {
     @NotNull
     List<SMUserProvisioning> listExternalUsers(
         @NotNull WebSession webSession,
+        @NotNull SMAuthProviderCustomConfiguration customConfiguration,
         @NotNull SMProvisioningFilter filter
-    );
+    ) throws DBWebException;
 }
