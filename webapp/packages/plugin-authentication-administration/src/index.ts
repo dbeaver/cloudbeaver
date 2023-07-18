57d2b08681fd548e860dbd67eff6246bf08d2973
Tatiana Fokina <tatiana.fokina@dbeaver.com>
2023-07-19 00:12:37 +0600

    CB-3486 fix front errors

--- webapp/packages/plugin-authentication-administration/src/index.ts
+++ webapp/packages/plugin-authentication-administration/src/index.ts
@@ -8,3 +8,5 @@ export * from './Administration/Users/Teams/TeamFormService';
 export * from './Administration/Users/Teams/ITeamFormProps';
 export * from './Administration/Users/Teams/Contexts/teamContext';
 export * from './Administration/Users/UserForm/UserFormService';
+export * from './Administration/IdentityProviders/IAuthConfigurationFormProps';
+export * from './Administration/IdentityProviders/AuthConfigurationFormService';
