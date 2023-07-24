f855c463968f672398089d327a722498d6d68f8b
Tatiana Fokina <tatiana.fokina@dbeaver.com>
2023-07-24 17:25:18 +0600

    CB-3587 add css modules, remove reshadow styles from snackbars

--- webapp/packages/core-blocks/src/Snackbars/SnackbarMarkups/SnackbarWrapper.tsx
+++ webapp/packages/core-blocks/src/Snackbars/SnackbarMarkups/SnackbarWrapper.tsx
@@ -6,46 +6,11 @@
  * you may not use this file except in compliance with the License.
  */
 import { useEffect, useState } from 'react';
-import styled, { css, use } from 'reshadow';
 
 import { IconButton } from '../../IconButton';
-
-const SNACKBAR_WRAPPER_STYLES = css`
-  notification {
-    composes: theme-background-surface theme-text-on-surface theme-elevation-z5 from global;
-    position: relative;
-    display: flex;
-    box-sizing: border-box;
-    overflow: hidden;
-    width: 500px;
-    margin-bottom: 16px;
-    margin-left: 16px;
-    padding: 16px 16px;
-    line-height: 1.5;
-    opacity: 0;
-    border-radius: var(--theme-group-element-radius);
-    transition: opacity 0.3s ease-in-out, transform 0.5s ease-in-out;
-    transform: translateX(-100%);
-
-    &[use|mounted] {
-      transform: translateX(0);
-      opacity: 1;
-    }
-    &[use|closing] {
-      opacity: 0;
-    }
-  }
-  IconButton {
-    position: absolute;
-    top: 8px;
-    right: 8px;
-    height: 22px;
-    width: 22px;
-    &:hover {
-      opacity: 0.7;
-    }
-  }
-`;
+import { s } from '../../s';
+import { useS } from '../../useS';
+import style from './SnackbarWrapper.m.css';
 
 interface Props {
   closing?: boolean;
@@ -61,16 +26,17 @@ export const SnackbarWrapper: React.FC<React.PropsWithChildren<Props>> = functio
   children,
   className,
 }) {
+  const styles = useS(style);
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  return styled(SNACKBAR_WRAPPER_STYLES)(
-    <notification as="div" className={className} {...use({ mounted, closing })}>
+  return (
+    <div data-testid="notification" className={s(styles, { notification: true, mounted, closing }, className)}>
       {children}
-      {!persistent && onClose && <IconButton name="cross" viewBox="0 0 16 16" onClick={onClose} />}
-    </notification>,
+      {!persistent && onClose && <IconButton name="cross" viewBox="0 0 16 16" className={styles.iconButton} onClick={onClose} />}
+    </div>
   );
 };
