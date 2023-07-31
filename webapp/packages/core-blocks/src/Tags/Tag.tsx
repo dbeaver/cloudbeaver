1073335af50acda9a3be3c28f05286054a76e417
Dmitriy Osipov <75555523+teunlao@users.noreply.github.com>
2023-07-31 17:35:16 +0200

    CB-3586: migrate tags to css-modules (#1835)
    Co-authored-by: mr-anton-t <42037741+mr-anton-t@users.noreply.github.com>

--- webapp/packages/core-blocks/src/Tags/Tag.tsx
+++ webapp/packages/core-blocks/src/Tags/Tag.tsx
@@ -6,9 +6,11 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import styled, { css } from 'reshadow';
 
 import { IconOrImage } from '../IconOrImage';
+import { useS } from '../useS';
+import { s } from '../s';
+import style from './Tags.m.css';
 
 export interface ITag<T extends string | number = string> {
   id: T;
@@ -16,73 +18,27 @@ export interface ITag<T extends string | number = string> {
   icon?: string;
 }
 
-const style = css`
-  tag-container {
-    composes: theme-ripple theme-background-secondary theme-text-on-secondary from global;
-    display: flex;
-    align-items: center;
-    padding: 4px 4px 4px 8px;
-    border-radius: var(--theme-form-element-radius);
-    max-width: 150px;
-    &::before {
-      border-radius: var(--theme-form-element-radius);
-    }
-  }
-  tag-icon {
-    display: flex;
-    flex-shrink: 0;
-    width: 16px;
-    margin-right: var(--theme-form-element-radius);
-  }
-  tag-content {
-    composes: theme-typography--caption from global;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-  tag-actions {
-    position: relative;
-    display: flex;
-  }
-  tag-action {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 12px;
-    padding: 4px;
-    cursor: pointer;
-    flex-shrink: 0;
-    opacity: 0.5;
-
-    &:hover {
-      opacity: 1;
-    }
-  }
-  IconOrImage {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
 interface Props<T extends string | number> extends ITag<T> {
   onRemove: (id: T) => void;
   className?: string;
 }
 
 export const Tag = observer(function Tag<T extends string | number>({ id, label, icon, onRemove, className }: Props<T>) {
-  return styled(style)(
-    <tag-container as="li" title={label} className={className}>
+  const styles = useS(style);
+
+  return (
+    <li title={label} className={s(styles, { tagContainer: true }, className)}>
       {icon && (
-        <tag-icon>
-          <IconOrImage icon={icon} />
-        </tag-icon>
+        <div className={s(styles, { tagIcon: true })}>
+          <IconOrImage icon={icon} className={s(styles, { iconOrImage: true })} />
+        </div>
       )}
-      <tag-content>{label}</tag-content>
-      <tag-actions>
-        <tag-action onClick={() => onRemove(id)}>
-          <IconOrImage icon="cross" />
-        </tag-action>
-      </tag-actions>
-    </tag-container>,
+      <div className={s(styles, { tagContent: true })}>{label}</div>
+      <div className={s(styles, { tagActions: true })}>
+        <div className={s(styles, { tagAction: true })} onClick={() => onRemove(id)}>
+          <IconOrImage className={s(styles, { iconOrImage: true })} icon="cross" />
+        </div>
+      </div>
+    </li>
   );
 });
