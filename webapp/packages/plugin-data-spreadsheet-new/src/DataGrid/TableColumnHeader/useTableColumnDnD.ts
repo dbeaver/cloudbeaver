a8c5990bde5c545c6c771e793d980d8f8d40bb14
Aleksei Potsetsuev <wrouds@gmail.com>
2023-08-14 15:58:56 +0300

    CB-895 chore: remove unused vars

--- webapp/packages/plugin-data-spreadsheet-new/src/DataGrid/TableColumnHeader/useTableColumnDnD.ts
+++ webapp/packages/plugin-data-spreadsheet-new/src/DataGrid/TableColumnHeader/useTableColumnDnD.ts
@@ -37,14 +37,14 @@ export function useTableColumnDnD(model: IDatabaseDataModel, resultIndex: number
   });
 
   const dndBox = useDNDBox({
-    canDrop(context, isOver) {
+    canDrop(context) {
       return (
         context.hasValue(DATA_CONTEXT_DV_DDM, model) &&
         context.hasValue(DATA_CONTEXT_DV_DDM_RESULT_INDEX, resultIndex) &&
         !context.hasValue(DATA_CONTEXT_DV_DDM_RS_COLUMN_KEY, columnKey)
       );
     },
-    onDrop(context, pos) {
+    onDrop(context) {
       const dndColumnKey = context.get(DATA_CONTEXT_DV_DDM_RS_COLUMN_KEY);
 
       if (columnKey && dndColumnKey) {
