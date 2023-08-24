4229235d40fb5ff77f9b49b0ce556e04a3466dbc
naumov <iamemptyhuh@gmail.com>
2023-08-24 12:56:41 +0200

    CB-3880 add 'additional' prefix

--- webapp/packages/plugin-authentication/src/Dialog/AuthDialog.tsx
+++ webapp/packages/plugin-authentication/src/Dialog/AuthDialog.tsx
@@ -114,7 +114,7 @@ export const AuthDialog: DialogComponent<IAuthOptions, null> = observer(function
   }
 
   if (additional) {
-    subTitle = `${state.activeConfiguration?.displayName || state.activeProvider?.label} ${translate('authentication_request_token')}`;
+    dialogTitle = `${translate('authentication_auth_additional')} ${dialogTitle}`;
   }
 
   async function login(linkUser: boolean, provider?: AuthProvider, configuration?: AuthProviderConfiguration) {
