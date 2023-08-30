303ff0cbe1c82ddbed0cf3fecff8b3120d8f1728
Denis Sinelnikov <denis.sinelnikov@dbeaver.com>
2023-08-30 14:09:38 +0400

    CB-2551. Fixed additional auth for disabled user

--- server/bundles/io.cloudbeaver.service.security/src/io/cloudbeaver/service/security/CBEmbeddedSecurityController.java
+++ server/bundles/io.cloudbeaver.service.security/src/io/cloudbeaver/service/security/CBEmbeddedSecurityController.java
@@ -1821,9 +1821,9 @@ public class CBEmbeddedSecurityController implements SMAdminController, SMAuthen
                 if (activeUserId == null) {
                     activeUserId = userIdFromCreds;
                 }
-                storedUserData.put(authConfiguration,
-                    saveSecuredCreds ? userAuthData : filterSecuredUserData(userAuthData, getAuthProvider(authProviderId)));
             }
+            storedUserData.put(authConfiguration,
+                saveSecuredCreds ? userAuthData : filterSecuredUserData(userAuthData, getAuthProvider(authProviderId)));
         }
 
         String tokenAuthRole = updateUserAuthRoleIfNeeded(activeUserId, detectedAuthRole);
