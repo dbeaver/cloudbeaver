8a9ed146ca849e90e8eaec64573fb9052f492e32
Denis Sinelnikov <denis.sinelnikov@dbeaver.com>
2023-09-08 19:15:34 +0400

    CB-3923. Refactor after review

--- server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/server/jobs/SqlLogStateJob.java
+++ server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/server/jobs/SqlLogStateJob.java
@@ -46,8 +46,6 @@ public class SqlLogStateJob extends AbstractJob {
     private final DBCStatement dbcStatement;
     private final DBCServerOutputReader dbcServerOutputReader;
 
-    private volatile boolean shouldCancel = false;
-
     public SqlLogStateJob(WebSession webSession, DBCExecutionContext dbcExecutionContext, DBCStatement dbcStatement,
                           DBCServerOutputReader dbcServerOutputReader) {
         super("Sql log state job");
@@ -61,23 +59,14 @@ public class SqlLogStateJob extends AbstractJob {
     protected IStatus run(DBRProgressMonitor monitor) {
         if (!DBWorkbench.getPlatform().isShuttingDown()) {
             try {
-                if (!dbcStatement.isStatementClosed() ) {
+                if (!dbcStatement.isStatementClosed()) {
                     dumpOutput(monitor);
-                } else {
-                    shouldCancel = true;
+                    schedule(100);
                 }
             } catch (Exception e) {
                 log.debug(e);
             }
-            schedule(100);
-        } else {
-            shouldCancel = true;
         }
-
-        if (shouldCancel) {
-            return Status.CANCEL_STATUS;
-        }
-
         return Status.OK_STATUS;
     }
 
