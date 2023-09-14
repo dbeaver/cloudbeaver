edc15b664f3042d22f2180e00ad8543892240e17
Alexey <wrouds@gmail.com>
2023-09-14 11:31:31 +0300

    CB-3678 feat: update react-data-grid (#1997)
    Co-authored-by: mr-anton-t <42037741+mr-anton-t@users.noreply.github.com>

--- webapp/packages/plugin-data-spreadsheet-new/src/DataGrid/TableColumnHeader/TableIndexColumnHeader.tsx
+++ webapp/packages/plugin-data-spreadsheet-new/src/DataGrid/TableColumnHeader/TableIndexColumnHeader.tsx
@@ -7,35 +7,21 @@
  */
 import { observer } from 'mobx-react-lite';
 import { useContext } from 'react';
-import styled, { css } from 'reshadow';
 
-import { getComputed, IconOrImage, useTranslate } from '@cloudbeaver/core-blocks';
-import type { HeaderRendererProps } from '@cloudbeaver/plugin-react-data-grid';
+import { getComputed, IconOrImage, s, useS, useTranslate } from '@cloudbeaver/core-blocks';
+import type { RenderHeaderCellProps } from '@cloudbeaver/plugin-react-data-grid';
 
 import { DataGridContext } from '../DataGridContext';
 import { DataGridSelectionContext } from '../DataGridSelection/DataGridSelectionContext';
 import { TableDataContext } from '../TableDataContext';
+import style from './TableIndexColumnHeader.m.css';
 
-const styles = css`
-  container {
-    width: 100%;
-    cursor: pointer;
-  }
-  IconOrImage {
-    cursor: auto;
-    width: 10px;
-    position: absolute;
-    right: 8px;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-`;
-
-export const TableIndexColumnHeader = observer<HeaderRendererProps<any>>(function TableIndexColumnHeader(props) {
+export const TableIndexColumnHeader = observer<RenderHeaderCellProps<any>>(function TableIndexColumnHeader(props) {
   const dataGridContext = useContext(DataGridContext);
   const selectionContext = useContext(DataGridSelectionContext);
   const tableDataContext = useContext(TableDataContext);
   const translate = useTranslate();
+  const styles = useS(style);
 
   if (!tableDataContext || !selectionContext || !dataGridContext) {
     throw new Error('Contexts required');
@@ -43,15 +29,17 @@ export const TableIndexColumnHeader = observer<HeaderRendererProps<any>>(functio
 
   const readonly = getComputed(() => tableDataContext.isReadOnly() || dataGridContext.model.isReadonly(dataGridContext.resultIndex));
 
-  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+  function handleClick(event: React.MouseEvent<HTMLDivElement>) {
     selectionContext.selectTable();
     dataGridContext.focus();
   }
 
-  return styled(styles)(
-    <container title={translate('data_grid_table_index_column_tooltip')} onClick={handleClick}>
-      {readonly && <IconOrImage title={translate('data_grid_table_readonly_tooltip')} icon="/icons/lock.png" />}
+  return (
+    <div title={translate('data_grid_table_index_column_tooltip')} className={s(styles, { container: true })} onClick={handleClick}>
+      {readonly && (
+        <IconOrImage title={translate('data_grid_table_readonly_tooltip')} icon="/icons/lock.png" className={s(styles, { iconOrImage: true })} />
+      )}
       {props.column.name}
-    </container>,
+    </div>
   );
 });
