bbf4a32d7669f60716381d942492460bb96ec57c
Dmitry Osipov <teunlao@gmail.com>
2023-09-20 01:48:31 +0200

    feat: migrate ConnectionMark to css-modules

--- webapp/packages/core-blocks/src/ConnectionImageWithMask/ConnectionMark.tsx
+++ webapp/packages/core-blocks/src/ConnectionImageWithMask/ConnectionMark.tsx
@@ -5,26 +5,9 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-import styled, { css, use } from 'reshadow';
-
-const styles = css`
-  status {
-    composes: theme-background-positive from global;
-    position: absolute;
-    opacity: 0;
-    transition: opacity 0.3s ease;
-    bottom: 0;
-    right: 0;
-    box-sizing: border-box;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-
-    &[|connected] {
-      opacity: 1;
-    }
-  }
-`;
+import { s } from '../s';
+import { useS } from '../useS';
+import style from './ConnectionMark.m.css';
 
 interface Props {
   connected: boolean;
@@ -32,5 +15,7 @@ interface Props {
 }
 
 export const ConnectionMark: React.FC<Props> = function ConnectionMark({ connected, className }) {
-  return styled(styles)(<status {...use({ connected })} className={className} />);
+  const styles = useS(style);
+
+  return <div className={s(styles, { status: true, connected }, className)} />;
 };
