e6d6cabc97f4f913b1665addb6dadac59ad0baa3
Dmitry Osipov <teunlao@gmail.com>
2023-09-20 01:34:43 +0200

    feat: migrate StatusMessage to css-modules

--- webapp/packages/core-blocks/src/StatusMessage.tsx
+++ webapp/packages/core-blocks/src/StatusMessage.tsx
@@ -6,38 +6,16 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import styled, { css } from 'reshadow';
 
 import { ENotificationType } from '@cloudbeaver/core-events';
 
 import { IconOrImage } from './IconOrImage';
 import { Link } from './Link';
 import { useTranslate } from './localization/useTranslate';
+import { s } from './s';
+import style from './StatusMessage.m.css';
 import { useErrorDetails } from './useErrorDetails';
-
-const styles = css`
-  status-message {
-    overflow: hidden;
-    composes: theme-typography--caption from global;
-    height: 24px;
-    display: flex;
-    align-items: center;
-    gap: 8px;
-  }
-  IconOrImage {
-    height: 24px;
-    width: 24px;
-  }
-  message,
-  Link {
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-  }
-  Link {
-    cursor: pointer;
-  }
-`;
+import { useS } from './useS';
 
 interface Props {
   message?: string | string[] | null;
@@ -48,6 +26,7 @@ interface Props {
 }
 
 export const StatusMessage = observer<Props>(function StatusMessage({ status, message, exception = null, className, onShowDetails }) {
+  const styles = useS(style);
   const translate = useTranslate();
   const errorDetails = useErrorDetails(exception);
   const isError = status === ENotificationType.Error || exception !== null;
@@ -71,14 +50,22 @@ export const StatusMessage = observer<Props>(function StatusMessage({ status, me
     onShowDetails = errorDetails.open;
   }
 
-  return styled(styles)(
-    <status-message className={className}>
+  return (
+    <div className={s(styles, { statusMessage: true }, className)}>
       {message && (
         <>
-          <IconOrImage icon={icon} />
-          <message title={message}>{onShowDetails ? <Link onClick={onShowDetails}>{message}</Link> : message}</message>
+          <IconOrImage className={s(styles, { iconOrImage: true })} icon={icon} />
+          <div className={s(styles, { message: true })} title={message}>
+            {onShowDetails ? (
+              <Link className={s(styles, { link: true })} onClick={onShowDetails}>
+                {message}
+              </Link>
+            ) : (
+              message
+            )}
+          </div>
         </>
       )}
-    </status-message>,
+    </div>
   );
 });
