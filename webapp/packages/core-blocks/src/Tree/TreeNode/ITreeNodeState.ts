713082215fd8bde05c1a668f210241968e4ab2e9
alex <48489896+devnaumov@users.noreply.github.com>
2023-09-28 12:46:02 +0200

    CB-3197 add interface comments (#2018)
    * CB-3197 add interface comments
    
    * CB-3197 make comments cleaner
    
    * CB-3197 add comment for the indeterminateSelected
    
    * CB-3197 fix code style
    
    ---------
    
    Co-authored-by: Alexey <wrouds@gmail.com>

--- webapp/packages/core-blocks/src/Tree/TreeNode/ITreeNodeState.ts
+++ webapp/packages/core-blocks/src/Tree/TreeNode/ITreeNodeState.ts
@@ -11,6 +11,7 @@ export interface ITreeNodeState {
   disabled?: boolean;
   loading?: boolean;
   selected?: boolean;
+  /** It is true when the node is neither selected nor unselected, used to indicate a mixed or partial selection in a group of sub-options */
   indeterminateSelected?: boolean;
   externalExpanded?: boolean;
   expanded?: boolean;
