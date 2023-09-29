e00ca8736443611a589bbebe75800a659d6aaca6
Alexander Skoblikov <aleksandr.skoblikov@dbeaver.com>
2023-09-29 19:30:34 +0200

    improve auth test (#2030)

--- server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/AuthenticationTest.java
+++ server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/AuthenticationTest.java
@@ -5,6 +5,7 @@ import io.cloudbeaver.utils.WebTestUtils;
 import org.jkiss.dbeaver.model.auth.SMAuthStatus;
 import org.jkiss.dbeaver.model.data.json.JSONUtils;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -21,7 +22,7 @@ public class AuthenticationTest {
 
     @Test
     public void testLoginUser() throws Exception {
-        HttpClient client = CEServerTestSuite.getClient();
+        HttpClient client = CEServerTestSuite.createClient();
         Map<String, Object> authInfo = WebTestUtils.authenticateUser(
             client, CEServerTestSuite.getScriptsPath(), CEServerTestSuite.GQL_API_URL);
         Assert.assertEquals(SMAuthStatus.SUCCESS.name(), JSONUtils.getString(authInfo, "authStatus"));
@@ -29,7 +30,7 @@ public class AuthenticationTest {
 
     @Test
     public void testReverseProxyAnonymousModeLogin() throws Exception {
-        HttpClient client = CEServerTestSuite.getClient();
+        HttpClient client = CEServerTestSuite.createClient();
         Map<String, Object> sessionInfo = openSession(client);
         Assert.assertTrue(JSONUtils.getBoolean(sessionInfo, "valid"));
         Map<String, Object> activeUser = getActiveUser(client);
