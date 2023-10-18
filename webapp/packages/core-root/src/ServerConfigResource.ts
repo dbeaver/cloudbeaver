c9c0039a754ab130f53a1ed0e45032726de28d21
naumov <iamemptyhuh@gmail.com>
2023-10-18 11:37:10 +0200

    CB-3913 move AI feature to the ee

--- webapp/packages/core-root/src/ServerConfigResource.ts
+++ webapp/packages/core-root/src/ServerConfigResource.ts
@@ -19,7 +19,6 @@ import { DataSynchronizationService } from './DataSynchronization/DataSynchroniz
 import { ServerConfigEventHandler } from './ServerConfigEventHandler';
 
 export const FEATURE_GIT_ID = 'git';
-export const FEATURE_AI_ID = 'ai';
 
 export type ServerConfig = Omit<SDKServerConfig, 'hostName'>;
 
