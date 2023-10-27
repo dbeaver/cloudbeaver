aa1a2734a0061d10944c0b5567b9c8cbf25c7d0b
Ainur <yagudin10@yandex.ru>
2023-10-27 12:48:23 +0200

    CB-4123 validate project permissions

--- server/bundles/io.cloudbeaver.service.fs/src/io/cloudbeaver/service/fs/model/WebFSServlet.java
+++ server/bundles/io.cloudbeaver.service.fs/src/io/cloudbeaver/service/fs/model/WebFSServlet.java
@@ -21,14 +21,18 @@ import io.cloudbeaver.model.session.WebSession;
 import io.cloudbeaver.server.CBApplication;
 import io.cloudbeaver.service.WebServiceServletBase;
 import io.cloudbeaver.service.fs.DBWServiceFS;
+import io.cloudbeaver.utils.WebAppUtils;
 import org.eclipse.jetty.server.Request;
 import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.model.data.json.JSONUtils;
+import org.jkiss.dbeaver.model.rm.RMConstants;
+import org.jkiss.dbeaver.model.rm.RMProject;
 import org.jkiss.utils.CommonUtils;
 import org.jkiss.utils.IOUtils;
 
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
@@ -36,7 +40,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
+@MultipartConfig()
 public class WebFSServlet extends WebServiceServletBase {
     private static final String PARAM_PROJECT_ID = "projectId";
     private final DBWServiceFS fs;
@@ -57,7 +63,13 @@ public class WebFSServlet extends WebServiceServletBase {
     }
 
     private void doGet(WebSession session, HttpServletRequest request, HttpServletResponse response) throws DBException, IOException {
-        Path path = getPath(session, request.getParameter(PARAM_PROJECT_ID), request.getParameter("fileURI"));
+        String projectId = request.getParameter(PARAM_PROJECT_ID);
+        RMProject project = WebAppUtils.getProjectById(session, projectId).getRMProject();
+        if (!project.hasProjectPermission(RMConstants.PERMISSION_PROJECT_RESOURCE_VIEW)) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "The user needs more permissions to load files from File Systems.");
+            return;
+        }
+        Path path = getPath(session, projectId, request.getParameter("fileURI"));
         session.addInfoMessage("Download data ...");
         response.setHeader("Content-Type", "application/octet-stream");
         response.setHeader("Content-Disposition", "attachment; filename=\"" + path.getFileName() + "\"");
@@ -68,14 +80,18 @@ public class WebFSServlet extends WebServiceServletBase {
         }
     }
 
-    private void doPost(WebSession session, HttpServletRequest request, HttpServletResponse response) throws DBException {
-        // Hack for getting request params
+    private void doPost(WebSession session, HttpServletRequest request, HttpServletResponse response) throws DBException, IOException {
+        // we need to set this attribute to get parts
         request.setAttribute(Request.__MULTIPART_CONFIG_ELEMENT, new MultipartConfigElement(""));
-        String projectId = JSONUtils.getString(getVariables(request), PARAM_PROJECT_ID);
-        String uri = JSONUtils.getString(getVariables(request), "parentURI");
+        Map<String, Object> variables = getVariables(request);
+        String projectId = JSONUtils.getString(variables, PARAM_PROJECT_ID);
+        RMProject project = WebAppUtils.getProjectById(session, projectId).getRMProject();
+        if (!project.hasProjectPermission(RMConstants.PERMISSION_PROJECT_RESOURCE_EDIT)) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "The user needs more permissions to upload files to File Systems.");
+            return;
+        }
+        String uri = JSONUtils.getString(variables, "parentURI");
         Path path = getPath(session, projectId, uri);
-        // set the final location of parent folder
-        request.setAttribute(Request.__MULTIPART_CONFIG_ELEMENT, new MultipartConfigElement(path.toString()));
         try {
             for (Part part : request.getParts()) {
                 String fileName = part.getSubmittedFileName();
