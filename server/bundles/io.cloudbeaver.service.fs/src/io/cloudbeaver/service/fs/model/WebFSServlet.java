b9a595f307936dadb456d49cade05f3bf6cbe548
Ainur <yagudin10@yandex.ru>
2023-10-27 16:28:15 +0200

    CB-4123 code style fix

--- server/bundles/io.cloudbeaver.service.fs/src/io/cloudbeaver/service/fs/model/WebFSServlet.java
+++ server/bundles/io.cloudbeaver.service.fs/src/io/cloudbeaver/service/fs/model/WebFSServlet.java
@@ -21,13 +21,10 @@ import io.cloudbeaver.model.session.WebSession;
 import io.cloudbeaver.server.CBApplication;
 import io.cloudbeaver.service.WebServiceServletBase;
 import io.cloudbeaver.service.fs.DBWServiceFS;
-import io.cloudbeaver.utils.WebAppUtils;
 import org.eclipse.jetty.server.Request;
 import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.model.data.json.JSONUtils;
-import org.jkiss.dbeaver.model.rm.RMConstants;
-import org.jkiss.dbeaver.model.rm.RMProject;
 import org.jkiss.utils.CommonUtils;
 import org.jkiss.utils.IOUtils;
 
@@ -54,6 +51,10 @@ public class WebFSServlet extends WebServiceServletBase {
 
     @Override
     protected void processServiceRequest(WebSession session, HttpServletRequest request, HttpServletResponse response) throws DBException, IOException {
+        if (!session.isAuthorizedInSecurityManager()) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Anonymous access restricted.");
+            return;
+        }
         if (request.getMethod().equals("POST")) {
             doPost(session, request, response);
         } else {
@@ -64,11 +65,6 @@ public class WebFSServlet extends WebServiceServletBase {
 
     private void doGet(WebSession session, HttpServletRequest request, HttpServletResponse response) throws DBException, IOException {
         String projectId = request.getParameter(PARAM_PROJECT_ID);
-        RMProject project = WebAppUtils.getProjectById(session, projectId).getRMProject();
-        if (!project.hasProjectPermission(RMConstants.PERMISSION_PROJECT_RESOURCE_VIEW)) {
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, "The user needs more permissions to load files from File Systems.");
-            return;
-        }
         Path path = getPath(session, projectId, request.getParameter("fileURI"));
         session.addInfoMessage("Download data ...");
         response.setHeader("Content-Type", "application/octet-stream");
@@ -85,12 +81,7 @@ public class WebFSServlet extends WebServiceServletBase {
         request.setAttribute(Request.__MULTIPART_CONFIG_ELEMENT, new MultipartConfigElement(""));
         Map<String, Object> variables = getVariables(request);
         String projectId = JSONUtils.getString(variables, PARAM_PROJECT_ID);
-        RMProject project = WebAppUtils.getProjectById(session, projectId).getRMProject();
-        if (!project.hasProjectPermission(RMConstants.PERMISSION_PROJECT_RESOURCE_EDIT)) {
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, "The user needs more permissions to upload files to File Systems.");
-            return;
-        }
-        String uri = JSONUtils.getString(variables, "parentURI");
+        String uri = JSONUtils.getString(variables, "toURI");
         Path path = getPath(session, projectId, uri);
         try {
             for (Part part : request.getParts()) {
