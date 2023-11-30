c9cec3dbecd115aac27c010566206a84f32d06f5
s.teleshev <s.teleshev@mu.se>
2023-11-30 19:52:56 +0100

    CB-4266 add license

--- webapp/packages/plugin-root/src/DataSynchronization/DataSynchronizationNotificationMessages.tsx
+++ webapp/packages/plugin-root/src/DataSynchronization/DataSynchronizationNotificationMessages.tsx
@@ -1,3 +1,11 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2023 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
 import { s, useS, useTranslate } from '@cloudbeaver/core-blocks';
 import type { ISynchronizationMessage } from '@cloudbeaver/core-root';
 import { uniq } from '@cloudbeaver/core-utils';
