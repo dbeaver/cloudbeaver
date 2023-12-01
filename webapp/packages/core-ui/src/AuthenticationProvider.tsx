d5828c26896346b15dd5ffcde5d746c01b82bcff
s.teleshev <s.teleshev@mu.se>
2023-12-01 18:36:05 +0100

    CB-3963 removed reshadow from AuthenticationProvider

--- webapp/packages/core-ui/src/AuthenticationProvider.tsx
+++ webapp/packages/core-ui/src/AuthenticationProvider.tsx
@@ -6,18 +6,8 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import styled, { css } from 'reshadow';
-
-import { Button, Container, GroupItem, useStyles, useTranslate } from '@cloudbeaver/core-blocks';
-
+import { Button, Container, GroupItem, useTranslate } from '@cloudbeaver/core-blocks';
 import { useAuthenticationAction } from './useAuthenticationAction';
-
-const styles = css`
-  Container {
-    justify-content: center;
-    align-items: center;
-  }
-`;
 
 export type Props = {
   providerId: string;
@@ -29,21 +19,20 @@ export type Props = {
 
 export const AuthenticationProvider = observer<Props>(function AuthenticationProvider(props) {
   const translate = useTranslate();
-  const style = useStyles(styles);
   const action = useAuthenticationAction(props);
 
   if (action.authorized) {
     return (props.children?.() as null) || null;
   }
 
-  return styled(style)(
-    <Container className={props.className} gap vertical>
+  return (
+    <Container className={props.className} center gap vertical>
       <GroupItem keepSize>{translate('authentication_request_token')}</GroupItem>
       <GroupItem keepSize>
         <Button type="button" mod={['unelevated']} loading={action.authenticating} onClick={action.auth}>
           {translate('authentication_authenticate')}
         </Button>
       </GroupItem>
-    </Container>,
+    </Container>
   );
 });
