6ed92dab146f42c98958213defd3103b4a502ca3
s.teleshev <s.teleshev@mu.se>
2023-12-12 13:15:26 +0100

    refactor(CB-4385): removed reshadow from IconButton

--- webapp/packages/core-blocks/src/IconButton.tsx
+++ webapp/packages/core-blocks/src/IconButton.tsx
@@ -8,33 +8,28 @@
 import { observer } from 'mobx-react-lite';
 import type React from 'react';
 import { Button, ButtonProps } from 'reakit/Button';
-import styled from 'reshadow';
-
-import type { ComponentStyle } from '@cloudbeaver/core-theming';
 
 import { Icon } from './Icon';
 import IconButtonStyles from './IconButton.m.css';
 import { s } from './s';
 import { StaticImage } from './StaticImage';
 import { useS } from './useS';
-import { useStyles } from './useStyles';
 
 interface Props {
   name: string;
   img?: boolean;
   viewBox?: string;
-  style?: ComponentStyle;
 }
 
 export type IconButtonProps = Props & ButtonProps;
 
-export const IconButton: React.FC<IconButtonProps> = observer(function IconButton({ name, img, viewBox, style, className, ...rest }) {
+export const IconButton: React.FC<IconButtonProps> = observer(function IconButton({ name, img, viewBox, className, ...rest }) {
   const styles = useS(IconButtonStyles);
 
-  return styled(useStyles(style))(
+  return (
     <Button {...rest} className={s(styles, { iconButton: true }, className)}>
       {img && <StaticImage className={s(styles, { staticImage: true })} icon={name} />}
       {!img && <Icon className={s(styles, { icon: true })} name={name} viewBox={viewBox} />}
-    </Button>,
+    </Button>
   );
 });
