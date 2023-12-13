a8df087958aaf79780dd1b20faa056d9a82e1b22
s.teleshev <s.teleshev@mu.se>
2023-12-13 21:25:05 +0100

    CB-4386 chore: features naming according to dbeaver

--- webapp/packages/plugin-log-viewer/src/locales/it.ts
+++ webapp/packages/plugin-log-viewer/src/locales/it.ts
@@ -1,5 +1,5 @@
 export default [
-  ['plugin_log_viewer_action_enable_label', 'Visualizzatore del Log'],
+  ['plugin_log_viewer_action_enable_label', 'Visualizzatore del log'],
   ['plugin_log_viewer_placeholder', 'Il visualizzatore del log non è attivo'],
   ['plugin_log_viewer_clear_log', 'Pulisci i log'],
   ['plugin_log_viewer_entry_type', 'Digita'],
