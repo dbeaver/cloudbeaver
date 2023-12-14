807365f42c43f8d5047c7b00faea93062f6c9f1a
alex <48489896+devnaumov@users.noreply.github.com>
2023-12-14 13:25:10 +0100

    CB-4306 update ce deps (#2206)
    * CB-4306 update ce deps
    
    * CB-4306 update yarn lock
    
    * CB-4306 revert quadtree-ts
    
    * CB-4306 lock version
    
    * CB-4306 pass missed globals to the vm context
    
    * CB-4306 revert prettier version
    
    * CB-4306 add types
    
    * CB-4306 fix: msw types
    
    * CB-4306 refactor: remove reshadow types declaration
    
    ---------
    
    Co-authored-by: Alexey <wrouds@gmail.com>
    Co-authored-by: mr-anton-t <42037741+mr-anton-t@users.noreply.github.com>

--- webapp/packages/core-authentication/src/__custom_mocks__/resolvers/mockGetActiveUser.ts
+++ webapp/packages/core-authentication/src/__custom_mocks__/resolvers/mockGetActiveUser.ts
@@ -5,18 +5,18 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-import type { GraphQLContext, GraphQLRequest, ResponseComposition } from 'msw';
+import { GraphQLResolverExtras, GraphQLResponseBody, HttpResponse, ResponseResolver } from 'msw';
 
 import type { GetActiveUserQuery, GetActiveUserQueryVariables } from '@cloudbeaver/core-sdk';
 
-export function mockGetActiveUser(
-  req: GraphQLRequest<GetActiveUserQueryVariables>,
-  res: ResponseComposition<GetActiveUserQuery>,
-  ctx: GraphQLContext<GetActiveUserQuery>,
-): ReturnType<ResponseComposition<GetActiveUserQuery>> {
-  return res(
-    ctx.data({
+export const mockGetActiveUser: ResponseResolver<
+  GraphQLResolverExtras<GetActiveUserQueryVariables>,
+  null,
+  GraphQLResponseBody<GetActiveUserQuery>
+> = function mockGetActiveUser() {
+  return HttpResponse.json({
+    data: {
       user: null as unknown as undefined,
-    }),
-  );
-}
+    },
+  });
+};
