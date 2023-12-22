982485b31284a9a1fe28ae24e9daea66abd50061
alex <48489896+devnaumov@users.noreply.github.com>
2023-12-22 15:57:33 +0100

    Cb 4414 plugin authentication administration css modules (#2246)
    * CB-4414 refactor identity providers to css modules
    
    * CB-4414 migrate UsersTable to css modules
    
    * CB-4414 formatting
    
    * CB-4414 add aria-label
    
    ---------
    
    Co-authored-by: mr-anton-t <42037741+mr-anton-t@users.noreply.github.com>

--- webapp/packages/plugin-authentication-administration/src/Administration/IdentityProviders/CreateAuthConfiguration.tsx
+++ webapp/packages/plugin-authentication-administration/src/Administration/IdentityProviders/CreateAuthConfiguration.tsx
@@ -6,63 +6,32 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import styled, { css } from 'reshadow';
 
-import { IconButton, Translate } from '@cloudbeaver/core-blocks';
+import { Fill, IconButton, s, Translate, useS } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
 
 import { AuthConfigurationForm } from './AuthConfigurationForm';
+import style from './CreateAuthConfiguration.m.css';
 import { CreateAuthConfigurationService } from './CreateAuthConfigurationService';
 
-const styles = css`
-  configuration-create {
-    display: flex;
-    flex-direction: column;
-    height: 660px;
-    overflow: hidden;
-  }
-
-  title-bar {
-    composes: theme-border-color-background theme-typography--headline6 from global;
-    box-sizing: border-box;
-    padding: 16px 24px;
-    align-items: center;
-    display: flex;
-    font-weight: 400;
-    flex: auto 0 0;
-  }
-
-  configuration-create-content {
-    composes: theme-background-secondary theme-text-on-secondary from global;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-    overflow: auto;
-  }
-
-  fill {
-    flex: 1;
-  }
-`;
-
 export const CreateAuthConfiguration: React.FC = observer(function CreateAuthConfiguration() {
+  const styles = useS(style);
   const service = useService(CreateAuthConfigurationService);
 
   if (!service.data) {
     return null;
   }
 
-  return styled(styles)(
-    <configuration-create>
-      <title-bar>
+  return (
+    <div className={s(styles, { box: true })}>
+      <div className={s(styles, { titleBar: true })}>
         <Translate token="administration_identity_providers_configuration_add" />
-        <fill />
+        <Fill />
         <IconButton name="cross" viewBox="0 0 16 16" onClick={service.cancelCreate} />
-      </title-bar>
-      <configuration-create-content>
+      </div>
+      <div className={s(styles, { content: true })}>
         <AuthConfigurationForm state={service.data} onCancel={service.cancelCreate} onSave={service.cancelCreate} />
-      </configuration-create-content>
-    </configuration-create>,
+      </div>
+    </div>
   );
 });
--- webapp/packages/plugin-authentication-administration/src/Administration/Users/Teams/GrantedConnections/GrantedConnectionsTableHeader/GrantedConnectionsTableHeader.tsx
+++ webapp/packages/plugin-authentication-administration/src/Administration/Users/Teams/GrantedConnections/GrantedConnectionsTableHeader/GrantedConnectionsTableHeader.tsx
@@ -6,9 +6,10 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import styled, { css } from 'reshadow';
 
-import { Filter, useStyles, useTranslate } from '@cloudbeaver/core-blocks';
+import { Filter, s, useS, useTranslate } from '@cloudbeaver/core-blocks';
+
+import style from './GrantedConnectionsTableHeader.m.css';
 
 export interface IFilterState {
   filterValue: string;
@@ -20,37 +21,19 @@ interface Props extends React.PropsWithChildren {
   className?: string;
 }
 
-const styles = css`
-  buttons {
-    display: flex;
-    gap: 16px;
-  }
-  header {
-    composes: theme-border-color-background theme-background-surface theme-text-on-surface from global;
-    overflow: hidden;
-    position: sticky;
-    top: 0;
-    z-index: 1;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 16px;
-    gap: 16px;
-    border-bottom: 1px solid;
-  }
-`;
-
 export const GrantedConnectionsTableHeader = observer<Props>(function GrantedConnectionsTableHeader({ filterState, disabled, className, children }) {
+  const styles = useS(style);
   const translate = useTranslate();
-  return styled(useStyles(styles))(
-    <header className={className}>
+
+  return (
+    <header className={s(styles, { header: true }, className)}>
       <Filter
         disabled={disabled}
         placeholder={translate('administration_teams_team_granted_connections_search_placeholder')}
         name="filterValue"
         state={filterState}
       />
-      <buttons>{children}</buttons>
-    </header>,
+      <div className={s(styles, { buttons: true })}>{children}</div>
+    </header>
   );
 });
