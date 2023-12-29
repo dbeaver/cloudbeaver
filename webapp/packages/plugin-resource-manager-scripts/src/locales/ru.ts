59b596679d11dadde8c68eee72d0b3f78287ea55
naumov <iamemptyhuh@gmail.com>
2023-12-29 15:27:05 +0100

    CB-3952 adjust resource file regex

--- webapp/packages/plugin-resource-manager-scripts/src/locales/ru.ts
+++ webapp/packages/plugin-resource-manager-scripts/src/locales/ru.ts
@@ -5,7 +5,7 @@ export default [
   ['plugin_resource_manager_scripts_save_script', 'Сохранить скрипт'],
   [
     'plugin_resource_manager_scripts_script_name_invalid_characters_message',
-    'Имя скрипта может содержать следующие символы "_-$.()@" и не может начинаться с точки',
+    'Имя скрипта не может содержать следующие символы / : " \\ и не может начинаться с точки',
   ],
   ['plugin_resource_manager_scripts_save_script_project_restriction_descripion', 'Проект скрипта не может отличаться от проекта подключения'],
 ];
