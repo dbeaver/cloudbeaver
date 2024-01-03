0f253672dd7bc6479a0c68285973e136c257a53c
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-01-03 17:14:18 +0100

    Cb 4461 json and xml formatting doesnt work in value panel 🔥 (#2268)
    * CB-4461 fix: json and xml formats properly
    
    * CB-4461 fix: edited cell can show its unformatted string value
    
    * CB-4461 fix: panel value - on empty selected cell return empty string
    
    ---------
    
    Co-authored-by: s.teleshev <s.teleshev@mu.se>
    Co-authored-by: mr-anton-t <42037741+mr-anton-t@users.noreply.github.com>

--- webapp/packages/plugin-data-viewer/src/DatabaseDataModel/Actions/ResultSet/isResultSetBinaryFileValue.ts
+++ webapp/packages/plugin-data-viewer/src/DatabaseDataModel/Actions/ResultSet/isResultSetBinaryFileValue.ts
@@ -1,6 +1,5 @@
 import type { IResultSetBinaryFileValue } from './IResultSetBinaryFileValue';
-import type { IResultSetContentValue } from './IResultSetContentValue';
 
-export function isResultSetBinaryFileValue(value: IResultSetContentValue): value is IResultSetBinaryFileValue {
-  return value.contentType === 'application/octet-stream' && Boolean(value?.binary);
+export function isResultSetBinaryFileValue(value: any): value is IResultSetBinaryFileValue {
+  return value?.contentType === 'application/octet-stream' && Boolean(value?.binary);
 }
