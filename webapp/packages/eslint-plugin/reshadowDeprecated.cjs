9ba515e2f5bc44a44f59536d1169f631d078d8ba
Aleksei Potsetsuev <wrouds@gmail.com>
2024-01-09 01:04:46 +0800

    CB-4395 feat: custom eslint rules

--- webapp/packages/eslint-plugin/reshadowDeprecated.cjs
+++ webapp/packages/eslint-plugin/reshadowDeprecated.cjs
@@ -1,4 +1,12 @@
-const rule = {
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2023 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
+module.exports = {
   meta: {
     docs: {
       description: 'Reshadow package is deprecated',
@@ -17,8 +25,4 @@ const rule = {
       },
     };
   },
-};
-
-module.exports = {
-  'no-deprecated-reshadow': rule,
 };
