a10118fc1e6d387c089d93e99117d061f03bbd54
Aleksei Potsetsuev <wrouds@gmail.com>
2024-01-09 14:47:09 +0800

    CB-4395 chore: eslint plugin catch exceptions

--- webapp/packages/eslint-plugin/noSyncComponentImport.cjs
+++ webapp/packages/eslint-plugin/noSyncComponentImport.cjs
@@ -17,44 +17,48 @@ module.exports = {
   },
   create: function (context) {
     function checkFileExtension(node) {
-      if (node.importKind === 'type' || node.exportKind === 'type') {
-        return;
-      }
-      const source = node.source;
-      // bail if the declaration doesn't have a source, e.g. "export { foo };", or if it's only partially typed like in an editor
-      if (!source || !source.value) {
-        return;
-      }
-
-      const importPathWithQueryString = source.value;
-      const importPath = importPathWithQueryString.replace(/\?(.*)$/, '');
-
-      let resolvedPath = importPath;
-
-      if (importPath.startsWith('./') || importPath.startsWith('../')) {
-        resolvedPath = path.resolve(path.dirname(context.filename), importPath);
-
-        if (path.extname(resolvedPath) === '') {
-          resolvedPath += '.tsx';
-        }
-
-        if (!fs.existsSync(resolvedPath)) {
+      try {
+        if (node.importKind === 'type' || node.exportKind === 'type') {
           return;
         }
-      } else {
-        resolvedPath = require.resolve(importPath, { paths: [path.dirname(context.filename)] });
-      }
+        const source = node.source;
+        // bail if the declaration doesn't have a source, e.g. "export { foo };", or if it's only partially typed like in an editor
+        if (!source || !source.value) {
+          return;
+        }
 
-      // get extension from resolved path, if possible.
-      // for unresolved, use source value.
-      const importExtension = path.extname(resolvedPath).substring(1);
-      const importerExtension = path.extname(context.filename).substring(1);
+        const importPathWithQueryString = source.value;
+        const importPath = importPathWithQueryString.replace(/\?(.*)$/, '');
 
-      if (importerExtension === 'ts' && importExtension === 'tsx') {
-        context.report({
-          node: source,
-          message: "Don't import/export .tsx files from .ts files directly, use React.lazy().",
-        });
+        let resolvedPath = importPath;
+
+        if (importPath.startsWith('./') || importPath.startsWith('../')) {
+          resolvedPath = path.resolve(path.dirname(context.filename), importPath);
+
+          if (path.extname(resolvedPath) === '') {
+            resolvedPath += '.tsx';
+          }
+
+          if (!fs.existsSync(resolvedPath)) {
+            return;
+          }
+        } else {
+          resolvedPath = require.resolve(importPath, { paths: [path.dirname(context.filename)] });
+        }
+
+        // get extension from resolved path, if possible.
+        // for unresolved, use source value.
+        const importExtension = path.extname(resolvedPath).substring(1);
+        const importerExtension = path.extname(context.filename).substring(1);
+
+        if (importerExtension === 'ts' && importExtension === 'tsx') {
+          context.report({
+            node: source,
+            message: "Don't import/export .tsx files from .ts files directly, use React.lazy().",
+          });
+        }
+      } catch (e) {
+        console.error('@cloudbeaver/no-sync-component-import: ', e);
       }
     }
 
