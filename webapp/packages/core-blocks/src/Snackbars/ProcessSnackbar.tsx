548eb8696c9c48e80ab5f7ba0ca89254d5832727
s.teleshev <s.teleshev@mu.se>
2024-01-18 16:36:31 +0100

    CB-3015 show process snack bar with delay fix

--- webapp/packages/core-blocks/src/Snackbars/ProcessSnackbar.tsx
+++ webapp/packages/core-blocks/src/Snackbars/ProcessSnackbar.tsx
@@ -6,6 +6,7 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
+import { useEffect, useState } from 'react';
 
 import { ENotificationType, INotificationProcessExtraProps, NotificationComponent } from '@cloudbeaver/core-events';
 
@@ -35,7 +36,14 @@ export const ProcessSnackbar: NotificationComponent<ProcessSnackbarProps> = obse
 
   const translate = useTranslate();
   const details = useErrorDetails(error);
-  const displayed = useStateDelay(notification.state.deleteDelay === 0, displayDelay);
+  const [delayState, setDelayState] = useState(false);
+  const displayed = useStateDelay(delayState, displayDelay);
+
+  useEffect(() => {
+    if (notification.state.deleteDelay === 0) {
+      setDelayState(true);
+    }
+  }, [notification.state.deleteDelay]);
 
   useActivationDelay(status === ENotificationType.Success, closeDelay, notification.close);
 
