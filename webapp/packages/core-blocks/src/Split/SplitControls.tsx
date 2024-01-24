8add2d2f6fff162d88fa07cfe272b409e0d7aad1
s.teleshev <s.teleshev@mu.se>
2024-01-25 00:29:05 +0100

    CB-4429 adds visible buttons for resizers

--- webapp/packages/core-blocks/src/Split/SplitControls.tsx
+++ webapp/packages/core-blocks/src/Split/SplitControls.tsx
@@ -11,6 +11,29 @@ import { useS } from '../useS';
 import SplitControlsStyles from './SplitControls.m.css';
 import { useSplit } from './useSplit';
 
+// TODO поправить либу.
+
+// TODO хранить/получить в стейте minSize/maxSize
+// ??? ничего не менять
+
+// рыба
+// 1. disableAutoMargin props в компонент сплит.
+// 2. если нету disableAutoMargin, то к компоненте split определяем
+// коллизию окна браузера и панели (вправо, влево, вниз, вверх)
+// если есть коллизия, то ставим minize/maxsize Splitter (component)
+
+// рыба внутри либы
+// 1. учитывать minSize/maxSize при состояниях minimize/maximize
+// и учитывать эти значения при определении состояние mode
+// 2. сделать геттер на реф дива который рендерит Split (его контейнер) - splitRef (protected)
+
+// setSize, getMainStyleSize
+// либа должна коллапсить не до 0 а до state.minSize
+// либа должна коллапсить не до 100% если maxSize > 0, то до state.maxSize
+// либа должна коллапсить не до 100% если maxSize < 0, то до calc(100% - maxSize in px)
+
+// финальная рыба
+// 1. дофиксить сплит контролс так как он не учитывает minSize/maxSize
 export const SplitControls: React.FC = function SplitControls() {
   const split = useSplit();
   const styles = useS(SplitControlsStyles);
@@ -18,6 +41,7 @@ export const SplitControls: React.FC = function SplitControls() {
   const inverse = split.state.isMainSecond();
 
   let inverseMode = split.state.mode;
+  const isResizeMode = split.state.mode === 'resize';
 
   if (split.state.size > split.state.getContainerSize()) {
     inverseMode = 'maximize';
@@ -64,12 +88,16 @@ export const SplitControls: React.FC = function SplitControls() {
       onDoubleClick={split.state.onDoubleClick}
     >
       {split.state.mode !== 'minimize' && (
-        <button className={s(styles, { button: true, primary: !inverse })} type="button" onClick={handlers.handleCollapse}>
+        <button
+          className={s(styles, { button: true, primary: !inverse, resizeButton: isResizeMode })}
+          type="button"
+          onClick={handlers.handleCollapse}
+        >
           <div className={s(styles, { ripple: true })} />
         </button>
       )}
       {split.state.mode !== 'maximize' && (
-        <button className={s(styles, { button: true, primary: inverse })} type="button" onClick={handlers.handleExpand}>
+        <button className={s(styles, { button: true, primary: inverse, resizeButton: isResizeMode })} type="button" onClick={handlers.handleExpand}>
           <div className={s(styles, { ripple: true })} />
         </button>
       )}
