3917ebb51120463df32c660a84797a960e4f4ed4
Ainur <ainur.iagudin@dbeaver.com>
2024-01-26 16:51:31 +0100

    CB-4560 move update product config to mutations

--- server/bundles/io.cloudbeaver.service.admin/src/io/cloudbeaver/service/admin/WebServiceBindingAdmin.java
+++ server/bundles/io.cloudbeaver.service.admin/src/io/cloudbeaver/service/admin/WebServiceBindingAdmin.java
@@ -182,11 +182,12 @@ public class WebServiceBindingAdmin extends WebServiceBindingBase<DBWServiceAdmi
 
             .dataFetcher("configureServer",
                 env -> getService(env).configureServer(getWebSession(env), env.getArgument("configuration")))
-            .dataFetcher("adminUpdateProductConfiguration",
-                env -> getService(env).updateProductConfiguration(getWebSession(env), env.getArgument("configuration")))
         .dataFetcher("setDefaultNavigatorSettings",
             env -> getService(env).setDefaultNavigatorSettings(getWebSession(env), WebServiceUtils.parseNavigatorSettings(env.getArgument("settings"))))
         ;
+        model.getMutationType()
+            .dataFetcher("adminUpdateProductConfiguration",
+                env -> getService(env).updateProductConfiguration(getWebSession(env), env.getArgument("configuration")));
     }
 
     @Override
