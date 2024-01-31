932b00daae81f8cf87830bb80a5e91ea9b43530e
s.teleshev <s.teleshev@mu.se>
2024-01-31 10:24:12 +0100

    CB-4429 fix: hide content if maximized pane

--- webapp/packages/core-blocks/src/Split/Pane.tsx
+++ webapp/packages/core-blocks/src/Split/Pane.tsx
@@ -16,7 +16,7 @@ import { useSplit } from './useSplit';
 export const Pane = observer<PaneProps>(function Pane({ className, children, ...rest }) {
   const styles = useS(style);
   const split = useSplit();
-  const shouldHideContent = rest.main && split.state.mode === 'minimize';
+  const shouldHideContent = (rest.main && split.state.mode === 'minimize') || (!rest.main && split.state.mode === 'maximize');
 
   return (
     <BasePane className={s(styles, { pane: true }, className)} {...rest}>
