05cba723069620b26747f7a3a92cc581cdede1d8
s.teleshev <s.teleshev@mu.se>
2024-02-06 18:28:46 +0100

    CB-4570 quota placeholder to module css

--- webapp/packages/plugin-data-viewer/src/ValuePanelPresentation/QuotaPlaceholder.tsx
+++ webapp/packages/plugin-data-viewer/src/ValuePanelPresentation/QuotaPlaceholder.tsx
@@ -6,60 +6,46 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import styled, { css } from 'reshadow';
 
-import { Link, usePermission, useTranslate } from '@cloudbeaver/core-blocks';
+import { Container, Link, s, usePermission, useS, useTranslate } from '@cloudbeaver/core-blocks';
 import { EAdminPermission } from '@cloudbeaver/core-root';
+
+import styles from './QuotaPlaceholder.m.css';
 
 interface Props {
   limit?: string;
   className?: string;
 }
 
-const style = css`
-  container {
-    margin: auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  }
-  p {
-    composes: theme-typography--body2 from global;
-    text-align: center;
-    margin: 0;
-  }
-  reason {
-    display: flex;
-    white-space: pre;
-  }
-  limit-word {
-    text-transform: lowercase;
-  }
-`;
-
 export const QuotaPlaceholder: React.FC<React.PropsWithChildren<Props>> = observer(function QuotaPlaceholder({ limit, className, children }) {
   const translate = useTranslate();
   const admin = usePermission(EAdminPermission.admin);
+  const style = useS(styles);
 
-  return styled(style)(
-    <container className={className}>
-      <p>
+  return (
+    <Container className={className} vertical center>
+      <Container center vertical>
         {translate('data_viewer_presentation_value_content_was_truncated')}
-        <reason>
-          {translate('data_viewer_presentation_value_content_truncated_placeholder') + ' '}
-          <limit-word>
+        <Container noWrap center>
+          <Container>{translate('data_viewer_presentation_value_content_truncated_placeholder')}</Container>
+          <Container className={s(style, { limitWord: true })} zeroBasis>
             {admin ? (
-              <Link title={limit} href="https://dbeaver.com/docs/cloudbeaver/Server-configuration/#resource-quotas" target="_blank" indicator>
+              <Link
+                className={s(style, { link: true })}
+                title={limit}
+                href="https://dbeaver.com/docs/cloudbeaver/Server-configuration/#resource-quotas"
+                target="_blank"
+                indicator
+              >
                 {translate('ui_limit')}
               </Link>
             ) : (
               translate('ui_limit')
             )}
-          </limit-word>
-        </reason>
-      </p>
+          </Container>
+        </Container>
+      </Container>
       {children}
-    </container>,
+    </Container>
   );
 });
