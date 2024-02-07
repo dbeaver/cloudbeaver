09e5141e37f095ad965772ad1ddf7f06cc20dc54
Serge Rider <serge@jkiss.org>
2024-02-07 14:07:55 +0300

    dbeaver/pro#2403 AWS secret controller stub (#2350)
    Co-authored-by: kseniaguzeeva <112612526+kseniaguzeeva@users.noreply.github.com>

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/app/VoidSecretController.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/app/VoidSecretController.java
@@ -21,8 +21,13 @@ import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.model.auth.SMCredentialsProvider;
 import org.jkiss.dbeaver.model.auth.SMSessionContext;
+import org.jkiss.dbeaver.model.exec.DBCFeatureNotSupportedException;
 import org.jkiss.dbeaver.model.secret.DBSSecretController;
 import org.jkiss.dbeaver.model.secret.DBSSecretControllerAuthorized;
+import org.jkiss.dbeaver.model.secret.DBSSecretObject;
+import org.jkiss.dbeaver.model.secret.DBSSecretValue;
+
+import java.util.List;
 
 /**
  * Void secret controller.
@@ -44,6 +49,12 @@ public class VoidSecretController implements DBSSecretController, DBSSecretContr
     @Override
     public void setPrivateSecretValue(@NotNull String secretId, @Nullable String secretValue) throws DBException {
         throw new DBException("Secret controller is read-only");
+    }
+
+    @NotNull
+    @Override
+    public List<DBSSecretValue> discoverCurrentUserSecrets(@NotNull DBSSecretObject secretObject) throws DBException {
+        throw new DBCFeatureNotSupportedException("Secrets discovery not supported");
     }
 
     @Override
