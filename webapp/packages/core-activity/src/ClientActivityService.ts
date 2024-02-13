74d3fd13b0770bc1f437a30abe927aa4744d635f
s.teleshev <s.teleshev@mu.se>
2024-02-13 18:07:02 +0100

    CB-4364 adds client activity service setsActivity method

--- webapp/packages/core-activity/src/ClientActivityService.ts
+++ webapp/packages/core-activity/src/ClientActivityService.ts
@@ -8,7 +8,7 @@
 import { makeObservable, observable } from 'mobx';
 
 import { injectable } from '@cloudbeaver/core-di';
-import { Executor, IExecutor, IExecutorHandler } from '@cloudbeaver/core-executor';
+import { Executor, IExecutor } from '@cloudbeaver/core-executor';
 import { SessionResource } from '@cloudbeaver/core-root';
 
 const SESSION_TOUCH_TIME_PERIOD = 1000 * 60;
@@ -16,29 +16,30 @@ const SESSION_TOUCH_TIME_PERIOD = 1000 * 60;
 @injectable()
 export class ClientActivityService {
   private timer: NodeJS.Timeout | null;
-  public isActive = false;
+  public isActive: boolean;
   public onActiveStateChange: IExecutor<boolean>;
 
   constructor(private readonly sessionResource: SessionResource) {
-    this.onActiveStateChangeHandler.bind(this);
+    this.setActivity.bind(this);
     this.updateActivity = this.updateActivity.bind(this);
     this.resetActivity = this.resetActivity.bind(this);
 
     this.timer = null;
+    this.isActive = false;
     this.onActiveStateChange = new Executor();
-    this.onActiveStateChange.addHandler(this.onActiveStateChangeHandler);
 
     makeObservable(this, {
       isActive: observable,
     });
   }
 
-  private onActiveStateChangeHandler: IExecutorHandler<boolean> = value => {
+  private setActivity = (value: boolean) => {
     this.isActive = value;
+    this.onActiveStateChange.execute(value);
   };
 
   resetActivity() {
-    this.onActiveStateChange.execute(false);
+    this.setActivity(false);
 
     if (this.timer !== null) {
       clearTimeout(this.timer);
@@ -52,7 +53,7 @@ export class ClientActivityService {
         clearTimeout(this.timer);
       }
 
-      this.onActiveStateChange.execute(true);
+      this.setActivity(true);
       this.sessionResource.touchSession();
       this.timer = setTimeout(this.resetActivity, SESSION_TOUCH_TIME_PERIOD);
     }
