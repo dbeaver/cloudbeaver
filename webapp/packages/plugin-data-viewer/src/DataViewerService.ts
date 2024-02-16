41d9f6a953c634ccafbf71d1e9367baaf28b749f
naumov <iamemptyhuh@gmail.com>
2024-02-16 14:08:20 +0100

    CB-4700 allow administrators to copy data

--- webapp/packages/plugin-data-viewer/src/DataViewerService.ts
+++ webapp/packages/plugin-data-viewer/src/DataViewerService.ts
@@ -7,16 +7,24 @@
  */
 import type { Connection } from '@cloudbeaver/core-connections';
 import { injectable } from '@cloudbeaver/core-di';
+import { EAdminPermission, SessionPermissionsResource } from '@cloudbeaver/core-root';
 
 import { DataViewerSettingsService } from './DataViewerSettingsService';
 
 @injectable()
 export class DataViewerService {
   get canCopyData() {
+    if (this.sessionPermissionsResource.has(EAdminPermission.admin)) {
+      return true;
+    }
+
     return !this.dataViewerSettingsService.settings.getValue('disableCopyData');
   }
 
-  constructor(private readonly dataViewerSettingsService: DataViewerSettingsService) {}
+  constructor(
+    private readonly dataViewerSettingsService: DataViewerSettingsService,
+    private readonly sessionPermissionsResource: SessionPermissionsResource,
+  ) {}
 
   isDataEditable(connection: Connection) {
     const disabled = this.dataViewerSettingsService.settings.getValue('disableEdit');
