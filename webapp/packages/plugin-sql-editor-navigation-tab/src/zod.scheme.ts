8dc38a1a253859871079b0630c0f66da1c6a46d1
s.teleshev <s.teleshev@mu.se>
2024-02-26 12:31:55 +0100

    CB-4634 adds license to zod.scheme

--- webapp/packages/plugin-sql-editor-navigation-tab/src/zod.scheme.ts
+++ webapp/packages/plugin-sql-editor-navigation-tab/src/zod.scheme.ts
@@ -1,3 +1,10 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2024 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
 import { schema } from '@cloudbeaver/core-utils';
 
 export const SQL_EDITOR_RESULT_TAB_SCHEME = schema.object({
