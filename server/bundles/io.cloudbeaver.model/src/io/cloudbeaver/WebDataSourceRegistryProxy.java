82750e3ed55e1d0e7ccc1f1805bdb24e9bb94cbc
Sergei Kovalchuk <serjiokov@gmail.com>
2024-02-29 11:31:39 +0100

    dbeaver/pro#2552 [TE] deleted profiles appear again after app launch
    Api fixed for store entire list of profiles

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/WebDataSourceRegistryProxy.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/WebDataSourceRegistryProxy.java
@@ -39,6 +39,7 @@ import org.jkiss.dbeaver.registry.DataSourceRegistry;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -249,6 +250,11 @@ public class WebDataSourceRegistryProxy implements DBPDataSourceRegistry, DataSo
     @Override
     public void updateAuthProfile(DBAAuthProfile profile) {
         dataSourceRegistry.updateAuthProfile(profile);
+    }
+
+    @Override
+    public void addAuthProfiles(Collection<DBAAuthProfile> profiles) {
+        dataSourceRegistry.addAuthProfiles(profiles);
     }
 
     @Override
