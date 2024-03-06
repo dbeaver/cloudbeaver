b210c7530a72fa2ba8ac690dabfb8110e9e62dba
naumov <iamemptyhuh@gmail.com>
2024-03-06 14:39:53 +0100

    CB-1084 add gql queries

--- webapp/packages/plugin-datasource-transaction-manager/src/TransactionManagerBootstrap.ts
+++ webapp/packages/plugin-datasource-transaction-manager/src/TransactionManagerBootstrap.ts
@@ -7,30 +7,73 @@
  */
 import { Bootstrap, injectable } from '@cloudbeaver/core-di';
 import { ActionService, MenuService } from '@cloudbeaver/core-view';
+import { ConnectionSchemaManagerService } from '@cloudbeaver/plugin-datasource-context-switch';
 import { MENU_APP_ACTIONS } from '@cloudbeaver/plugin-top-app-bar';
 
 import { ACTION_COMMIT } from './actions/ACTION_COMMIT';
 import { ACTION_COMMIT_MODE_TOGGLE } from './actions/ACTION_COMMIT_MODE_TOGGLE';
 import { ACTION_ROLLBACK } from './actions/ACTION_ROLLBACK';
+import { TransactionManagerService } from './TransactionManagerService';
 
 @injectable()
 export class TransactionManagerBootstrap extends Bootstrap {
-  constructor(private readonly menuService: MenuService, private readonly actionService: ActionService) {
+  constructor(
+    private readonly menuService: MenuService,
+    private readonly actionService: ActionService,
+    private readonly connectionSchemaManagerService: ConnectionSchemaManagerService,
+    private readonly transactionManagerService: TransactionManagerService,
+  ) {
     super();
   }
 
   register() {
     this.menuService.addCreator({
       menus: [MENU_APP_ACTIONS],
-      getItems: (context, items) => [...items, ACTION_COMMIT, ACTION_ROLLBACK, ACTION_COMMIT_MODE_TOGGLE],
+      isApplicable: () => !!this.connectionSchemaManagerService.currentConnection?.connected && !!this.transactionManagerService.currentContext,
+      getItems: (_, items) => [...items, ACTION_COMMIT, ACTION_ROLLBACK, ACTION_COMMIT_MODE_TOGGLE],
     });
 
     this.actionService.addHandler({
       id: 'commit-mode-base',
-      isActionApplicable: (context, action) => [ACTION_COMMIT, ACTION_ROLLBACK, ACTION_COMMIT_MODE_TOGGLE].includes(action),
-      isLabelVisible: (context, action) => action === ACTION_COMMIT || action === ACTION_ROLLBACK,
-      isHidden: (context, action) => false,
-      handler: (context, action) => {},
+      isActionApplicable: (_, action) => [ACTION_COMMIT, ACTION_ROLLBACK, ACTION_COMMIT_MODE_TOGGLE].includes(action),
+      isLabelVisible: (_, action) => action === ACTION_COMMIT || action === ACTION_ROLLBACK,
+      getActionInfo: (_, action) => {
+        if (action === ACTION_COMMIT_MODE_TOGGLE) {
+          const auto = this.transactionManagerService.autoCommitMode;
+          const icon = `/icons/commit_mode_${auto ? 'auto' : 'manual'}.png`;
+          const label = `plugin_datasource_transaction_manager_commit_mode_switch_to_${auto ? 'manual' : 'auto'}`;
+
+          return { ...action.info, icon, label, tooltip: label };
+        }
+
+        return action.info;
+      },
+      isHidden: (context, action) => {
+        if (action === ACTION_COMMIT || action === ACTION_ROLLBACK) {
+          return this.transactionManagerService.autoCommitMode;
+        }
+
+        return false;
+      },
+      handler: async (_, action) => {
+        const context = this.transactionManagerService.currentContext;
+
+        if (!context) {
+          return;
+        }
+
+        switch (action) {
+          case ACTION_COMMIT:
+            await this.transactionManagerService.commit(context.connectionId, context.id);
+            break;
+          case ACTION_ROLLBACK:
+            await this.transactionManagerService.rollback(context.connectionId, context.id);
+            break;
+          case ACTION_COMMIT_MODE_TOGGLE:
+            await this.transactionManagerService.setAutoCommit(context.connectionId, context.id, !context.autoCommit);
+            break;
+        }
+      },
     });
   }
 
--- webapp/packages/plugin-datasource-transaction-manager/src/TransactionManagerService.ts
+++ webapp/packages/plugin-datasource-transaction-manager/src/TransactionManagerService.ts
@@ -5,9 +5,119 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
+import { computed, makeAutoObservable } from 'mobx';
+
+import { ConnectionExecutionContextResource, createConnectionParam } from '@cloudbeaver/core-connections';
 import { injectable } from '@cloudbeaver/core-di';
+import { NotificationService } from '@cloudbeaver/core-events';
+import { AsyncTaskInfoService, GraphQLService } from '@cloudbeaver/core-sdk';
+import { TableViewerStorageService } from '@cloudbeaver/plugin-data-viewer';
+import { ConnectionSchemaManagerService } from '@cloudbeaver/plugin-datasource-context-switch';
+import { NavigationTabsService } from '@cloudbeaver/plugin-navigation-tabs';
+import { SqlDataSourceService } from '@cloudbeaver/plugin-sql-editor';
+
+const DEFAULT_AUTO_COMMIT = true;
 
 @injectable()
 export class TransactionManagerService {
-  constructor() {}
+  get currentContext() {
+    const tab = this.navigationTabsService.currentTab;
+
+    if (tab) {
+      const model = this.tableViewerStorageService.get(tab.handlerState.tableId);
+
+      if (model) {
+        return model.source.executionContext?.context;
+      }
+
+      const source = this.sqlDataSourceService.get(tab.handlerState.editorId);
+
+      if (source) {
+        return source.executionContext;
+      }
+    }
+
+    return null;
+  }
+
+  get autoCommitMode() {
+    const context = this.currentContext;
+
+    if (context) {
+      return context.autoCommit ?? DEFAULT_AUTO_COMMIT;
+    }
+
+    return DEFAULT_AUTO_COMMIT;
+  }
+
+  constructor(
+    private readonly notificationService: NotificationService,
+    private readonly graphQLService: GraphQLService,
+    private readonly asyncTaskInfoService: AsyncTaskInfoService,
+    private readonly navigationTabsService: NavigationTabsService,
+    private readonly tableViewerStorageService: TableViewerStorageService,
+    private readonly connectionExecutionContextResource: ConnectionExecutionContextResource,
+    private readonly connectionSchemaManagerService: ConnectionSchemaManagerService,
+    private readonly sqlDataSourceService: SqlDataSourceService,
+  ) {
+    makeAutoObservable(this, {
+      currentContext: computed,
+      autoCommitMode: computed,
+    });
+  }
+
+  async setAutoCommit(connectionId: string, contextId: string, autoCommit: boolean) {
+    const task = this.asyncTaskInfoService.create(async () => {
+      const { taskInfo } = await this.graphQLService.sdk.asyncSqlSetAutoCommit({
+        connectionId,
+        contextId,
+        autoCommit,
+      });
+
+      return taskInfo;
+    });
+
+    await this.asyncTaskInfoService.run(task);
+
+    if (this.currentContext) {
+      const param = createConnectionParam(this.currentContext.projectId, this.currentContext.connectionId);
+      await this.connectionExecutionContextResource.refreshConnectionContexts(param);
+    }
+  }
+
+  async commit(connectionId: string, contextId: string) {
+    const task = this.asyncTaskInfoService.create(async () => {
+      const { taskInfo } = await this.graphQLService.sdk.asyncSqlCommitTransaction({
+        connectionId,
+        contextId,
+      });
+
+      return taskInfo;
+    });
+
+    const result = await this.asyncTaskInfoService.run(task);
+    const connection = this.connectionSchemaManagerService.currentConnection;
+
+    if (result.taskResult) {
+      this.notificationService.logInfo({ title: connection?.name ?? result.name ?? '', message: result.taskResult });
+    }
+  }
+
+  async rollback(connectionId: string, contextId: string) {
+    const task = this.asyncTaskInfoService.create(async () => {
+      const { taskInfo } = await this.graphQLService.sdk.asyncSqlRollbackTransaction({
+        connectionId,
+        contextId,
+      });
+
+      return taskInfo;
+    });
+
+    const result = await this.asyncTaskInfoService.run(task);
+    const connection = this.connectionSchemaManagerService.currentConnection;
+
+    if (result.taskResult) {
+      this.notificationService.logInfo({ title: connection?.name ?? result.name ?? '', message: result.taskResult });
+    }
+  }
 }
