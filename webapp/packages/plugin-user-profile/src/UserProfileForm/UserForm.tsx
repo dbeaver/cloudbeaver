d0b7503f2659d1d837349e6f6fdd8d562d8ccf04
s.teleshev <s.teleshev@mu.se>
2024-03-07 11:42:49 +0100

    CB-4469 makes reshadowless user info form

--- webapp/packages/plugin-user-profile/src/UserProfileForm/UserForm.tsx
+++ webapp/packages/plugin-user-profile/src/UserProfileForm/UserForm.tsx
@@ -6,13 +6,12 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import styled from 'reshadow';
 
 import { AUTH_PROVIDER_LOCAL_ID } from '@cloudbeaver/core-authentication';
-import { Button, s, StatusMessage, useS, useStyles, useTranslate } from '@cloudbeaver/core-blocks';
+import { Button, s, SContext, StatusMessage, StyleRegistry, useS, useTranslate } from '@cloudbeaver/core-blocks';
 import { ENotificationType } from '@cloudbeaver/core-events';
 import type { UserInfo } from '@cloudbeaver/core-sdk';
-import { BASE_TAB_STYLES, TabList, TabsState, UNDERLINE_TAB_BIG_STYLES, UNDERLINE_TAB_STYLES } from '@cloudbeaver/core-ui';
+import { baseTabStyles, TabList, TabsState, underlineTabBigStyles, underlineTabStyles } from '@cloudbeaver/core-ui';
 
 import { AuthenticationPanel } from './Authentication/AuthenticationPanel';
 import { AuthenticationTab } from './Authentication/AuthenticationTab';
@@ -27,40 +26,42 @@ interface Props {
   onClose?: () => void;
 }
 
+const registry: StyleRegistry = [[baseTabStyles, { mode: 'append', styles: [underlineTabStyles, underlineTabBigStyles] }]];
+
 export const UserForm = observer<Props>(function UserForm({ user, state, onClose }) {
   const translate = useTranslate();
-  const style = [BASE_TAB_STYLES, UNDERLINE_TAB_STYLES, UNDERLINE_TAB_BIG_STYLES];
-  const styles = useStyles(style);
   const moduleStyle = useS(userFormStyles);
 
   const localProvider = user.linkedAuthProviders.includes(AUTH_PROVIDER_LOCAL_ID);
 
-  return styled(styles)(
+  return (
     <div className={s(moduleStyle, { box: true })}>
       <TabsState>
-        <div className={s(moduleStyle, { topBar: true })}>
-          <div className={s(moduleStyle, { topBarTabs: true })}>
-            <div className={s(moduleStyle, { statusMessage: true })}>
-              <StatusMessage type={ENotificationType.Info} message={state.info.statusMessage} />
+        <SContext registry={registry}>
+          <div className={s(moduleStyle, { topBar: true })}>
+            <div className={s(moduleStyle, { topBarTabs: true })}>
+              <div className={s(moduleStyle, { statusMessage: true })}>
+                <StatusMessage type={ENotificationType.Info} message={state.info.statusMessage} />
+              </div>
+              <TabList className={s(moduleStyle, { tabList: true })} aria-label="User Settings" disabled={state.info.disabled}>
+                <UserInfoTab />
+                {localProvider && <AuthenticationTab />}
+              </TabList>
             </div>
-            <TabList className={s(moduleStyle, { tabList: true })} aria-label="User Settings" style={style} disabled={state.info.disabled}>
-              <UserInfoTab />
-              {localProvider && <AuthenticationTab />}
-            </TabList>
+            <div className={s(moduleStyle, { topBarActions: true })}>
+              {onClose && (
+                <Button type="button" mod={['outlined']} onClick={onClose}>
+                  {translate('ui_processing_cancel')}
+                </Button>
+              )}
+            </div>
           </div>
-          <div className={s(moduleStyle, { topBarActions: true })}>
-            {onClose && (
-              <Button type="button" mod={['outlined']} onClick={onClose}>
-                {translate('ui_processing_cancel')}
-              </Button>
-            )}
+          <div className={s(moduleStyle, { contentBox: true })}>
+            <UserInfoPanel user={user} />
+            {localProvider && <AuthenticationPanel />}
           </div>
-        </div>
-        <div className={s(moduleStyle, { contentBox: true })}>
-          <UserInfoPanel user={user} style={style} />
-          {localProvider && <AuthenticationPanel style={style} />}
-        </div>
+        </SContext>
       </TabsState>
-    </div>,
+    </div>
   );
 });
