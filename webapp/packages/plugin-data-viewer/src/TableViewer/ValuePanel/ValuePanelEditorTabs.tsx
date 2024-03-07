493348ee10e84c18aae9822ab984d5d141e49532
s.teleshev <s.teleshev@mu.se>
2024-03-07 18:18:50 +0100

    CB-4469 removes css duplication from ValuePanelEditorTabs

--- webapp/packages/plugin-data-viewer/src/TableViewer/ValuePanel/ValuePanelEditorTabs.tsx
+++ webapp/packages/plugin-data-viewer/src/TableViewer/ValuePanel/ValuePanelEditorTabs.tsx
@@ -10,11 +10,12 @@ import { observer } from 'mobx-react-lite';
 import { SContext, StyleRegistry } from '@cloudbeaver/core-blocks';
 import { baseTabStyles, TabPanelList, tabPanelStyles, underlineTabStyles } from '@cloudbeaver/core-ui';
 
+import valuePanelStyles from './ValuePanel.m.css';
 import styles from './ValuePanelEditorTabs.m.css';
 
 const tabListPanelRegistry: StyleRegistry = [
   [baseTabStyles, { mode: 'append', styles: [underlineTabStyles, styles] }],
-  [tabPanelStyles, { mode: 'append', styles: [styles] }],
+  [tabPanelStyles, { mode: 'append', styles: [styles, valuePanelStyles] }],
 ];
 
 export const ValuePanelEditorTabs = observer(function ValuePanelEditorTabs() {
