d979db5b63b46c4c327d070dea2a4e76d417b3e3
s.teleshev <s.teleshev@mu.se>
2024-03-14 11:14:23 +0100

    CB-4469 fix: revert tab component to get back close icon for scripts/datasets in TE

--- webapp/packages/core-ui/src/Tabs/Tab/Tab.tsx
+++ webapp/packages/core-ui/src/Tabs/Tab/Tab.tsx
@@ -6,14 +6,20 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import { useMemo, useRef } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { Tab as BaseTab } from 'reakit/Tab';
 
-import { getComputed, s, useS, useTranslate } from '@cloudbeaver/core-blocks';
+import { getComputed, Icon, s, useS, useTranslate } from '@cloudbeaver/core-blocks';
+import type { IDataContext } from '@cloudbeaver/core-data-context';
+import { useMenu } from '@cloudbeaver/core-view';
 
+import { ContextMenu } from '../../ContextMenu/ContextMenu';
 import { TabContext } from '../TabContext';
-import TabStyles from './Tab.m.css';
-import { TabActions } from './TabActions';
+import type { ITabsContext } from '../TabsContext';
+import { DATA_CONTEXT_TAB_ID } from './DATA_CONTEXT_TAB_ID';
+import { DATA_CONTEXT_TABS_CONTEXT } from './DATA_CONTEXT_TABS_CONTEXT';
+import { MENU_TAB } from './MENU_TAB';
+import style from './Tab.m.css';
 import type { TabProps } from './TabProps';
 import { useTab } from './useTab';
 
@@ -24,21 +30,29 @@ export const Tab = observer<TabProps>(function Tab(props) {
   const tabContext = useMemo(() => ({ tabId }), [tabId]);
   const tab = useTab(tabId, onOpen, onClose, onClick);
   const info = tab.getInfo();
-  const styles = useS(TabStyles);
+  const styles = useS(style);
   const canClose = getComputed(() => !!onClose || (tab.closable && tab.state.closable));
+
+  const menu = useMenu({
+    menu: MENU_TAB,
+    context: props.menuContext,
+  });
+
+  menu.context.set(DATA_CONTEXT_TABS_CONTEXT, tab.state);
+  menu.context.set(DATA_CONTEXT_TAB_ID, tabId);
 
   return (
     <TabContext.Provider value={tabContext}>
       <div className={s(styles, { tabOuter: true })}>
         <div className={s(styles, { tabInner: true, tabInnerSelected: tab.selected })}>
-          <TabActions
-            className={s(styles, { tabActions: true })}
-            state={tab.state}
-            tabId={tabId}
-            handleClose={tab.handleClose}
-            menuContext={props.menuContext}
-            canClose={canClose}
-          />
+          <div className={s(styles, { tabActions: true }, className)}>
+            {canClose && (
+              <div className={s(styles, { tabAction: true })} title={translate('ui_close')} onClick={tab.handleClose}>
+                <Icon className={s(styles, { icon: true })} name="cross-bold" viewBox="0 0 7 8" />
+              </div>
+            )}
+            <TabMenu tabId={tabId} state={tab.state} menuContext={props.menuContext} />
+          </div>
           <BaseTab
             ref={ref}
             {...tab.state.state}
@@ -54,5 +68,40 @@ export const Tab = observer<TabProps>(function Tab(props) {
         </div>
       </div>
     </TabContext.Provider>
+  );
+});
+
+interface TabMenuProps {
+  tabId: string;
+  state: ITabsContext<any>;
+  menuContext?: IDataContext;
+}
+
+const TabMenu = observer<TabMenuProps>(function TabMenu({ tabId, state, menuContext }) {
+  const styles = useS(style);
+
+  const [menuOpened, switchState] = useState(false);
+  const menu = useMenu({
+    menu: MENU_TAB,
+    context: menuContext,
+  });
+
+  menu.context.set(DATA_CONTEXT_TABS_CONTEXT, state);
+  menu.context.set(DATA_CONTEXT_TAB_ID, tabId);
+
+  const hidden = getComputed(() => !menu.items.length || menu.items.every(item => item.hidden));
+
+  if (hidden) {
+    return null;
+  }
+
+  return (
+    <div className={s(styles, { portal: true, portalMenuOpened: menuOpened })}>
+      <ContextMenu menu={menu} placement="bottom-start" modal disclosure onVisibleSwitch={switchState}>
+        <div className={s(styles, { tabAction: true })}>
+          <Icon className={s(styles, { icon: true })} name="dots" viewBox="0 0 32 32" />
+        </div>
+      </ContextMenu>
+    </div>
   );
 });
