60605e0e25aef9562b6cb460203e190d6558fcfd
Aleksei Potsetsuev <wrouds@gmail.com>
2024-03-18 20:29:37 +0800

    CB-4222 fix: hide hex and base64 text presentation for non-binary data

--- webapp/packages/plugin-data-viewer/src/ValuePanelPresentation/TextValue/TextValuePresentationBootstrap.ts
+++ webapp/packages/plugin-data-viewer/src/ValuePanelPresentation/TextValue/TextValuePresentationBootstrap.ts
@@ -70,14 +70,14 @@ export class TextValuePresentationBootstrap extends Bootstrap {
       name: 'data_viewer_presentation_value_text_hex_title',
       order: Number.MAX_SAFE_INTEGER,
       panel: () => React.Fragment,
-      // isHidden: (_, context) => !isBlobPresentationAvailable(context),
+      isHidden: (_, context) => !isBlobPresentationAvailable(context),
     });
     this.textValuePresentationService.add({
       key: 'application/octet-stream;type=base64',
       name: 'data_viewer_presentation_value_text_base64_title',
       order: Number.MAX_SAFE_INTEGER,
       panel: () => React.Fragment,
-      // isHidden: (_, context) => !isBlobPresentationAvailable(context),
+      isHidden: (_, context) => !isBlobPresentationAvailable(context),
     });
   }
 
