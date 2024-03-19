a7c729e7986bce7fc2fa3d74bebf05cf7f5ea048
s.teleshev <s.teleshev@mu.se>
2024-03-19 17:20:51 +0100

    CB-4654 removes reshadow from plugin-authentication

--- webapp/packages/plugin-authentication/src/Dialog/AuthProviderForm/ConfigurationsList.tsx
+++ webapp/packages/plugin-authentication/src/Dialog/AuthProviderForm/ConfigurationsList.tsx
@@ -7,7 +7,6 @@
  */
 import { observer } from 'mobx-react-lite';
 import { useState } from 'react';
-import styled, { css } from 'reshadow';
 
 import { AuthProvider, AuthProviderConfiguration, AuthProvidersResource, comparePublicAuthConfigurations } from '@cloudbeaver/core-authentication';
 import {
@@ -18,10 +17,11 @@ import {
   IconOrImage,
   Link,
   Loader,
+  s,
   TextPlaceholder,
   Translate,
   usePromiseState,
-  useStyles,
+  useS,
   useTranslate,
 } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
@@ -30,33 +30,7 @@ import type { UserInfo } from '@cloudbeaver/core-sdk';
 import { ServerConfigurationAdministrationNavService } from '@cloudbeaver/plugin-administration';
 
 import { AuthenticationService } from '../../AuthenticationService';
-
-const styles = css`
-  container {
-    display: flex;
-    flex-direction: column;
-    overflow: auto;
-    flex: 1;
-  }
-  Filter {
-    margin: 0 24px 12px 24px;
-  }
-  list {
-    overflow: auto;
-  }
-  Cell {
-    composes: theme-border-color-secondary from global;
-    border-bottom: 1px solid;
-    padding: 0 16px;
-  }
-  IconOrImage {
-    width: 100%;
-    height: 100%;
-  }
-  center {
-    margin: auto;
-  }
-`;
+import styles from './ConfigurationsList.m.css';
 
 interface IProviderConfiguration {
   provider: AuthProvider;
@@ -85,7 +59,7 @@ export const ConfigurationsList = observer<Props>(function ConfigurationsList({
   const serverConfigurationAdministrationNavService = useService(ServerConfigurationAdministrationNavService);
   const authenticationService = useService(AuthenticationService);
   const translate = useTranslate();
-  const style = useStyles(styles);
+  const style = useS(styles);
 
   const [search, setSearch] = useState('');
   const authTaskState = usePromiseState(authTask);
@@ -132,10 +106,10 @@ export const ConfigurationsList = observer<Props>(function ConfigurationsList({
   }
 
   if (activeProvider && activeConfiguration) {
-    return styled(style)(
-      <container className={className}>
+    return (
+      <div className={s(style, { container: true }, className)}>
         <Loader state={authTaskState} message="authentication_authorizing" hideException>
-          <center>
+          <div className={s(style, { center: true })}>
             {providerDisabled ? (
               <TextPlaceholder>
                 {translate('plugin_authentication_authentication_method_disabled')}
@@ -146,31 +120,41 @@ export const ConfigurationsList = observer<Props>(function ConfigurationsList({
                 <Translate token="authentication_login" />
               </Button>
             )}
-          </center>
+          </div>
         </Loader>
-      </container>,
+      </div>
     );
   }
 
-  return styled(style)(
-    <container className={className}>
+  return (
+    <container className={s(style, { container: true }, className)}>
       {configurations.length >= 10 && (
-        <Filter placeholder={translate('authentication_identity_provider_search_placeholder')} value={search} max onChange={setSearch} />
+        <Filter
+          className={s(style, { filter: true })}
+          placeholder={translate('authentication_identity_provider_search_placeholder')}
+          value={search}
+          max
+          onChange={setSearch}
+        />
       )}
-      <list>
+      <div className={s(style, { list: true })}>
         {filteredConfigurations.map(({ provider, configuration }) => {
           const icon = configuration.iconURL || provider.icon;
           const title = `${configuration.displayName}\n${configuration.description || ''}`;
           return (
             <Link key={configuration.id} title={title} wrapper onClick={() => login(false, provider, configuration)}>
-              <Cell before={icon ? <IconOrImage icon={icon} /> : undefined} description={configuration.description}>
+              <Cell
+                className={s(style, { cell: true })}
+                before={icon ? <IconOrImage className={s(style, { iconOrImage: true })} icon={icon} /> : undefined}
+                description={configuration.description}
+              >
                 {configuration.displayName}
               </Cell>
             </Link>
           );
         })}
-      </list>
+      </div>
       <Loader state={authTaskState} message="authentication_authorizing" overlay hideException />
-    </container>,
+    </container>
   );
 });
