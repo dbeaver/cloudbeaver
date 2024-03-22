772bb89d6a8f858bc6a495c1c60858281bf6adb0
s.teleshev <s.teleshev@mu.se>
2024-03-22 23:43:55 +0100

    adds husky with license script

---
+++ scripts/license.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+add_missed_license() {
+    local TEXT="$1"
+    local FILES_PATH="$2"
+    TEXT=$(echo "$TEXT" | tr '\n' '\0' | xargs -0 -n1)
+    FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E "$FILES_PATH")
+
+    for FILE in $FILES; do
+        if ! grep -Fxq "$TEXT" "$FILE"; then
+            echo "$TEXT\n\n$(cat "$FILE")" > "$FILE"
+            git add "$FILE"
+        fi
+    done
+}
