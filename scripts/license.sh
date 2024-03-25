0880eca49e137cea3f154fcefed9b5698bf676b1
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-03-25 16:17:31 +0100

    license: notify user for updated files with new license header

--- scripts/license.sh
+++ scripts/license.sh
@@ -4,13 +4,18 @@ add_missed_license() {
     local TEXT="$1"
     local FILES_PATH="$2"
     TEXT=$(echo "$TEXT" | tr '\n' '\0' | xargs -0 -n1)
-    FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E "$FILES_PATH" || true)
-
+    FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep -E "$FILES_PATH" || true)
+    HAS_FILES_TO_EDIT=false
 
     for FILE in $FILES; do
         if ! grep -Fxq "$TEXT" "$FILE"; then
+            HAS_FILES_TO_EDIT=true
             echo "$TEXT\n\n$(cat "$FILE")" > "$FILE"
-            git add "$FILE"
         fi
     done
+
+    if [ "$HAS_FILES_TO_EDIT" = true ]; then
+        echo "Found files without license header. Please check the git Changes wether the license headers were added correctly:"
+        exit 1
+    fi
 }
