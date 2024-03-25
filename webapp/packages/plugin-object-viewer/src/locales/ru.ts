1fd1ebeecf605cec3f3f24de57f916e1ef2578e3
alex <48489896+devnaumov@users.noreply.github.com>
2024-03-25 16:07:04 +0100

    CB-4074 transform metadata editor footer to the new context menu (#2490)
    * CB-4074 transform metadata editor footer to the new context menu
    
    * CB-4074 use existing data context for nodes
    
    ---------
    
    Co-authored-by: mr-anton-t <42037741+mr-anton-t@users.noreply.github.com>

--- webapp/packages/plugin-object-viewer/src/locales/ru.ts
+++ webapp/packages/plugin-object-viewer/src/locales/ru.ts
@@ -4,4 +4,5 @@ export default [
   ['plugin_object_viewer_table_name', 'Название'],
   ['plugin_object_viewer_table_no_items', 'Нет объектов для показа'],
   ['plugin_object_viewer_error', 'Произошла ошибка при загрузке вкладки'],
+  ['plugin_object_viewer_delete_object_fail', 'Не удалось удалить объект'],
 ];
