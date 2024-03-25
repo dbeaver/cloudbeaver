d2394c4da11922e58f687e134c1c37a1c0e9d5f4
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-03-25 12:47:23 +0100

    fix: license can pass file not fitted to provided FILES_PATH pattern

--- scripts/license.sh
+++ scripts/license.sh
@@ -4,7 +4,8 @@ add_missed_license() {
     local TEXT="$1"
     local FILES_PATH="$2"
     TEXT=$(echo "$TEXT" | tr '\n' '\0' | xargs -0 -n1)
-    FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E "$FILES_PATH")
+    FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E "$FILES_PATH" || true)
+
 
     for FILE in $FILES; do
         if ! grep -Fxq "$TEXT" "$FILE"; then
