f7fe0a607452d16e57b8c13766218835de230595
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-03-25 23:30:21 +0100

    adds check for unstaged files with license

--- webapp/scripts/license.sh
+++ webapp/scripts/license.sh
@@ -4,18 +4,26 @@ add_missed_license() {
     local TEXT="$1"
     local FILES_PATH="$2"
     TEXT=$(echo "$TEXT" | tr '\n' '\0' | xargs -0 -n1)
-    FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep -E "$FILES_PATH" || true)
-    HAS_FILES_TO_EDIT=false
+    STAGED_FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep -E "$FILES_PATH" || true)
+    UNSTAGED_FILES=$(git diff --name-only --diff-filter=ACMR | grep -E "$FILES_PATH" || true)
+    HAS_FILES_WITHOUT_LICENSE=false
+    HAS_FILES_WITH_LICENSE_TO_STAGE=false
 
-    for FILE in $FILES; do
+    for FILE in $STAGED_FILES; do
         if ! grep -Fxq "$TEXT" "$FILE"; then
-            HAS_FILES_TO_EDIT=true
-            echo "$TEXT\n\n$(cat "$FILE")" > "$FILE"
+            HAS_FILES_WITHOUT_LICENSE=true
+            echo "$TEXT\n$(cat "$FILE")" > "$FILE"
         fi
     done
 
-    if [ "$HAS_FILES_TO_EDIT" = true ]; then
-        echo "Found files without license header. Please check the git Changes wether the license headers were added correctly:"
+    for FILE in $UNSTAGED_FILES; do
+        if grep -Fxq "$TEXT" "$FILE"; then
+            HAS_FILES_WITH_LICENSE_TO_STAGE=true
+        fi
+    done
+
+    if [ "$HAS_FILES_WITHOUT_LICENSE" = true ] || [ "$HAS_FILES_WITH_LICENSE_TO_STAGE" = true ]; then
+        echo "Found files without license header. Please check the git Changes wether the license headers were added correctly"
         exit 1
     fi
 }
