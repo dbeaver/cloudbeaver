b55a999ef69bb60095576a0499fbcb681842ca76
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-03-25 23:39:45 +0100

    change pre commit to pre push

--- webapp/scripts/license.sh
+++ webapp/scripts/license.sh
@@ -5,9 +5,7 @@ add_missed_license() {
     local FILES_PATH="$2"
     TEXT=$(echo "$TEXT" | tr '\n' '\0' | xargs -0 -n1)
     STAGED_FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep -E "$FILES_PATH" || true)
-    UNSTAGED_FILES=$(git diff --name-only --diff-filter=ACMR | grep -E "$FILES_PATH" || true)
     HAS_FILES_WITHOUT_LICENSE=false
-    HAS_FILES_WITH_LICENSE_TO_STAGE=false
 
     for FILE in $STAGED_FILES; do
         if ! grep -Fxq "$TEXT" "$FILE"; then
@@ -16,13 +14,7 @@ add_missed_license() {
         fi
     done
 
-    for FILE in $UNSTAGED_FILES; do
-        if grep -Fxq "$TEXT" "$FILE"; then
-            HAS_FILES_WITH_LICENSE_TO_STAGE=true
-        fi
-    done
-
-    if [ "$HAS_FILES_WITHOUT_LICENSE" = true ] || [ "$HAS_FILES_WITH_LICENSE_TO_STAGE" = true ]; then
+    if [ "$HAS_FILES_WITHOUT_LICENSE" = true ]; then
         echo "Found files without license header. Please check the git Changes wether the license headers were added correctly"
         exit 1
     fi
