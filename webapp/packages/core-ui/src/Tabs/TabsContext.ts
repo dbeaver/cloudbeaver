cbeec5d4a7b9f1a88712fc32ecd1f4ce2982ef42
Aleksei Potsetsuev <wrouds@gmail.com>
2024-03-27 21:36:07 +0800

    CB-4767 feat: support for schema for useTabLocalState

--- webapp/packages/core-ui/src/Tabs/TabsContext.ts
+++ webapp/packages/core-ui/src/Tabs/TabsContext.ts
@@ -10,7 +10,7 @@ import type { TabStateReturn } from 'reakit/Tab';
 
 import type { IDataContext } from '@cloudbeaver/core-data-context';
 import type { IExecutor } from '@cloudbeaver/core-executor';
-import type { MetadataMap, MetadataValueGetter } from '@cloudbeaver/core-utils';
+import type { MetadataMap, MetadataValueGetter, schema } from '@cloudbeaver/core-utils';
 
 import type { ITabData, ITabInfo, ITabsContainer } from './TabsContainer/ITabsContainer';
 
@@ -30,8 +30,8 @@ export interface ITabsContext<T = Record<string, any>> {
   context: IDataContext;
   canClose: (tabId: string) => boolean;
   getTabInfo: (tabId: string) => ITabInfo<T> | undefined;
-  getTabState: <T>(tabId: string, valueGetter?: MetadataValueGetter<string, T>) => T;
-  getLocalState: <T>(tabId: string, valueGetter?: MetadataValueGetter<string, T>) => T;
+  getTabState: <T>(tabId: string, valueGetter?: MetadataValueGetter<string, T>, schema?: schema.AnyZodObject) => T;
+  getLocalState: <T>(tabId: string, valueGetter?: MetadataValueGetter<string, T>, schema?: schema.AnyZodObject) => T;
   open: (tabId: string) => Promise<void>;
   close: (tabId: string) => Promise<void>;
   closeAll: () => Promise<void>;
