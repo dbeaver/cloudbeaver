d798e63845f696c4b77635558aa5cb7f7207ec79
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-04-08 17:02:58 +0200

    CB-4911 validate missed license

--- webapp/scripts/license.sh
+++ webapp/scripts/license.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-add_missed_license() {
+validate_missed_license() {
     local TEXT="$1"
     local FILES_PATH="$2"
     TEXT=$(echo "$TEXT" | tr '\n' '\0' | xargs -0 -n1)
@@ -10,12 +10,12 @@ add_missed_license() {
     for FILE in $STAGED_FILES; do
         if ! grep -Fxq "$TEXT" "$FILE"; then
             HAS_FILES_WITHOUT_LICENSE=true
-            echo "$TEXT\n$(cat "$FILE")" > "$FILE"
+            git restore --staged "$FILE"
         fi
     done
 
     if [ "$HAS_FILES_WITHOUT_LICENSE" = true ]; then
-        echo "Found files without license header. Please check the git Changes wether the license headers were added correctly"
+        echo "Found files without license header. Please add license to all unstaged files."
         exit 1
     fi
 }
