c4cded59ca0abd70c6703cb1f1cc45278dd45bd3
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2024-04-12 18:03:01 +0400

    CB-4629 anonymous preferences

--- server/bundles/io.cloudbeaver.service.auth/src/io/cloudbeaver/service/auth/impl/WebServiceAuthImpl.java
+++ server/bundles/io.cloudbeaver.service.auth/src/io/cloudbeaver/service/auth/impl/WebServiceAuthImpl.java
@@ -264,7 +264,9 @@ public class WebServiceAuthImpl implements DBWServiceAuth {
         webSession.addInfoMessage("Set user parameter - " + name);
         try {
             String serializedValue = value == null ? null : value.toString();
-            webSession.getSecurityController().setCurrentUserParameter(name, serializedValue);
+            if (webSession.getUser() != null) {
+                webSession.getSecurityController().setCurrentUserParameter(name, serializedValue);
+            }
             var params = new HashMap<String, Object>();
             params.put(name, value);
             webSession.getUserContext().getPreferenceStore().updatePreferenceValues(params);
@@ -283,7 +285,9 @@ public class WebServiceAuthImpl implements DBWServiceAuth {
             throw new DBWebException("Preferences cannot be changed for anonymous user");
         }
         try {
-            webSession.getSecurityController().setCurrentUserParameters(parameters);
+            if (webSession.getUser() != null) {
+                webSession.getSecurityController().setCurrentUserParameters(parameters);
+            }
             webSession.getUserContext().getPreferenceStore().updatePreferenceValues(parameters);
             return new WebUserInfo(webSession, webSession.getUser());
         } catch (DBException e) {
