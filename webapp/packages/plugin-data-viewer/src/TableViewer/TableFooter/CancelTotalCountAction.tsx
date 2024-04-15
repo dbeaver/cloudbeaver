815cd26c6a26d63a055fafed3c4887e8d60dd4e6
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-04-15 16:26:03 +0200

    CB-4270 cleanup

--- webapp/packages/plugin-data-viewer/src/TableViewer/TableFooter/CancelTotalCountAction.tsx
+++ webapp/packages/plugin-data-viewer/src/TableViewer/TableFooter/CancelTotalCountAction.tsx
@@ -6,12 +6,10 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import styled from 'reshadow';
 
 import { Container, IconButton, useTranslate } from '@cloudbeaver/core-blocks';
 
 import styles from './CancelTotalCountAction.m.css';
-import { tableFooterMenuStyles } from './TableFooterMenu/TableFooterMenuItem';
 
 interface Props {
   onClick: VoidFunction;
@@ -20,10 +18,10 @@ interface Props {
 export const CancelTotalCountAction = observer(function CancelTotalCountAction({ onClick }: Props) {
   const translate = useTranslate();
 
-  return styled(tableFooterMenuStyles)(
+  return (
     <Container className={styles.action} title={translate('ui_processing_cancel')} noWrap center zeroBasis keepSize onClick={onClick}>
       <IconButton className={styles.icon} name="cross" title={translate('ui_processing_cancel')} viewBox="0 0 32 32" />
       <span className={styles.cancelText}>{translate('ui_processing_cancel')}</span>
-    </Container>,
+    </Container>
   );
 });
