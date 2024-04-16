07021633c64873eb8de6d66c09cc5adfcea1ca3b
Aleksei Potsetsuev <wrouds@gmail.com>
2024-04-16 20:49:36 +0800

    CB-4629 fix: tab focusing

--- webapp/packages/core-ui/src/Tabs/TabsState.tsx
+++ webapp/packages/core-ui/src/Tabs/TabsState.tsx
@@ -73,7 +73,7 @@ export const TabsState = observer(function TabsState<T = Record<string, any>>({
   const [openExecutor] = useState(() => new Executor<ITabData<T>>());
 
   const state = useTabState({
-    selectedId: selectedId || currentTabId || container?.selectedId,
+    selectedId: selectedId || currentTabId || container?.selectedId || null,
     orientation,
     manual,
   });
