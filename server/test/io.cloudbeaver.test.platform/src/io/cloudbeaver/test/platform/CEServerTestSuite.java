6c0efebded9945bbfc3b5ab0cd039d6b6b7d5def
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2024-05-03 18:32:56 +0400

    CB-5050 query translator unit tests

--- server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/CEServerTestSuite.java
+++ server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/CEServerTestSuite.java
@@ -35,6 +35,7 @@ import java.nio.file.Path;
 @Suite.SuiteClasses(
     {
         ConnectionsTest.class,
+        SQLQueryTranslatorTest.class,
         AuthenticationTest.class,
         ResourceManagerTest.class,
         RMLockTest.class,
