f5f685ee8627029082490b9d8895005e9c0329eb
Ainur <ainur.iagudin@dbeaver.com>
2024-05-08 18:05:18 +0200

    CB-5109 add beta features config param

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/app/BaseWebAppConfiguration.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/app/BaseWebAppConfiguration.java
@@ -33,11 +33,14 @@ public abstract class BaseWebAppConfiguration implements WebAppConfiguration {
     protected boolean resourceManagerEnabled;
     protected boolean showReadOnlyConnectionInfo;
     protected String[] enabledFeatures;
+    protected String[] betaFeatures;
+
 
     public BaseWebAppConfiguration() {
         this.plugins = new LinkedHashMap<>();
         this.resourceManagerEnabled = true;
         this.enabledFeatures = null;
+        this.betaFeatures = new String[0];
         this.showReadOnlyConnectionInfo = false;
     }
 
@@ -46,6 +49,7 @@ public abstract class BaseWebAppConfiguration implements WebAppConfiguration {
         this.defaultUserTeam = src.defaultUserTeam;
         this.resourceManagerEnabled = src.resourceManagerEnabled;
         this.enabledFeatures = src.enabledFeatures;
+        this.betaFeatures = src.betaFeatures;
         this.showReadOnlyConnectionInfo = src.showReadOnlyConnectionInfo;
     }
 
@@ -103,5 +107,9 @@ public abstract class BaseWebAppConfiguration implements WebAppConfiguration {
 
     public boolean isShowReadOnlyConnectionInfo() {
         return showReadOnlyConnectionInfo;
+    }
+
+    public String[] getBetaFeatures() {
+        return betaFeatures;
     }
 }
