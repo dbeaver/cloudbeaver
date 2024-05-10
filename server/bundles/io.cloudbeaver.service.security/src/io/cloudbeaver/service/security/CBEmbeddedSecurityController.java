7e9cca4d7b7d74a6d4bffbbff274e027a1473669
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2024-05-10 15:40:49 +0400

    CB-4460 fix unexpected team role modification

--- server/bundles/io.cloudbeaver.service.security/src/io/cloudbeaver/service/security/CBEmbeddedSecurityController.java
+++ server/bundles/io.cloudbeaver.service.security/src/io/cloudbeaver/service/security/CBEmbeddedSecurityController.java
@@ -251,23 +251,43 @@ public class CBEmbeddedSecurityController<T extends WebAuthApplication>
         }
     }
 
-    public void setUserTeams(@NotNull Connection dbCon, String userId, String[] teamIds, String grantorId)
+    //TODO implement add/delete user teams api
+    protected void setUserTeams(@NotNull Connection dbCon, String userId, String[] teamIds, String grantorId)
         throws SQLException {
-        JDBCUtils.executeStatement(
-            dbCon,
-            database.normalizeTableNames("DELETE FROM {table_prefix}CB_USER_TEAM WHERE USER_ID=?"),
-            userId
-        );
+
+        String deleteUserTeamsSql = "DELETE FROM {table_prefix}CB_USER_TEAM WHERE USER_ID=?";
+
+        if (!ArrayUtils.isEmpty(teamIds)) {
+            deleteUserTeamsSql =
+                deleteUserTeamsSql + " AND TEAM_ID NOT IN (" + SQLUtils.generateParamList(teamIds.length) + ")";
+        }
+        try (PreparedStatement dbStat = dbCon.prepareStatement(database.normalizeTableNames(deleteUserTeamsSql))) {
+            int index = 1;
+            dbStat.setString(index++, userId);
+            for (String teamId : teamIds) {
+                dbStat.setString(index++, teamId);
+            }
+        }
+
         String defaultUserTeam = getDefaultUserTeam();
         if (CommonUtils.isNotEmpty(defaultUserTeam) && !ArrayUtils.contains(teamIds, defaultUserTeam)) {
             teamIds = ArrayUtils.add(String.class, teamIds, defaultUserTeam);
         }
         if (!ArrayUtils.isEmpty(teamIds)) {
+            Set<String> currentUserTeams = new HashSet<>(JDBCUtils.queryStrings(
+                dbCon,
+                "SELECT TEAM_ID FROM {table_prefix}CB_USER_TEAM WHERE USER_ID=?",
+                userId
+            ));
+
             try (PreparedStatement dbStat = dbCon.prepareStatement(
                 database.normalizeTableNames("INSERT INTO {table_prefix}CB_USER_TEAM" +
                     "(USER_ID,TEAM_ID,GRANT_TIME,GRANTED_BY) VALUES(?,?,?,?)"))
             ) {
                 for (String teamId : teamIds) {
+                    if (currentUserTeams.contains(teamId)) {
+                        continue;
+                    }
                     dbStat.setString(1, userId);
                     dbStat.setString(2, teamId);
                     dbStat.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
