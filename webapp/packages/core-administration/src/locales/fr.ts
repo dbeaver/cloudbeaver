95a8d3619e52dc2ce62a5fe80daee86e1a5cf461
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-24 10:00:15 +0200

    Added French translation file for core-administration (fr.ts)
    I have added the fr.ts file to the cloudbeaver/webapp/packages/core-administration/src/locales/ directory. This file contains the French translations for various administration-related strings used in the application. The translations include menu entries, configuration wizard titles, and success messages.

---
+++ webapp/packages/core-administration/src/locales/fr.ts
@@ -0,0 +1,7 @@
+export default [
+  ['administration_menu_enter', 'Administration'],
+  ['administration_menu_back', 'Retour à l\'application'],
+  ['administration_configuration_wizard_title', 'Configuration initiale du serveur'],
+  ['administration_configuration_wizard_finish_success_title', 'Serveur configuré'],
+  ['administration_configuration_wizard_finish_success_message', 'Vous pouvez vous connecter en tant qu\'administrateur pour configurer des paramètres supplémentaires.'],
+];
