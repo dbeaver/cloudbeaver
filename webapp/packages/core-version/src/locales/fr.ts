22f74ebc584cd8206d63e5c5183a25785634a86b
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-24 15:13:11 +0200

    Added French translation file for core-version (fr.ts)
    I have added the fr.ts file to the cloudbeaver/webapp/packages/core-version/src/locales/ directory. This file contains the French translations for various version-related strings used in the application, including version information, release dates, tags, and error messages for loading version information.

---
+++ webapp/packages/core-version/src/locales/fr.ts
@@ -0,0 +1,9 @@
+export default [
+  ['version', 'Version'],
+  ['versions', 'Versions'],
+  ['version_date', 'Date de sortie'],
+  ['version_tag', 'Tag'],
+  ['version_latest', 'Dernière'],
+  ['version_current', 'Actuelle'],
+  ['versions_load_fail', 'Impossible d\'obtenir les informations sur la dernière version de l\'application'],
+];
