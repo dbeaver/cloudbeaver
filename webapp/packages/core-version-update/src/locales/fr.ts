61487a2cce9a0112699ffc46df4390e4144c47b6
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-24 15:11:47 +0200

    Added French translation file for core-version-update (fr.ts)
    I have added the fr.ts file to the cloudbeaver/webapp/packages/core-version-update/src/locales/ directory. This file contains the French translations for various version update-related messages used in the application, including messages about version status, refresh actions, and update instructions.

---
+++ webapp/packages/core-version-update/src/locales/fr.ts
@@ -0,0 +1,10 @@
+export default [
+  ['version_update', 'Mise à jour de version'],
+  ['version_update_version_is_up_to_date', 'La version est à jour'],
+  ['version_update_new_version_available', 'Une nouvelle version est disponible'],
+  ['version_update_version_no_info', 'Aucune information de version disponible'],
+  ['version_update_versions_refresh_successful', 'Liste des versions rafraîchie avec succès'],
+  ['version_update_versions_refresh_fail', 'Échec du rafraîchissement de la liste des versions'],
+  ['version_update_how_to_update', 'Comment mettre à jour'],
+  ['version_update_info', 'Info'],
+];
