89d841afd14d402b46f4b03a487384eb5303dddd
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-24 09:57:42 +0200

    Added French translation file (fr.ts)
    I have added the fr.ts file to the cloudbeaver/webapp/packages/core-app/src/locales/ directory. This file contains the French translations for various strings used in the application. The translations include settings, theme, language, dialog titles and messages, as well as labels for SQL generation.

---
+++ webapp/packages/core-app/src/locales/fr.ts
@@ -0,0 +1,11 @@
+export default [
+  ['app_shared_settingsMenu_config', 'Configuration'],
+  ['app_shared_settingsMenu_theme', 'Thème'],
+  ['app_shared_settingsMenu_lang', 'Langue'],
+  ['app_shared_inlineEditor_dialog_title', 'Mode Edition'],
+  ['app_shared_inlineEditor_dialog_apply', 'Appliquer'],
+  ['app_shared_inlineEditor_dialog_cancel', 'Annuler'],
+  ['app_shared_navigationTabsBar_placeholder', 'Il n’y a aucun objet à afficher. Double-cliquez sur un objet dans l’arborescence de navigation pour l’ouvrir.'],
+  ['app_shared_sql_generators_panel_title', 'Générer du SQL'],
+  ['app_shared_sql_generators_dialog_title', 'SQL Généré'],
+];
