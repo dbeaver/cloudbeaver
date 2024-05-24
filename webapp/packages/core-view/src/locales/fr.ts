e9a8c77fc7ce3f97c72d904d302e11d36d589012
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-24 15:17:09 +0200

    Added French translation file for core-view (fr.ts)
    I have added the fr.ts file to the cloudbeaver/webapp/packages/core-view/src/locales/ directory. This file contains the French translations for various view-related strings used in the application, including layout settings, zoom actions, and labels for creating and editing items and folders.

---
+++ webapp/packages/core-view/src/locales/fr.ts
@@ -0,0 +1,8 @@
+export default [
+  ['core_view_layout', 'Disposition'],
+  ['core_view_zoom_in', 'Agrandir'],
+  ['core_view_zoom_out', 'Réduire'],
+  ['core_view_action_create_label', 'Créer'],
+  ['core_view_action_new_folder', 'Nouveau dossier'],
+  ['core_view_action_edit_label', 'Modifier'],
+];
