74da82852ca7079d3f58a0fbbef28cd4f8669f8a
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-24 10:09:03 +0200

    Added French translation file for core-authentication (fr.ts)
    I have added the fr.ts file to the cloudbeaver/webapp/packages/core-authentication/src/locales/ directory. This file contains the French translations for various authentication-related strings used in the application. The translations include settings group labels, options for disabling anonymous access, and password policy requirements such as minimum length, case sensitivity, digits, and special characters.

---
+++ webapp/packages/core-authentication/src/locales/fr.ts
@@ -0,0 +1,10 @@
+export default [
+  ['core_authentication_auth_settings_group', 'Authentification'],
+  ['settings_authentication_disable_anonymous_access_name', 'Désactiver l\'accès anonyme'],
+  ['settings_authentication_disable_anonymous_access_description', 'Désactiver la fonction d\'accès anonyme'],
+
+  ['core_authentication_password_policy_min_length', 'Le mot de passe doit contenir au moins {arg:min} caractères'],
+  ['core_authentication_password_policy_upper_lower_case', 'Le mot de passe doit contenir des lettres majuscules et minuscules'],
+  ['core_authentication_password_policy_min_digits', 'Le mot de passe doit contenir au moins {arg:min} chiffres'],
+  ['core_authentication_password_policy_min_special_characters', 'Le mot de passe doit contenir au moins {arg:min} caractères spéciaux'],
+];
