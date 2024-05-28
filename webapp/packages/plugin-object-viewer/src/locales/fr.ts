040c792bcdb66dff4f69f9f44dce80b2466cc468
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 15:42:24 +0200

    Added French translation to plugin-object-viewer (fr.ts)
    Added French translations for the object viewer plugin messages.

---
+++ webapp/packages/plugin-object-viewer/src/locales/fr.ts
@@ -0,0 +1,8 @@
+export default [
+  ['plugin_object_viewer_properties_tab', 'Propriétés'],
+  ['plugin_object_viewer_object_info_tab', 'Infos'],
+  ['plugin_object_viewer_table_name', 'Nom'],
+  ['plugin_object_viewer_table_no_items', 'Il n\'y a aucun élément à afficher'],
+  ['plugin_object_viewer_error', 'Une erreur s\'est produite lors du chargement de l\'onglet'],
+  ['plugin_object_viewer_delete_object_fail', 'Échec de la suppression de l\'objet'],
+];
