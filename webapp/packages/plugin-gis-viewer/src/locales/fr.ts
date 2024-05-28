eb1758d85b28530c6e9213702b8673ecc7281907
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 15:31:28 +0200

    Added French translation to plugin-gis-viewer (fr.ts)
    Added French translations for the GIS viewer plugin messages.

---
+++ webapp/packages/plugin-gis-viewer/src/locales/fr.ts
@@ -0,0 +1,6 @@
+export default [
+  ['gis_presentation_title', 'Carte'],
+  ['gis_presentation_placeholder', 'Aucune donnée disponible'],
+  ['gis_presentation_base_tile_street_name', 'Rue'],
+  ['gis_presentation_base_tile_topography_name', 'Topographie'],
+];
