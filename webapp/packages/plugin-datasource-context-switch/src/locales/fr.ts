b0a741f71d0197e1c72067d29d0d8b16d05ff8cb
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 15:23:31 +0200

    Added French translation to plugin-datasource-context-switch (fr.ts)
    Added French translations for the context switch plugin messages.

---
+++ webapp/packages/plugin-datasource-context-switch/src/locales/fr.ts
@@ -0,0 +1,8 @@
+export default [
+  ['plugin_datasource_context_switch_select_connection', 'Sélectionner la connexion'],
+  ['plugin_datasource_context_switch_select_container', 'Sélectionner le conteneur'],
+  [
+    'plugin_datasource_context_switch_select_container_tooltip',
+    'Les modifications seront appliquées à tous les scripts actifs associés à une connexion "{arg:name}"',
+  ],
+];
