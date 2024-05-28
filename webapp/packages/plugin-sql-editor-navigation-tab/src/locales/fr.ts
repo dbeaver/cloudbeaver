9aa6e49a5dc36f7ca5d19aa3c57e9f58bf7a6ecb
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 16:01:28 +0200

    Added French translation to plugin-sql-editor-navigation-tab (fr.ts)
    Added French translations for the SQL editor navigation tab plugin messages.

---
+++ webapp/packages/plugin-sql-editor-navigation-tab/src/locales/fr.ts
@@ -0,0 +1,7 @@
+export default [
+  ['plugin_sql_editor_navigation_tab_action_sql_editor_new', 'SQL'],
+  ['plugin_sql_editor_navigation_tab_action_sql_editor_new_tooltip', 'Ouvrir l\'éditeur SQL'],
+  ['plugin_sql_editor_navigation_tab_action_sql_editor_new_tooltip_context', 'Ouvrir l\'éditeur SQL pour {arg:connection}'],
+  ['plugin_sql_editor_navigation_tab_data_source_save_confirmation_title', 'Enregistrer les modifications'],
+  ['plugin_sql_editor_navigation_tab_data_source_save_confirmation_message', 'Voulez-vous enregistrer les modifications ?'],
+];
