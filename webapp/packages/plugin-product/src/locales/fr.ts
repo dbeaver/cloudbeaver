e55fb4faa3af5513a127a6ee85ca62a6316e7e06
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 15:43:04 +0200

    Added French translation to plugin-product (fr.ts)
    Added French translations for the product plugin messages.

---
+++ webapp/packages/plugin-product/src/locales/fr.ts
@@ -0,0 +1,9 @@
+export default [
+  ['app_product_info', 'Informations sur le produit'],
+  ['app_product_info_name', 'Nom du produit'],
+  ['app_product_info_description', 'Description du produit'],
+  ['app_product_info_license_info', 'Informations sur la licence'],
+  ['app_product_info_build_time', 'Heure de construction'],
+  ['app_product_info_contacts', 'Contacts'],
+  ['app_product_info_placeholder', 'Aucune information à afficher'],
+];
