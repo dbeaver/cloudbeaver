fb9cb08c15b7df4aa01400d90067abb32a6ece18
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 16:07:38 +0200

    Added French translation to plugin-tools-panel (fr.ts)
    Added French translations for the tools panel plugin messages.

---
+++ webapp/packages/plugin-tools-panel/src/locales/fr.ts
@@ -0,0 +1,6 @@
+export default [
+  ['plugin_tools_panel_tools_settings_group', 'Outils'],
+  ['plugin_tools_panel_settings_disable_label', 'Désactiver les outils'],
+  ['plugin_tools_panel_settings_disable_description', 'Supprime le menu Outils de la barre de menu supérieure'],
+  ['plugin_tools_panel_menu_title', 'Outils'],
+];
