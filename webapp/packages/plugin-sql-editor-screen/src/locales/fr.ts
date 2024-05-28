953dc84a580812fdbe2153bc372b0d07ec2024ce
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 16:02:59 +0200

    Added French translation to plugin-sql-editor-screen (fr.ts)
    Added French translations for the SQL editor screen plugin messages.

---
+++ webapp/packages/plugin-sql-editor-screen/src/locales/fr.ts
@@ -0,0 +1,5 @@
+export default [
+  ['sql_editor_screen_no_context_title', 'Erreur lors de l\'ouverture dans un nouvel onglet'],
+  ['sql_editor_screen_no_context_message', 'Veuillez sélectionner une connexion pour l\'éditeur SQL actuel'],
+  ['sql_editor_screen_context_not_found', 'Contexte non trouvé'],
+];
