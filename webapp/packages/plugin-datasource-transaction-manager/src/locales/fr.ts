a5b9caeda16e9dde86386dae0d7485845d8d41fc
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 15:29:38 +0200

    Added French translation to plugin-datasource-transaction-manager (fr.ts)
    Added French translations for the transaction manager plugin messages.

---
+++ webapp/packages/plugin-datasource-transaction-manager/src/locales/fr.ts
@@ -0,0 +1,10 @@
+export default [
+  ['plugin_datasource_transaction_manager_commit', 'Commit'],
+  ['plugin_datasource_transaction_manager_rollback', 'Rollback'],
+  ['plugin_datasource_transaction_manager_commit_mode_switch_to_auto', 'Passer en mode auto-commit'],
+  ['plugin_datasource_transaction_manager_commit_mode_switch_to_manual', 'Passer en mode commit manuel'],
+  ['plugin_datasource_transaction_manager_commit_fail', 'Échec de la transaction de commit'],
+  ['plugin_datasource_transaction_manager_rollback_fail', 'Échec de l\'annulation de la transaction'],
+  ['plugin_datasource_transaction_manager_commit_mode_fail', 'Échec du changement de mode de commit'],
+  ['plugin_datasource_transaction_manager_commit_confirmation_message', 'Voulez-vous commiter les modifications ?'],
+];
