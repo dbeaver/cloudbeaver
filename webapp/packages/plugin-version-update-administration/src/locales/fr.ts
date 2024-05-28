8f097102dc60f769ed2eb7ba23638790aa9097c8
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 16:12:09 +0200

    Added French translation to plugin-version-update-administration (fr.ts)
    Added French translations for the version update administration plugin messages.

---
+++ webapp/packages/plugin-version-update-administration/src/locales/fr.ts
@@ -0,0 +1,5 @@
+export default [
+  ['plugin_version_update_administration_version_selector_label', 'Version'],
+  ['plugin_version_update_administration_recommendations_label', 'Recommandations'],
+  ['plugin_version_update_administration_recommendations', 'Nous recommandons fortement d\'éviter la rétrogradation du produit. Nous ne pouvons pas garantir le bon fonctionnement de l\'application après cette procédure.'],
+];
