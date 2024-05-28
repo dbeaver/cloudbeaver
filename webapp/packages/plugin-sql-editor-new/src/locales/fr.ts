29e21e514121d3a1248413b3fb03a22fcd17701c
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 16:02:11 +0200

    Added French translation to plugin-sql-editor-new (fr.ts)
    Added French translations for the new SQL editor plugin messages.

---
+++ webapp/packages/plugin-sql-editor-new/src/locales/fr.ts
@@ -0,0 +1,6 @@
+export default [
+  ['plugin_sql_editor_new_merge_conflict_keep_current_label', 'Garder l\'actuel'],
+  ['plugin_sql_editor_new_merge_conflict_keep_current_tooltip', 'Garder les modifications actuelles'],
+  ['plugin_sql_editor_new_merge_conflict_accept_incoming_label', 'Accepter les modifications entrantes'],
+  ['plugin_sql_editor_new_merge_conflict_accept_incoming_tooltip', 'Accepter les modifications entrantes'],
+];
