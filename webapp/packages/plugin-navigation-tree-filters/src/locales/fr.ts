f740290450ad211b6d78748f422be26354703bf9
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 15:38:24 +0200

    Added French translation to plugin-navigation-tree-filters (fr.ts)
    Added French translations for the navigation tree filters plugin messages.

---
+++ webapp/packages/plugin-navigation-tree-filters/src/locales/fr.ts
@@ -0,0 +1,11 @@
+export default [
+  ['plugin_navigation_tree_filters_title', 'Filtres'],
+  ['plugin_navigation_tree_filters_info', 'Vous pouvez utiliser des masques (%_and%)'],
+  ['plugin_navigation_tree_filters_configuration', 'Configurer le filtre "{arg:name}"'],
+  ['plugin_navigation_tree_filters_reset', 'Réinitialiser le filtre'],
+  ['plugin_navigation_tree_filters_filter', 'Filtrer'],
+  ['plugin_navigation_tree_filters_include', 'Inclure'],
+  ['plugin_navigation_tree_filters_exclude', 'Exclure'],
+  ['plugin_navigation_tree_filters_submit_fail', 'Échec de l\'application des filtres'],
+  ['plugin_navigation_tree_filters_reset_fail', 'Échec de la réinitialisation des filtres'],
+];
