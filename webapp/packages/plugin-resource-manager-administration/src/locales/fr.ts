ce55cf7203e31d52d37d4be0c090dbc232324712
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 15:53:03 +0200

    Added French translation to plugin-resource-manager-administration (fr.ts)
    Added French translations for the resource manager administration plugin messages.

---
+++ webapp/packages/plugin-resource-manager-administration/src/locales/fr.ts
@@ -0,0 +1,4 @@
+export default [
+  ['plugin_resource_manager_administration_settings_enable', 'Activer {alias:plugin_resource_manager_title}'],
+  ['plugin_resource_manager_administration_settings_enable_description', 'Activer la fonctionnalité {alias:plugin_resource_manager_title}'],
+];
