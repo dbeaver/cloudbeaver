d8f697629e76c25e76343b11989cced4d272abe4
matthieukhl <140057664+matthieukhl@users.noreply.github.com>
2024-05-28 16:09:00 +0200

    Added French translation to plugin-user-profile-settings (fr.ts)
    Added French translations for the user profile settings plugin messages.

---
+++ webapp/packages/plugin-user-profile-settings/src/locales/fr.ts
@@ -0,0 +1,7 @@
+export default [
+  ['plugin_user_profile_settings_tab_label', 'Préférences'],
+  ['plugin_user_profile_settings_action_description', 'Préférences utilisateur'],
+  ['plugin_user_profile_settings_save_success', 'Préférences enregistrées avec succès'],
+  ['plugin_user_profile_settings_save_fail', 'Échec de l\'enregistrement des préférences'],
+  ['plugin_user_profile_settings_restore_defaults', 'Restaurer les paramètres par défaut'],
+];
