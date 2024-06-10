79c026546e6afad02b25f20b86c92f1a45b9f1d4
LonwoLonwo <anvolka@yandex.ru>
2024-06-10 20:59:39 +0200

    CB-5268 fix tooltip; add localization

--- webapp/packages/plugin-connections/src/locales/ru.ts
+++ webapp/packages/plugin-connections/src/locales/ru.ts
@@ -7,6 +7,7 @@ export default [
   ['connections_public_connection_edit_reconnect_failed', 'Не удалось переподключиться'],
   ['connections_public_connection_folder_move_failed', 'Ошибка перемещения в папку'],
   ['connections_public_connection_folder_move_duplication', 'Выбранные папки или папка назначения содержит папки с таким же названием ({arg:name})'],
+  ['connections_public_connection_cloud_auth_required', 'Вы должны подключиться через "{arg:providerLabel}", чтобы работать с этим соединением.'],
   ['plugin_connections_connection_form_project_invalid', 'У вас нет разрешения создавать коннекшены в выбранном проекте'],
   ['plugin_connections_connection_form_host_configuration_invalid', 'Конфигурация хоста не поддерживается'],
   ['plugin_connections_connection_form_name_invalid', "Поле 'Название подключения' не может быть пустым"],
