89245a5291b11d03efedc0f9c20a8f7d44a4fd1a
naumov <iamemptyhuh@gmail.com>
2024-06-12 15:30:01 +0200

    CB-4706 review fixes

--- webapp/packages/plugin-react-data-grid/src/DataGrid.tsx
+++ webapp/packages/plugin-react-data-grid/src/DataGrid.tsx
@@ -5,14 +5,11 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-import { ForwardedRef, forwardRef, Key, RefAttributes } from 'react';
-import ReactDataGrid, { DataGridHandle, DataGridProps } from 'react-data-grid';
+import { forwardRef } from 'react';
+import ReactDataGrid from 'react-data-grid';
 
 import './reset.css';
 
-const DataGridInner: <R, SR = unknown, K extends Key = Key>(props: DataGridProps<R, SR, K>, ref: ForwardedRef<DataGridHandle>) => JSX.Element =
-  function DataGrid(props, ref) {
-    return <ReactDataGrid {...props} ref={ref} className={['react-data-grid-container', props.className].join(' ')} />;
-  };
-
-export const DataGrid = forwardRef(DataGridInner) as <T>(props: DataGridProps<T> & RefAttributes<DataGridHandle>) => ReturnType<typeof DataGridInner>;
+export const DataGrid = forwardRef(function DataGrid(props, ref) {
+  return <ReactDataGrid {...props} ref={ref} className={['react-data-grid-container', props.className].join(' ')} />;
+}) as typeof ReactDataGrid;
