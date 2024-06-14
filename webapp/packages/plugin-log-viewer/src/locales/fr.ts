b8de1251b94717be90c81290f43f240083ec70fe
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-06-14 14:23:12 +0200

    CB-5266 shorter translate for log-viewer french action enable label

--- webapp/packages/plugin-log-viewer/src/locales/fr.ts
+++ webapp/packages/plugin-log-viewer/src/locales/fr.ts
@@ -7,7 +7,7 @@
  */
 export default [
   ['plugin_log_viewer_log_viewer_settings_group', 'Visionneuse de journaux'],
-  ['plugin_log_viewer_action_enable_label', 'Activer la visionneuse de journaux'],
+  ['plugin_log_viewer_action_enable_label', 'Visualiseur de journaux'],
   ['plugin_log_viewer_placeholder', "La visionneuse de journaux n'est pas active"],
   ['plugin_log_viewer_clear_log', 'Effacer les journaux'],
   ['plugin_log_viewer_entry_type', 'Type'],
