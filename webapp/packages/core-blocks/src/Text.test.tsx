1a6a4433ef808efed6f889ebbc21beaa1c16a763
naumov <iamemptyhuh@gmail.com>
2024-06-19 22:57:17 +0200

    CB-5277 add tests for core blocks components

---
+++ webapp/packages/core-blocks/src/Text.test.tsx
@@ -0,0 +1,44 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2024 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+import { waitFor } from '@testing-library/react';
+
+import { coreEventsManifest } from '@cloudbeaver/core-events';
+import { coreLocalizationManifest } from '@cloudbeaver/core-localization';
+import { coreSettingsManifest } from '@cloudbeaver/core-settings';
+import { coreThemingManifest } from '@cloudbeaver/core-theming';
+import { createApp, renderInApp } from '@cloudbeaver/tests-runner';
+
+import { Text } from './Text';
+
+const app = createApp(coreEventsManifest, coreSettingsManifest, coreThemingManifest, coreLocalizationManifest);
+
+describe('Text Component', () => {
+  it('renders children correctly', async () => {
+    const { getByText } = renderInApp(<Text>Hello World</Text>, app);
+    const text = await waitFor(() => getByText('Hello World'));
+    expect(text).toBeInTheDocument();
+  });
+
+  it('applies custom className', () => {
+    const { container } = renderInApp(<Text className="custom-class">Hello World</Text>, app);
+    expect(container.getElementsByClassName('custom-class')).toHaveLength(1);
+  });
+
+  it('passes HTML attributes correctly', () => {
+    const { container } = renderInApp(
+      <Text id="custom-id" data-testid="custom-testid">
+        Hello World
+      </Text>,
+      app,
+    );
+
+    const div = container.firstChild;
+    expect(div).toHaveAttribute('id', 'custom-id');
+    expect(div).toHaveAttribute('data-testid', 'custom-testid');
+  });
+});
---
+++ webapp/packages/core-blocks/src/TextPlaceholder.test.tsx
@@ -0,0 +1,31 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2024 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+import { waitFor } from '@testing-library/react';
+
+import { coreEventsManifest } from '@cloudbeaver/core-events';
+import { coreLocalizationManifest } from '@cloudbeaver/core-localization';
+import { coreSettingsManifest } from '@cloudbeaver/core-settings';
+import { coreThemingManifest } from '@cloudbeaver/core-theming';
+import { createApp, renderInApp } from '@cloudbeaver/tests-runner';
+
+import { TextPlaceholder } from './TextPlaceholder';
+
+const app = createApp(coreEventsManifest, coreSettingsManifest, coreThemingManifest, coreLocalizationManifest);
+
+describe('TextPlaceholder Component', () => {
+  it('renders children correctly', async () => {
+    const { getByText } = renderInApp(<TextPlaceholder>Hello World</TextPlaceholder>, app);
+    const text = await waitFor(() => getByText('Hello World'));
+    expect(text).toBeInTheDocument();
+  });
+
+  it('applies custom className', () => {
+    const { container } = renderInApp(<TextPlaceholder className="custom-class">Hello World</TextPlaceholder>, app);
+    expect(container.getElementsByClassName('custom-class')).toHaveLength(1);
+  });
+});
