ce878d77835983ad0a204392daebe9bc6ffb1cd8
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-07-08 11:30:58 +0200

    CB-5178 removes license build and build time info from new "About" menu setting

--- webapp/packages/plugin-product/src/locales/ru.ts
+++ webapp/packages/plugin-product/src/locales/ru.ts
@@ -1,9 +1,14 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2024 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
 export default [
-  ['app_product_info', 'Информация о продукте'],
+  ['app_product_info', 'О продукте'],
   ['app_product_info_name', 'Название продукта'],
   ['app_product_info_description', 'Описание продукта'],
-  ['app_product_info_license_info', 'Информация о лицензии'],
-  ['app_product_info_build_time', 'Время последней сборки'],
   ['app_product_info_contacts', 'Контакты'],
   ['app_product_info_placeholder', 'Нет доступной информации о продукте'],
 ];
