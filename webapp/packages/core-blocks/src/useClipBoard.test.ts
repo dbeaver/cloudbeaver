a369097b5b784827338c21cd2ee736ec03814df0
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-07-19 13:20:49 +0200

    CB-5340 cleanup

--- webapp/packages/core-blocks/src/useClipBoard.test.ts
+++ webapp/packages/core-blocks/src/useClipBoard.test.ts
@@ -5,6 +5,7 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
+import { beforeEach, describe, expect, it } from '@jest/globals';
 import { renderHook } from '@testing-library/react';
 
 import * as coreDi from '@cloudbeaver/core-di';
--- webapp/packages/core-blocks/src/useObservableRef.test.ts
+++ webapp/packages/core-blocks/src/useObservableRef.test.ts
@@ -5,6 +5,7 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
+import { beforeAll, describe, expect, test } from '@jest/globals';
 import { renderHook } from '@testing-library/react';
 import { action, computed, isObservable, observable, runInAction } from 'mobx';
 
