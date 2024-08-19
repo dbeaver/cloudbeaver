126e47b7e50f181f978f2dbb117055ba7a5c9305
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-08-19 10:35:12 +0200

    CB-5431 cleanup

--- webapp/packages/core-administration/src/AdministrationScreen/ConfigurationWizard/ConfigurationWizardService.ts
+++ webapp/packages/core-administration/src/AdministrationScreen/ConfigurationWizard/ConfigurationWizardService.ts
@@ -11,6 +11,7 @@ import { injectable } from '@cloudbeaver/core-di';
 import { NotificationService } from '@cloudbeaver/core-events';
 import { ServerConfigResource } from '@cloudbeaver/core-root';
 import { ScreenService } from '@cloudbeaver/core-routing';
+import { isNotNullDefined } from '@cloudbeaver/core-utils';
 
 import { AdministrationItemService, filterHiddenAdministrationItem } from '../../AdministrationItem/AdministrationItemService';
 import { filterConfigurationWizard } from '../../AdministrationItem/filterConfigurationWizard';
@@ -87,11 +88,12 @@ export class ConfigurationWizardService {
   }
 
   private async onRouteChange() {
-    const isExistingPage =
-      this.administrationScreenService?.activeScreen?.item && this.isStepAvailable(this.administrationScreenService.activeScreen.item);
-    const isCurrentStepAvailable = this.currentStep && this.isStepAvailable(this.currentStep.name);
-
     await this.serverConfigResource.load();
+
+    const isExistingPage =
+      isNotNullDefined(this.administrationScreenService?.activeScreen?.item) &&
+      this.isStepAvailable(this.administrationScreenService.activeScreen.item);
+    const isCurrentStepAvailable = isNotNullDefined(this.currentStep) && this.isStepAvailable(this.currentStep.name);
 
     if (!this.serverConfigResource.data?.configurationMode) {
       return;
@@ -112,12 +114,14 @@ export class ConfigurationWizardService {
 
   isStepAvailable(name: string): boolean {
     const stepIndex = this.steps.findIndex(step => step.name === name);
+
     if (stepIndex === -1) {
       return false;
     }
 
     for (let i = 0; i < stepIndex; i++) {
       const prevStep = this.steps[i];
+
       if (!this.isStepDone(prevStep) || this.isStepDisabled(prevStep)) {
         return false;
       }
