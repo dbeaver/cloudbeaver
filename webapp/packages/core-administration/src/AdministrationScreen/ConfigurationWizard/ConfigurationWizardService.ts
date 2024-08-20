0211f91dae7da8885926d8926289656bbe3cb181
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-08-20 11:36:25 +0200

    Revert "CB-5431 cleanup"
    This reverts commit 126e47b7e50f181f978f2dbb117055ba7a5c9305.

--- webapp/packages/core-administration/src/AdministrationScreen/ConfigurationWizard/ConfigurationWizardService.ts
+++ webapp/packages/core-administration/src/AdministrationScreen/ConfigurationWizard/ConfigurationWizardService.ts
@@ -11,7 +11,6 @@ import { injectable } from '@cloudbeaver/core-di';
 import { NotificationService } from '@cloudbeaver/core-events';
 import { ServerConfigResource } from '@cloudbeaver/core-root';
 import { ScreenService } from '@cloudbeaver/core-routing';
-import { isNotNullDefined } from '@cloudbeaver/core-utils';
 
 import { AdministrationItemService, filterHiddenAdministrationItem } from '../../AdministrationItem/AdministrationItemService';
 import { filterConfigurationWizard } from '../../AdministrationItem/filterConfigurationWizard';
@@ -88,12 +87,11 @@ export class ConfigurationWizardService {
   }
 
   private async onRouteChange() {
-    await this.serverConfigResource.load();
-
     const isExistingPage =
-      isNotNullDefined(this.administrationScreenService?.activeScreen?.item) &&
-      this.isStepAvailable(this.administrationScreenService.activeScreen.item);
-    const isCurrentStepAvailable = isNotNullDefined(this.currentStep) && this.isStepAvailable(this.currentStep.name);
+      this.administrationScreenService?.activeScreen?.item && this.isStepAvailable(this.administrationScreenService.activeScreen.item);
+    const isCurrentStepAvailable = this.currentStep && this.isStepAvailable(this.currentStep.name);
+
+    await this.serverConfigResource.load();
 
     if (!this.serverConfigResource.data?.configurationMode) {
       return;
@@ -114,14 +112,12 @@ export class ConfigurationWizardService {
 
   isStepAvailable(name: string): boolean {
     const stepIndex = this.steps.findIndex(step => step.name === name);
-
     if (stepIndex === -1) {
       return false;
     }
 
     for (let i = 0; i < stepIndex; i++) {
       const prevStep = this.steps[i];
-
       if (!this.isStepDone(prevStep) || this.isStepDisabled(prevStep)) {
         return false;
       }
