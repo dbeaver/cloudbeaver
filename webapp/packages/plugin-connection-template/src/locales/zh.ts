012b4a1620e5ed2bb0a9791c44c929ab8cb1b658
李飞 <lif@tongfun.com>
2024-08-26 09:28:42 +0800

    chore: Update Chinese translations

--- webapp/packages/plugin-connection-template/src/locales/zh.ts
+++ webapp/packages/plugin-connection-template/src/locales/zh.ts
@@ -1,3 +1,10 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2024 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
 export default [
   ['basicConnection_connectionDialog_newConnection', '新连接'],
   ['basicConnection_connectionDialog_title', '连接数据库'],
@@ -8,6 +15,6 @@ export default [
   ['basicConnection_connectionDialog_passwordPlaceholder', '密码'],
   ['basicConnection_connectionDialog_connecting', '连接中...'],
   ['basicConnection_connectionDialog_connecting_message', '连接数据库...'],
-  ['plugin_connection_template_connect_success', 'Connection is established'],
+  ['plugin_connection_template_connect_success', '已建立连接'],
   ['plugin_connection_template_action_connection_template_label', '从模板创建'],
 ];
