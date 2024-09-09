c68fe532b42238f66507912f9aeb42b378c618d7
Elizabeth <e1izabeth.k@outlook.com>
2024-09-09 23:20:37 +0300

    Revert "dbeaver/pro#2965 Add logging"

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/utils/WebDataSourceUtils.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/utils/WebDataSourceUtils.java
@@ -201,11 +201,9 @@ public class WebDataSourceUtils {
             }
             authProperties.replace(AuthModelDatabaseNativeCredentials.PROP_USER_PASSWORD, null);
             dataSourceContainer.resetPassword();
-            log.debug("saveAuthProperties: dataSourceContainer.resetPassword()");
         } else {
             if (authProperties == null) {
                 // No changes
-                log.debug("saveAuthProperties: authProperties == null");
                 return;
             }
         }
@@ -296,7 +294,6 @@ public class WebDataSourceUtils {
             saveConfig = false;
         }
         if (saveConfig) {
-            log.debug("updateConnectionCredentials: dataSourceContainer.persistConfiguration(saveCredentials: " + dataSourceContainer.isSavePassword() + ")");
             dataSourceContainer.persistConfiguration();
         }
     }
