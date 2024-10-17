318f970abb9a3fb72bd504fda15d20cdea66f3e4
sergeyteleshev <iamsergeyteleshev@gmail.com>
2024-10-17 16:10:07 +0200

    CB-5795 removes notification

--- webapp/packages/core-blocks/src/index.ts
+++ webapp/packages/core-blocks/src/index.ts
@@ -245,7 +245,6 @@ export * from './Snackbars/SnackbarMarkups/SnackbarBody.js';
 export * from './Snackbars/SnackbarMarkups/SnackbarFooter.js';
 export * from './Snackbars/Snackbar.js';
 export * from './Snackbars/ActionSnackbar.js';
-export * from './Snackbars/RedirectSnackbar.js';
 export * from './Snackbars/ProcessSnackbar.js';
 export * from './useUserData.js';
 export * from './useListKeyboardNavigation.js';
