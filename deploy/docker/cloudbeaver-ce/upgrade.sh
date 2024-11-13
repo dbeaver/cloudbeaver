b24f2d3ae88c6f7c86e56a8d3d0b598b04a22bc4
Greg Miller <ggxedq@gmail.com>
2024-11-13 19:07:11 +0100

    dbeaver/dbeaver-devops#1574 Reworked script, added start process as dbeaver user

--- deploy/docker/cloudbeaver-ce/upgrade.sh
+++ deploy/docker/cloudbeaver-ce/upgrade.sh
@@ -4,11 +4,9 @@ VOLUME_PATH="/opt/cloudbeaver/workspace"
 NEW_USER="dbeaver"
 NEW_GROUP="dbeaver"
 
-id $NEW_USER &>/dev/null || useradd -m -s /bin/bash $NEW_USER
-
 chown -R $NEW_USER:$NEW_GROUP $VOLUME_PATH
 find $VOLUME_PATH -type d -exec chmod 775 {} +
 find $VOLUME_PATH -type f -exec chmod 664 {} +
 chmod 775 $VOLUME_PATH
 
-exec ./run-server.sh
+exec su $NEW_USER -c "JAVA_HOME=$JAVA_HOME PATH=$PATH ./run-server.sh"
