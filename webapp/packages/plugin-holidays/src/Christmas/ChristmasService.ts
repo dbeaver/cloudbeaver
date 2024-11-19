779bc8e745ea2d86cb002f86fa040f0b5c68258f
Andrey Sychev <nukemoore@gmail.com>
2024-11-19 13:00:05 +0100

    CB-5918 fix: start date

--- webapp/packages/plugin-holidays/src/Christmas/ChristmasService.ts
+++ webapp/packages/plugin-holidays/src/Christmas/ChristmasService.ts
@@ -17,8 +17,8 @@ export class ChristmasService extends Bootstrap implements IHoliday {
   public name = 'Merry Christmas';
   public logoSrc = '/icons/christmas_logo.svg';
   public iconSrc = '/icons/christmas_action.svg';
-  public startDate = new Date(new Date().getFullYear(), 9, 1);
-  public endDate = new Date(new Date().getFullYear(), 0, 7);
+  public startDate = new Date(new Date().getFullYear(), 11, 12); // December, 12
+  public endDate = new Date(new Date().getFullYear(), 0, 7); // January, 7
 
   constructor(private readonly holidayService: HolidaysService) {
     super();
@@ -40,7 +40,6 @@ export class ChristmasService extends Bootstrap implements IHoliday {
   }
 
   startCelebration(): void {
-    console.log(this.christmas);
     this.christmas.start();
   }
 
