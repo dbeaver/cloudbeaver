ab0925c1a9533f52165b71118af7411dcdd2c516
mayerro <roman.kunitskiy@gmail.com>
2024-11-20 15:29:33 +0100

    dbeaver/dbeaver-devops#1553 fixed user usage and ownership applying

--- deploy/scripts/launch-product.sh
+++ deploy/scripts/launch-product.sh
@@ -3,21 +3,10 @@
 # This script is needed to change ownership and run the application as user dbeaver during the upgrade from version 24.2.0
 
 # Define the path to the workspace volume and user/group for ownership changes
-VOLUME_PATH="/opt/cloudbeaver/workspace"
-NEW_USER="dbeaver"
-NEW_GROUP="dbeaver"
-
 # Change ownership of the VOLUME_PATH to the dbeaver user and group
-chown -R $NEW_USER:$NEW_GROUP $VOLUME_PATH
-
-# This allows the dbeaver user and group to have read, write, and execute permissions, while others have only read and execute
-find $VOLUME_PATH -type d -exec chmod 775 {} +
-
-# This allows the dbeaver user and group to read and write files, while others can only read
-find $VOLUME_PATH -type f -exec chmod 664 {} +
-
-# This ensures that the root workspace directory itself has the correct permissions
-chmod 775 $VOLUME_PATH
+# Variables DBEAVER_<UID|GID> are inherited from the dbeaver/base-java image
+# PWD equals WORKDIR value from product Dockerfile
+chown -R $DBEAVER_UID:$DBEAVER_GID $PWD
 
 # Execute run-server.sh as the dbeaver user with the JAVA_HOME and PATH environment variables
-exec su $NEW_USER -c "JAVA_HOME=$JAVA_HOME PATH=$PATH ./run-server.sh"
+exec su $DBEAVER_UID -c "JAVA_HOME=$JAVA_HOME PATH=$PATH ./run-server.sh"
