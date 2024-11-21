62c7715136f409fd56105e43830eea5e5c31f391
Greg Miller <ggxedq@gmail.com>
2024-11-21 11:57:47 +0100

    dbeaver/dbeaver-devops#1553 Rewrited comment

--- deploy/scripts/launch-product.sh
+++ deploy/scripts/launch-product.sh
@@ -3,6 +3,7 @@
 # This script is needed to change ownership and run the application as user dbeaver during the upgrade from version 24.2.0
 
 # Change ownership of the WORKDIR to the dbeaver user and group
+# Variables DBEAVER_<UID|GID> are defined in the Dockerfile and exported to the runtime environment
 # PWD equals WORKDIR value from product Dockerfile
 chown -R $DBEAVER_UID:$DBEAVER_GID $PWD
 
