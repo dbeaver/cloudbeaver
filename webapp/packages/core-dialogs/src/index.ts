16b4e376637fd6b6ccf088df6b330b2fb32cafc4
naumov <iamemptyhuh@gmail.com>
2024-12-05 11:07:39 +0100

    CB-4071 remove old context menu

--- webapp/packages/core-dialogs/src/index.ts
+++ webapp/packages/core-dialogs/src/index.ts
@@ -6,16 +6,4 @@
  * you may not use this file except in compliance with the License.
  */
 export * from './CommonDialog/CommonDialogService.js';
-
-export * from './Menu/IMenuPanel.js';
-export * from './Menu/StaticMenu/StaticMenu.js';
-
-export * from './Menu/models/ComputedContextMenuModel.js';
-export * from './Menu/models/ComputedMenuItemModel.js';
-export * from './Menu/models/ComputedMenuPanelModel.js';
-
-// contextMenu
-export * from './Menu/ContextMenu/ContextMenuService.js';
-export * from './Menu/ContextMenu/IContextMenuItem.js';
-export * from './Menu/ContextMenu/IMenuContext.js';
 export * from './manifest.js';
