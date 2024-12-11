feb8df1ddef9369f8d32ca2a7b0371bd15f1cf0b
Andrey Sychev <nukemoore@gmail.com>
2024-12-11 12:37:38 +0100

    CB-6000 feat: change the way of updating task state

--- webapp/packages/core-root/src/AsyncTask/AsyncTaskInfoService.ts
+++ webapp/packages/core-root/src/AsyncTask/AsyncTaskInfoService.ts
@@ -5,20 +5,49 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-import { injectable } from '@cloudbeaver/core-di';
-import { type AsyncTaskInfo, GraphQLService } from '@cloudbeaver/core-sdk';
-import { cancellableTimeout } from '@cloudbeaver/core-utils';
+import type { Subscription } from 'rxjs';
 
+import { Disposable, injectable } from '@cloudbeaver/core-di';
+import { type AsyncTaskInfo, GraphQLService, type WsAsyncTaskInfo } from '@cloudbeaver/core-sdk';
+
+import { ServerEventId } from '../SessionEventSource.js';
 import { AsyncTask } from './AsyncTask.js';
-
-const DELAY_BETWEEN_TRIES = 250;
+import { AsyncTaskInfoEventHandler } from './AsyncTaskInfoEventHandler.js';
 
 @injectable()
-export class AsyncTaskInfoService {
+export class AsyncTaskInfoService extends Disposable {
   private readonly tasks: Map<string, AsyncTask>;
+  private connection: Subscription | null;
+  private onEvent: any;
 
-  constructor(private readonly graphQLService: GraphQLService) {
+  constructor(
+    private readonly graphQLService: GraphQLService,
+    private readonly asyncTaskInfoEventHandler: AsyncTaskInfoEventHandler,
+  ) {
+    super();
     this.tasks = new Map();
+    this.connection = null;
+
+    this.onEvent = asyncTaskInfoEventHandler.onEvent<WsAsyncTaskInfo>(ServerEventId.CbSessionTaskInfoUpdated, async data => {
+      console.log('onEvent', data);
+      // const task = this.tasks.get(data.taskId);
+
+      // if (task?.pending && task.info) {
+      //   await task.updateInfoAsync(async task => {
+      //     const { taskInfo } = await this.graphQLService.sdk.getAsyncTaskInfo({
+      //       taskId: task.info!.id,
+      //       removeOnFinish: false,
+      //     });
+
+      //     return taskInfo;
+      //   });
+      // }
+    });
+  }
+
+  override dispose(): void {
+    this.connection?.unsubscribe();
+    this.onEvent.dispose();
   }
 
   create(getter: () => Promise<AsyncTaskInfo>): AsyncTask {
@@ -26,7 +55,7 @@ export class AsyncTaskInfoService {
     this.tasks.set(task.id, task);
 
     if (this.tasks.size === 1) {
-      setTimeout(() => this.update(), 1);
+      this.connection = this.asyncTaskInfoEventHandler.eventsSubject.connect();
     }
 
     return task;
@@ -53,6 +82,11 @@ export class AsyncTaskInfoService {
 
     this.tasks.delete(taskId);
 
+    if (this.tasks.size === 0) {
+      this.connection?.unsubscribe();
+      this.connection = null;
+    }
+
     if (task.info !== null) {
       await this.graphQLService.sdk.getAsyncTaskInfo({
         taskId: task.info.id,
@@ -65,35 +99,6 @@ export class AsyncTaskInfoService {
     const task = this.tasks.get(taskId);
 
     await task?.cancelAsync();
-  }
-
-  private async update() {
-    while (this.tasks.size > 0) {
-      const tasks = Array.from(this.tasks.values());
-
-      for (const task of tasks) {
-        this.updateTask(task);
-      }
-
-      await cancellableTimeout(DELAY_BETWEEN_TRIES);
-    }
-  }
-
-  private async updateTask(task: AsyncTask): Promise<void> {
-    try {
-      if (task.pending && task.info) {
-        await task.updateInfoAsync(async task => {
-          const { taskInfo } = await this.graphQLService.sdk.getAsyncTaskInfo({
-            taskId: task.info!.id,
-            removeOnFinish: false,
-          });
-
-          return taskInfo;
-        });
-      }
-    } catch (e: any) {
-      console.log('Failed to check async task status', e);
-    }
   }
 
   private async cancelTask(info: AsyncTaskInfo): Promise<void> {
