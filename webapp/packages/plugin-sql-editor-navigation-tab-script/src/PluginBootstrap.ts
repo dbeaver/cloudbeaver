37d363823e270cae0a9c2f52ef28537dd5e8dadf
Andrey Sychev <nukemoore@gmail.com>
2024-12-30 11:00:03 +0100

    CB-5832 feat: add shortcut for save as script action

--- webapp/packages/plugin-sql-editor-navigation-tab-script/src/PluginBootstrap.ts
+++ webapp/packages/plugin-sql-editor-navigation-tab-script/src/PluginBootstrap.ts
@@ -14,7 +14,7 @@ import { isResourceOfType, ProjectInfoResource, ProjectsService } from '@cloudbe
 import { CachedMapAllKey, CachedTreeChildrenKey } from '@cloudbeaver/core-resource';
 import { getRmResourcePath, NAV_NODE_TYPE_RM_RESOURCE, ResourceManagerResource, RESOURCES_NODE_PATH } from '@cloudbeaver/core-resource-manager';
 import { createPath, getPathName } from '@cloudbeaver/core-utils';
-import { ActionService, MenuService } from '@cloudbeaver/core-view';
+import { ActionService, KeyBindingService, MenuService } from '@cloudbeaver/core-view';
 import { NavigationTabsService } from '@cloudbeaver/plugin-navigation-tabs';
 import { getResourceKeyFromNodeId } from '@cloudbeaver/plugin-navigation-tree-rm';
 import { RESOURCE_NAME_REGEX, ResourceManagerService } from '@cloudbeaver/plugin-resource-manager';
@@ -33,6 +33,7 @@ import {
 import { isSQLEditorTab, SqlEditorNavigatorService } from '@cloudbeaver/plugin-sql-editor-navigation-tab';
 
 import { ACTION_SAVE_AS_SCRIPT } from './ACTION_SAVE_AS_SCRIPT.js';
+import { KEY_BINDING_SQL_EDITOR_SAVE_AS_SCRIPT } from './KEY_BINDING_SQL_EDITOR_SAVE_AS_SCRIPT.js';
 import { ResourceSqlDataSource } from './ResourceSqlDataSource.js';
 import { SqlEditorTabResourceService } from './SqlEditorTabResourceService.js';
 
@@ -55,6 +56,7 @@ export class PluginBootstrap extends Bootstrap {
     private readonly sqlEditorSettingsService: SqlEditorSettingsService,
     private readonly resourceManagerResource: ResourceManagerResource,
     private readonly resourceManagerScriptsService: ResourceManagerScriptsService,
+    private readonly keyBindingService: KeyBindingService,
   ) {
     super();
   }
@@ -78,105 +80,7 @@ export class PluginBootstrap extends Bootstrap {
 
         return dataSource instanceof MemorySqlDataSource || dataSource instanceof LocalStorageSqlDataSource;
       },
-      handler: async (context, action) => {
-        const state = context.get(DATA_CONTEXT_SQL_EDITOR_STATE)!;
-
-        let dataSource: ISqlDataSource | ResourceSqlDataSource | undefined = this.sqlDataSourceService.get(state.editorId);
-
-        if (!dataSource) {
-          return;
-        }
-
-        if (action === ACTION_SAVE_AS_SCRIPT) {
-          let projectId = dataSource.executionContext?.projectId ?? null;
-          await this.projectInfoResource.load(CachedMapAllKey);
-          const name = getSqlEditorName(state, dataSource);
-
-          if (projectId) {
-            const project = this.projectInfoResource.get(projectId);
-
-            if (!project?.canEditResources) {
-              projectId = null;
-            }
-          }
-
-          const result = await this.commonDialogService.open(SaveScriptDialog, {
-            defaultScriptName: name,
-            projectId,
-            validation: async ({ name, projectId }, setMessage) => {
-              const trimmedName = name.trim();
-
-              if (!projectId || !trimmedName.length) {
-                return false;
-              }
-
-              if (!RESOURCE_NAME_REGEX.test(trimmedName)) {
-                setMessage('plugin_resource_manager_scripts_script_name_invalid_characters_message');
-                return false;
-              }
-
-              const project = this.projectInfoResource.get(projectId);
-              const nameWithExtension = this.projectInfoResource.getNameWithExtension(projectId, SCRIPTS_TYPE_ID, trimmedName);
-              const rootFolder = project ? this.resourceManagerScriptsService.getRootFolder(project) : undefined;
-              const key = getRmResourcePath(projectId, rootFolder);
-
-              try {
-                await this.resourceManagerResource.load(CachedTreeChildrenKey(key));
-                return !this.resourceManagerResource.has(createPath(key, nameWithExtension));
-              } catch (exception: any) {
-                return false;
-              }
-            },
-          });
-
-          if (result !== DialogueStateResult.Rejected && result !== DialogueStateResult.Resolved) {
-            try {
-              projectId = result.projectId;
-
-              if (!projectId) {
-                throw new Error('Project not selected');
-              }
-
-              const project = this.projectInfoResource.get(projectId);
-              if (!project) {
-                throw new Error('Project not found');
-              }
-
-              const nameWithoutExtension = result.name.trim();
-              const scriptName = this.projectInfoResource.getNameWithExtension(projectId, SCRIPTS_TYPE_ID, nameWithoutExtension);
-              const scriptsRootFolder = this.resourceManagerScriptsService.getRootFolder(project);
-              const folderResourceKey = getResourceKeyFromNodeId(createPath(RESOURCES_NODE_PATH, projectId, scriptsRootFolder));
-
-              if (!folderResourceKey) {
-                this.notificationService.logError({ title: 'ui_error', message: 'plugin_sql_editor_navigation_tab_resource_save_script_error' });
-                return;
-              }
-
-              const resourceKey = createPath(folderResourceKey, scriptName);
-
-              await this.resourceManagerScriptsService.createScript(resourceKey, dataSource.executionContext, dataSource.script);
-
-              dataSource = this.sqlDataSourceService.create(state, ResourceSqlDataSource.key, {
-                script: dataSource.script,
-                executionContext: dataSource.executionContext,
-              });
-
-              (dataSource as ResourceSqlDataSource).setResourceKey(resourceKey);
-
-              this.notificationService.logSuccess({
-                title: 'plugin_sql_editor_navigation_tab_resource_save_script_success',
-                message: nameWithoutExtension,
-              });
-
-              if (!this.resourceManagerScriptsService.active) {
-                this.resourceManagerScriptsService.togglePanel();
-              }
-            } catch (exception) {
-              this.notificationService.logException(exception as any, 'plugin_sql_editor_navigation_tab_resource_save_script_error');
-            }
-          }
-        }
-      },
+      handler: this.saveAsScriptHandler.bind(this),
       getActionInfo: (context, action) => {
         if (action === ACTION_SAVE_AS_SCRIPT) {
           return {
@@ -188,6 +92,8 @@ export class PluginBootstrap extends Bootstrap {
         return action.info;
       },
     });
+
+    this.navigationTabsService.registerAction(ACTION_SAVE_AS_SCRIPT);
 
     this.menuService.addCreator({
       menus: [SQL_EDITOR_TOOLS_MENU],
@@ -201,6 +107,115 @@ export class PluginBootstrap extends Bootstrap {
       },
       getItems: (context, items) => [...items, ACTION_SAVE_AS_SCRIPT],
     });
+
+    this.keyBindingService.addKeyBindingHandler({
+      id: 'save-as-script',
+      binding: KEY_BINDING_SQL_EDITOR_SAVE_AS_SCRIPT,
+      actions: [ACTION_SAVE_AS_SCRIPT],
+      contexts: [DATA_CONTEXT_SQL_EDITOR_STATE],
+      isBindingApplicable: (_, action) => action === ACTION_SAVE_AS_SCRIPT,
+      handler: this.saveAsScriptHandler.bind(this),
+    });
+  }
+
+  private async saveAsScriptHandler(context: any, action: any) {
+    const state = context.get(DATA_CONTEXT_SQL_EDITOR_STATE)!;
+
+    let dataSource: ISqlDataSource | ResourceSqlDataSource | undefined = this.sqlDataSourceService.get(state.editorId);
+
+    if (!dataSource) {
+      return;
+    }
+
+    if (action === ACTION_SAVE_AS_SCRIPT) {
+      let projectId = dataSource.executionContext?.projectId ?? null;
+      await this.projectInfoResource.load(CachedMapAllKey);
+      const name = getSqlEditorName(state, dataSource);
+
+      if (projectId) {
+        const project = this.projectInfoResource.get(projectId);
+
+        if (!project?.canEditResources) {
+          projectId = null;
+        }
+      }
+
+      const result = await this.commonDialogService.open(SaveScriptDialog, {
+        defaultScriptName: name,
+        projectId,
+        validation: async ({ name, projectId }, setMessage) => {
+          const trimmedName = name.trim();
+
+          if (!projectId || !trimmedName.length) {
+            return false;
+          }
+
+          if (!RESOURCE_NAME_REGEX.test(trimmedName)) {
+            setMessage('plugin_resource_manager_scripts_script_name_invalid_characters_message');
+            return false;
+          }
+
+          const project = this.projectInfoResource.get(projectId);
+          const nameWithExtension = this.projectInfoResource.getNameWithExtension(projectId, SCRIPTS_TYPE_ID, trimmedName);
+          const rootFolder = project ? this.resourceManagerScriptsService.getRootFolder(project) : undefined;
+          const key = getRmResourcePath(projectId, rootFolder);
+
+          try {
+            await this.resourceManagerResource.load(CachedTreeChildrenKey(key));
+            return !this.resourceManagerResource.has(createPath(key, nameWithExtension));
+          } catch (exception: any) {
+            return false;
+          }
+        },
+      });
+
+      if (result !== DialogueStateResult.Rejected && result !== DialogueStateResult.Resolved) {
+        try {
+          projectId = result.projectId;
+
+          if (!projectId) {
+            throw new Error('Project not selected');
+          }
+
+          const project = this.projectInfoResource.get(projectId);
+          if (!project) {
+            throw new Error('Project not found');
+          }
+
+          const nameWithoutExtension = result.name.trim();
+          const scriptName = this.projectInfoResource.getNameWithExtension(projectId, SCRIPTS_TYPE_ID, nameWithoutExtension);
+          const scriptsRootFolder = this.resourceManagerScriptsService.getRootFolder(project);
+          const folderResourceKey = getResourceKeyFromNodeId(createPath(RESOURCES_NODE_PATH, projectId, scriptsRootFolder));
+
+          if (!folderResourceKey) {
+            this.notificationService.logError({ title: 'ui_error', message: 'plugin_sql_editor_navigation_tab_resource_save_script_error' });
+            return;
+          }
+
+          const resourceKey = createPath(folderResourceKey, scriptName);
+
+          await this.resourceManagerScriptsService.createScript(resourceKey, dataSource.executionContext, dataSource.script);
+
+          dataSource = this.sqlDataSourceService.create(state, ResourceSqlDataSource.key, {
+            script: dataSource.script,
+            executionContext: dataSource.executionContext,
+          });
+
+          (dataSource as ResourceSqlDataSource).setResourceKey(resourceKey);
+
+          this.notificationService.logSuccess({
+            title: 'plugin_sql_editor_navigation_tab_resource_save_script_success',
+            message: nameWithoutExtension,
+          });
+
+          if (!this.resourceManagerScriptsService.active) {
+            this.resourceManagerScriptsService.togglePanel();
+          }
+        } catch (exception) {
+          this.notificationService.logException(exception as any, 'plugin_sql_editor_navigation_tab_resource_save_script_error');
+        }
+      }
+    }
   }
 
   private canOpenHandler(data: INodeNavigationData, contexts: IExecutionContextProvider<INodeNavigationData>): void {
