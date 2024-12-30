7fb4f1ac24851bce42acd19333bf9d30c1f81c9a
Andrey Sychev <nukemoore@gmail.com>
2024-12-30 14:43:33 +0100

    CB-5832 fix: ru localization

--- webapp/packages/plugin-help/src/locales/ru.ts
+++ webapp/packages/plugin-help/src/locales/ru.ts
@@ -21,7 +21,7 @@ export default [
   ['sql_editor_shortcut_execute_script', 'Выполнить скрипт'],
   ['sql_editor_shortcut_show_execution_plan', 'Показать план выполнения'],
   ['sql_editor_shortcut_format', 'Форматировать скрипт'],
-  ['sql_editor_shortcut_save_as_script', 'Сохранить скрипт'],
+  ['sql_editor_shortcut_save_as_script', 'Сохранить как скрипт'],
   ['sql_editor_shortcut_open_editor_in_new_tab', 'Открыть SQL редактор в новой бразуерной вкладке'],
   ['sql_editor_shortcut_undo', 'Отменить'],
   ['sql_editor_shortcut_redo', 'Повторить'],
