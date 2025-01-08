d9f5cf51604f952bd4a13f802861f6ad2ff2e341
Andrey Sychev <nukemoore@gmail.com>
2025-01-08 19:50:28 +0100

    CB-5756 feat: redesign user profile

--- webapp/packages/plugin-user-profile/src/UserProfileForm/UserAuthenticationPart/ChangePassword.tsx
+++ webapp/packages/plugin-user-profile/src/UserProfileForm/UserAuthenticationPart/ChangePassword.tsx
@@ -6,19 +6,40 @@
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
+import { useState } from 'react';
 
-import { Form, Group, GroupTitle, InputField, useCustomInputValidation, usePasswordValidation, useTranslate } from '@cloudbeaver/core-blocks';
+import { UserInfoResource } from '@cloudbeaver/core-authentication';
+import {
+  ColoredContainer,
+  Container,
+  Form,
+  Group,
+  GroupTitle,
+  InputField,
+  ToolsAction,
+  ToolsPanel,
+  useCustomInputValidation,
+  useForm,
+  usePasswordValidation,
+  useTranslate,
+} from '@cloudbeaver/core-blocks';
+import { useService } from '@cloudbeaver/core-di';
+import { NotificationService } from '@cloudbeaver/core-events';
 import { isValuesEqual } from '@cloudbeaver/core-utils';
 
 import type { IUserProfileFormAuthenticationState } from './IUserProfileFormAuthenticationState.js';
 
-interface Props {
-  state: IUserProfileFormAuthenticationState;
-  disabled?: boolean;
-}
-
-export const ChangePassword = observer<Props>(function ChangePassword({ state, disabled }) {
+export const ChangePassword = observer(function ChangePassword() {
+  const INITIAL_STATE: IUserProfileFormAuthenticationState = {
+    oldPassword: '',
+    password: '',
+    repeatedPassword: '',
+  };
   const translate = useTranslate();
+  const [state, setState] = useState<IUserProfileFormAuthenticationState>(INITIAL_STATE);
+  const notificationService = useService(NotificationService);
+  const userInfoResource = useService(UserInfoResource);
+  const disabled = userInfoResource.isLoading();
   const passwordValidationRef = usePasswordValidation();
   const passwordRepeatRef = useCustomInputValidation<string>(value => {
     if (!isValuesEqual(value, state.password, null)) {
@@ -27,47 +48,83 @@ export const ChangePassword = observer<Props>(function ChangePassword({ state, d
     return null;
   });
 
+  const form = useForm({
+    async onSubmit() {
+      try {
+        await userInfoResource.updateLocalPassword(state.oldPassword, state.password);
+        resetForm();
+        notificationService.logSuccess({ title: 'plugin_user_profile_authentication_change_password_success' });
+      } catch (exception) {
+        if (exception instanceof Error) {
+          notificationService.logError({ title: exception.name, message: exception.message });
+        }
+      }
+    },
+  });
+
+  function resetForm() {
+    setState(() => INITIAL_STATE);
+    form.ref?.reset();
+  }
+
   return (
-    <Form>
-      <Group form gap>
-        <GroupTitle>{translate('plugin_user_profile_authentication_change_password')}</GroupTitle>
-        <InputField
-          type="password"
-          name="oldPassword"
-          state={state}
-          readOnly={disabled}
-          mapValue={(value?: string) => value?.trim() ?? ''}
-          small
-          required
-        >
-          {translate('plugin_user_profile_authentication_change_password_current_password')}
-        </InputField>
-        <InputField
-          ref={passwordValidationRef}
-          type="password"
-          name="password"
-          autoComplete="new-password"
-          state={state}
-          readOnly={disabled}
-          mapValue={(value?: string) => value?.trim() ?? ''}
-          small
-          required
-        >
-          {translate('plugin_user_profile_authentication_change_password_new_password')}
-        </InputField>
-        <InputField
-          ref={passwordRepeatRef}
-          type="password"
-          name="repeatedPassword"
-          state={state}
-          readOnly={disabled}
-          mapValue={(value?: string) => value?.trim() ?? ''}
-          small
-          required
-        >
-          {translate('plugin_user_profile_authentication_change_password_repeat_password')}
-        </InputField>
-      </Group>
-    </Form>
+    <ColoredContainer wrap overflow gap>
+      <Container medium gap>
+        <Form context={form} onChange={value => setState(prev => ({ ...prev, value }))}>
+          <ColoredContainer parent overflow compact vertical noWrap gap>
+            <Group overflow box keepSize>
+              <ToolsPanel rounded minHeight>
+                <ToolsAction icon="admin-save" viewBox="0 0 24 24" onClick={() => form.submit()}>
+                  {translate('plugin_user_profile_authentication_change_password_submit_label')}
+                </ToolsAction>
+                <ToolsAction icon="admin-cancel" viewBox="0 0 24 24" onClick={resetForm}>
+                  {translate('ui_processing_cancel')}
+                </ToolsAction>
+              </ToolsPanel>
+            </Group>
+
+            <Group form gap>
+              <GroupTitle>{translate('plugin_user_profile_authentication_change_password')}</GroupTitle>
+              <InputField
+                type="password"
+                name="oldPassword"
+                state={state}
+                readOnly={disabled}
+                mapValue={(value?: string) => value?.trim() ?? ''}
+                small
+                required
+              >
+                {translate('plugin_user_profile_authentication_change_password_current_password')}
+              </InputField>
+              <InputField
+                ref={passwordValidationRef}
+                type="password"
+                name="password"
+                autoComplete="new-password"
+                state={state}
+                readOnly={disabled}
+                mapValue={(value?: string) => value?.trim() ?? ''}
+                small
+                required
+              >
+                {translate('plugin_user_profile_authentication_change_password_new_password')}
+              </InputField>
+              <InputField
+                ref={passwordRepeatRef}
+                type="password"
+                name="repeatedPassword"
+                state={state}
+                readOnly={disabled}
+                mapValue={(value?: string) => value?.trim() ?? ''}
+                small
+                required
+              >
+                {translate('plugin_user_profile_authentication_change_password_repeat_password')}
+              </InputField>
+            </Group>
+          </ColoredContainer>
+        </Form>
+      </Container>
+    </ColoredContainer>
   );
 });
--- webapp/packages/plugin-user-profile/src/manifest.ts
+++ webapp/packages/plugin-user-profile/src/manifest.ts
@@ -17,10 +17,7 @@ export const userProfilePlugin: PluginManifest = {
     () => import('./LocaleService.js').then(m => m.LocaleService),
     () => import('./UserProfileTabsService.js').then(m => m.UserProfileTabsService),
     () => import('./UserProfileOptionsPanelService.js').then(m => m.UserProfileOptionsPanelService),
-    () => import('./UserProfileForm/UserProfileFormBootstrap.js').then(m => m.UserProfileFormBootstrap),
-    () => import('./UserProfileForm/UserProfileFormService.js').then(m => m.UserProfileFormService),
     () => import('./UserProfileForm/UserInfoPart/UserProfileFormInfoPartBootstrap.js').then(m => m.UserProfileFormInfoPartBootstrap),
-    () => import('./UserProfileForm/UserInfoPart/UserProfileFormInfoPartService.js').then(m => m.UserProfileFormInfoPartService),
     () =>
       import('./UserProfileForm/UserAuthenticationPart/UserProfileFormAuthenticationPartBootstrap.js').then(
         m => m.UserProfileFormAuthenticationPartBootstrap,
