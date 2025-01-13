1a84ad061954902fbb550bd366d4ff9e90af6e40
Andrey Sychev <nukemoore@gmail.com>
2025-01-13 12:44:26 +0100

    CB-6063 fix: hasRowIdentifier in getResult

--- webapp/packages/plugin-data-viewer/src/DatabaseDataModel/DatabaseDataSource.ts
+++ webapp/packages/plugin-data-viewer/src/DatabaseDataModel/DatabaseDataSource.ts
@@ -207,7 +207,7 @@ export abstract class DatabaseDataSource<TOptions, TResult extends IDatabaseData
   }
 
   hasElementIdentifier(resultIndex: number): boolean {
-    return this.getResult(resultIndex)?.data?.hasElementIdentifier === true;
+    return this.getResult(resultIndex)?.data?.hasRowIdentifier === true;
   }
 
   isReadonly(resultIndex: number): boolean {
