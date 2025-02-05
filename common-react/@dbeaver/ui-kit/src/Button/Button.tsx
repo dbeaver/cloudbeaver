e0896aa88c222a78d0ac1dc2428b980650b5a829
Andrey Sychev <nukemoore@gmail.com>
2025-02-05 10:52:43 +0100

    feat: add ui-kit package to common-react

---
+++ common-react/@dbeaver/ui-kit/src/Button/Button.tsx
@@ -0,0 +1,6 @@
+import { Button as AriaButton } from "@ariakit/react";
+import "./button.css";
+
+export function Button() {
+  return <AriaButton className="ak-button ak-button-default">Button</AriaButton>;
+}
