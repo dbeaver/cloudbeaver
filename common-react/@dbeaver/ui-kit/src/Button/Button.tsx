74241c0b5f62a0e45074ac6f30520c9ef75ae6a5
Andrey Sychev <nukemoore@gmail.com>
2025-02-10 10:34:47 +0100

    feat: create simple button with 2 variants

--- common-react/@dbeaver/ui-kit/src/Button/Button.tsx
+++ common-react/@dbeaver/ui-kit/src/Button/Button.tsx
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-import { Button as AriaButton, type ButtonProps } from "@ariakit/react";
-import "./Button.css";
+import { Button as AriaButton, type ButtonProps } from '@ariakit/react';
+import './Button.css';
 
 interface UiKitButtonProps extends ButtonProps {
-  variant?: "primary" | "secondary";
+  variant?: 'primary' | 'secondary';
 }
 
-export function Button({className, variant = "primary", ...props}: UiKitButtonProps) {
-  const classToApply = `btn-${variant}`;
+export function Button({ className, variant = 'primary', ...props }: UiKitButtonProps) {
+  const classToApply = `btn btn-${variant}`;
 
   return <AriaButton className={className ?? '' + ' ' + classToApply} {...props} />;
 }
