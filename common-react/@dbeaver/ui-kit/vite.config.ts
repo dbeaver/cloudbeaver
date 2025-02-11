c88f33ef27aa8a415237daebff5511a4a5eb3659
Andrey Sychev <nukemoore@gmail.com>
2025-02-11 14:30:23 +0100

    fix: mdx stories support

--- common-react/@dbeaver/ui-kit/vite.config.ts
+++ common-react/@dbeaver/ui-kit/vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
+import { assetResolverPlugin } from '@wroud/vite-plugin-asset-resolver';
 
 /** @type {import('vite').UserConfig} */
 export default {
-    plugins: [tailwindcss()],
-  }
+  plugins: [tailwindcss(), assetResolverPlugin()],
+}
