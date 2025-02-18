ce1b82a6f75a23bccfa66d1cb58b12ed6ea588f7
Wroud <wrouds@gmail.com>
2025-02-19 01:13:36 +0800

    chore: try to fix strange build bug

--- common-react/@dbeaver/react-data-grid/src/mapEditCellRenderer.tsx
+++ common-react/@dbeaver/react-data-grid/src/mapEditCellRenderer.tsx
@@ -1,6 +1,6 @@
 import type { RenderEditCellProps } from 'react-data-grid';
 import type { IInnerRow } from './IInnerRow.js';
-import { TextEditor } from './editors/TextEditor.js';
+import { TextEditor } from './editors/TextEditor2.js';
 
 export function mapEditCellRenderer({ rowIdx, column, onClose }: RenderEditCellProps<IInnerRow, unknown>) {
   return <TextEditor rowIdx={rowIdx} colIdx={column.idx} onClose={() => onClose()} />;
