aae41f0eee8d1041a86911a1b6e389d49896aa84
Andrey Sychev <nukemoore@gmail.com>
2025-02-25 12:42:17 +0100

    build: add vite plugin

--- common-react/@dbeaver/ui-kit/vite.config.ts
+++ common-react/@dbeaver/ui-kit/vite.config.ts
@@ -1,7 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import { assetResolverPlugin } from '@wroud/vite-plugin-asset-resolver';
+import { tscPlugin } from '@wroud/vite-plugin-tsc';
 
 /** @type {import('vite').UserConfig} */
 export default {
-  plugins: [tailwindcss(), assetResolverPlugin()],
-}
+  plugins: [
+    tailwindcss(),
+    assetResolverPlugin(),
+    tscPlugin({
+      tscArgs: ['-b'],
+      enableOverlay: true,
+      prebuild: true,
+    }),
+  ],
+};
