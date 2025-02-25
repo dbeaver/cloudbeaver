666c1d11279c954bf399a964413d5ff18f352b63
Ruslan <pupkinvasya7000@gmail.com>
2025-02-25 16:41:01 +0100

    CB-6271 add timeout of inaction in configuration mode

--- server/bundles/io.cloudbeaver.server.ce/src/io/cloudbeaver/server/jetty/CBJettyServer.java
+++ server/bundles/io.cloudbeaver.server.ce/src/io/cloudbeaver/server/jetty/CBJettyServer.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -94,7 +94,7 @@ public class CBJettyServer {
                 ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
                 servletContextHandler.setBaseResourceAsPath(contentRootPath);
                 servletContextHandler.addFilter(
-                    new FilterHolder(new ServerConfigurationTimeLimitFilter()), "/*", null);
+                    new FilterHolder(new ServerConfigurationTimeLimitFilter(application)), "/*", null);
                 String rootURI = serverConfiguration.getRootURI();
                 servletContextHandler.setContextPath(rootURI);
 
--- server/bundles/io.cloudbeaver.server.ce/src/io/cloudbeaver/server/jetty/filters/ServerConfigurationTimeLimitFilter.java
+++ server/bundles/io.cloudbeaver.server.ce/src/io/cloudbeaver/server/jetty/filters/ServerConfigurationTimeLimitFilter.java
@@ -1,11 +1,26 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.cloudbeaver.server.jetty.filters;
 
-import io.cloudbeaver.model.app.BaseServletApplication;
+import io.cloudbeaver.server.CBApplication;
 import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jkiss.dbeaver.Log;
-import org.jkiss.dbeaver.runtime.DBWorkbench;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -13,38 +28,24 @@ import java.time.Duration;
 public class ServerConfigurationTimeLimitFilter implements Filter {
     private static final Log log = Log.getLog(ServerConfigurationTimeLimitFilter.class);
 
-    private static final int MINUTES_OF_INACTION_BEFORE_DISABLING_REQUEST_PROCESSING = 60;
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
+    private static final int MINUTES_OF_INACTION_BEFORE_DISABLING_REQUEST_PROCESSING = 2;
+    private final CBApplication<?> application;
+
+    public ServerConfigurationTimeLimitFilter(CBApplication<?> application) {
+        this.application = application;
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        System.out.println("Incoming request: " + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+        boolean isOutOfTime = System.currentTimeMillis() - application.getApplicationStartTime()
+            > Duration.ofMinutes(MINUTES_OF_INACTION_BEFORE_DISABLING_REQUEST_PROCESSING).toMillis();
+        if (application.isConfigurationMode() && isOutOfTime) {
+            log.warn("Server configuration time has expired. A restart server is required to continue");
+            buildErrorResponse((HttpServletResponse) response);
+            return;
 
-        if (DBWorkbench.getPlatform().getApplication() instanceof BaseServletApplication servletApplication) {
-            if (!servletApplication.isRequestProcessingEnabled()) {
-                buildErrorResponse((HttpServletResponse) response);
-                return;
-            }
-            boolean isOutOfTime = System.currentTimeMillis() - servletApplication.getApplicationStartTime()
-                > Duration.ofMinutes(MINUTES_OF_INACTION_BEFORE_DISABLING_REQUEST_PROCESSING).toMillis();
-            if (servletApplication.isConfigurationMode() && isOutOfTime) {
-                log.warn("Server configuration time has expired. A restart server is required to continue");
-                servletApplication.disableRequestProcessing();
-                buildErrorResponse((HttpServletResponse) response);
-                return;
-
-            }
         }
         chain.doFilter(request, response);
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
     }
 
     private void buildErrorResponse(HttpServletResponse response) throws IOException {
