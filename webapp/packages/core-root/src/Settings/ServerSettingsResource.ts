c889640d0df343a6502a7d848bcc33a95a0ae70d
naumov <iamemptyhuh@gmail.com>
2025-03-10 16:17:36 +0100

    CB-6042 extend sync dep

--- webapp/packages/core-root/src/Settings/ServerSettingsResource.ts
+++ webapp/packages/core-root/src/Settings/ServerSettingsResource.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@ import { injectable } from '@cloudbeaver/core-di';
 import { CachedDataResource } from '@cloudbeaver/core-resource';
 import { GraphQLService, type ProductSettings } from '@cloudbeaver/core-sdk';
 
-import { ServerConfigResource } from '../ServerConfigResource.js';
+import { SessionDataResource } from '../SessionDataResource.js';
 
 export type ServerSettings = ProductSettings;
 
@@ -17,16 +17,14 @@ export type ServerSettings = ProductSettings;
 export class ServerSettingsResource extends CachedDataResource<ServerSettings | null> {
   constructor(
     private readonly graphQLService: GraphQLService,
-    serverConfigResource: ServerConfigResource,
+    sessionDataResource: SessionDataResource,
   ) {
     super(() => null, undefined, []);
-
-    this.sync(serverConfigResource);
+    this.sync(sessionDataResource);
   }
 
   protected async loader(): Promise<ServerSettings> {
     const { settings } = await this.graphQLService.sdk.getProductSettings();
-
     return settings as ServerSettings;
   }
 }
