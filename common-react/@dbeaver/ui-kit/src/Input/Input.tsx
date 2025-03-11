bf571420016d13b42f324ea292b8b91e998f278e
Andrey Sychev <nukemoore@gmail.com>
2025-03-11 16:16:24 +0100

    refactor: unify controls, add new global control css variables

--- common-react/@dbeaver/ui-kit/src/Input/Input.tsx
+++ common-react/@dbeaver/ui-kit/src/Input/Input.tsx
@@ -6,18 +6,25 @@
  * you may not use this file except in compliance with the License.
  */
 
-import type { ComponentPropsWithRef } from 'react';
+import { useId, type ComponentPropsWithRef } from 'react';
 import './Input.css';
 
 export interface InputProps extends Omit<ComponentPropsWithRef<'input'>, 'size'> {
   size?: 'small' | 'medium' | 'large' | 'xlarge';
+  label?: React.ReactNode;
 }
 
 export const Input: React.FC<InputProps> = function Input({ size, className, ...props }) {
+  const id = useId();
   const classNameToApply = `dbv-kit-input dbv-kit-input--${size ?? 'medium'} ${className}`.trim();
   return (
     <div className="dbv-kit-input-wrapper">
-      <input className={classNameToApply} {...props} />
+      {props.label && (
+        <label htmlFor={id} className={`dbv-kit-input__label ${props.required ? 'dbv-kit-input__label--required' : ''}`}>
+          {props.label}
+        </label>
+      )}
+      <input id={id} className={classNameToApply} {...props} />
     </div>
   );
 };
