c71c19d17a9732df3de2b7da8c130eb0d8a2e451
Andrey Sychev <nukemoore@gmail.com>
2025-04-11 11:38:46 +0200

    #5362 refactor: remove value prop from BlobFormatter

--- common-react/@dbeaver/react-data-grid/src/formatters/BlobFormatter.tsx
+++ common-react/@dbeaver/react-data-grid/src/formatters/BlobFormatter.tsx
@@ -1,16 +1,11 @@
-import { NullFormatter } from './NullFormatter.js';
 import { clsx } from '@dbeaver/ui-kit';
 
 interface Props {
-  value: unknown;
   className?: string;
   onClick?: React.MouseEventHandler<HTMLElement>;
 }
 
-export function BlobFormatter({ value, className, onClick }: Props) {
-  if (value === null) {
-    return <NullFormatter className={className} onClick={onClick} />;
-  }
+export function BlobFormatter({ className, onClick }: Props) {
   return (
     <span className={clsx('tw:uppercase', className)} onClick={onClick}>
       [blob]
