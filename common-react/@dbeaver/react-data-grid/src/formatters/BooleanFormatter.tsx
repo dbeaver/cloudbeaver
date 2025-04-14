e3c7fb498fdd2763f809647673ea926040a81e76
Andrey Sychev <nukemoore@gmail.com>
2025-04-14 10:29:41 +0200

    #5440 refactor: split onClick and onKeyDown interfaces

--- common-react/@dbeaver/react-data-grid/src/formatters/BooleanFormatter.tsx
+++ common-react/@dbeaver/react-data-grid/src/formatters/BooleanFormatter.tsx
@@ -4,14 +4,15 @@ import { Checkbox, clsx, Focusable } from '@dbeaver/ui-kit';
 interface Props {
   value: boolean | null;
   className?: string;
-  onClick?: () => void;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+  onKeyDown?: React.KeyboardEventHandler<HTMLElement>;
 }
 
-export function BooleanFormatter({ value, className, onClick }: Props) {
+export function BooleanFormatter({ value, className, onClick, onKeyDown }: Props) {
   const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
     if (event.code === 'Enter' || event.code === 'Space') {
       event.preventDefault();
-      onClick?.();
+      onKeyDown?.(event);
     }
   };
   return (
