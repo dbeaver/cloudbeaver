6e7229f817a46adcb97b1fdc4a71c58291216a4e
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-04-16 17:36:04 +0200

    CB-5465 fixes checkbox defaultChecked wrong behavior

--- webapp/packages/core-blocks/src/FormControls/Checkboxes/useCheckboxState.ts
+++ webapp/packages/core-blocks/src/FormControls/Checkboxes/useCheckboxState.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,8 @@ export function useCheckboxState<TKey extends string>(options: CheckboxStateOpti
     } else {
       checked = !!currentState;
     }
+  } else if (name !== undefined && state !== undefined) {
+    checked = false;
   }
 
   if (options.inverse) {
