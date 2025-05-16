df669a7ecfc47dfa08b4fdee07f936a594ec5b6f
Andrey Sychev <nukemoore@gmail.com>
2025-05-16 12:06:46 +0200

    dbeaver/pro#5554 refactor: create content wrapper, move general styles there, add loader wrapper

--- common-react/@dbeaver/ui-kit/src/Button/ButtonRoot.tsx
+++ common-react/@dbeaver/ui-kit/src/Button/ButtonRoot.tsx
@@ -1,22 +1,23 @@
 import { Button as AriaButton, type ButtonProps as AriaKitButtonProps } from '@ariakit/react';
 import { Spinner } from '../Spinner/Spinner.js';
 import { clsx } from '../utils/clsx.js';
+import './ButtonRoot.css';
 
 export interface ButtonRootProps extends Omit<AriaKitButtonProps, 'clickOnEnter' | 'clickOnSpace'> {
   loading?: boolean;
   loader?: React.ReactNode;
 }
 
-export function ButtonRoot({ loading, loader, children, onClick, ...props }: ButtonRootProps) {
+export function ButtonRoot({ loading, loader, children, onClick, className, ...props }: ButtonRootProps) {
   if (loading) {
     props['aria-busy'] = true;
     props['data-loading'] = true;
   }
 
   return (
-    <AriaButton onClick={loading ? () => null : onClick} {...props}>
-      {loading && (loader ? loader : <Spinner className="tw:absolute" />)}
-      <span className={clsx(loading && 'tw:opacity-0', 'tw:w-full')}>{children}</span>
+    <AriaButton onClick={loading ? () => null : onClick} className={clsx(className, 'tw:relative')} {...props}>
+      <div className={clsx('dbv-kit-button__content', loading && 'tw:opacity-0')}>{children}</div>
+      {loading && <div className="dbv-kit-button__loader">{loader ? loader : <Spinner />}</div>}
     </AriaButton>
   );
 }
