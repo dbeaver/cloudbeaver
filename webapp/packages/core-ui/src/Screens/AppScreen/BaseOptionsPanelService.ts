5800b4e2414514ae640f2e70ed27b4b9c32f6274
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-05-26 13:34:30 +0200

    dbeaver/pro#5173 fix: does not loads deleted driver info after

--- webapp/packages/core-ui/src/Screens/AppScreen/BaseOptionsPanelService.ts
+++ webapp/packages/core-ui/src/Screens/AppScreen/BaseOptionsPanelService.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -55,6 +55,12 @@ export abstract class BaseOptionsPanelService<T> {
     const result = await this.optionsPanelService.close();
 
     if (result) {
+      this.dispose();
+    }
+  }
+
+  dispose(): void {
+    if (this.itemId) {
       this.itemId = null;
     }
   }
