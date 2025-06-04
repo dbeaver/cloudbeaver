223f7c1a47aba43a541fca21b21c75f94ee24a14
mayerro <roman.kunitskiy@gmail.com>
2025-06-04 14:06:34 +0200

    dbeaver/dbeaver-devops#1941 disabled checks in codacy page

--- webapp/packages/core-ui/src/locales/ru.ts
+++ webapp/packages/core-ui/src/locales/ru.ts
@@ -1,6 +1,7 @@
 export default [
   ['core_ui_empty', 'Пусто'],
   ['core_ui_empty_empty', 'Пусто, Пусто'],
+  ['core_ui_empty_empty_emptyboy', 'Пусто, Пусто, Пустобой'],
   ['core_ui_form_save_error', 'Ошибка при сохранении формы'],
   ['core_ui_switch_tab_error', 'Невозможно переключить вкладку во время валидации формы'],
 ];
