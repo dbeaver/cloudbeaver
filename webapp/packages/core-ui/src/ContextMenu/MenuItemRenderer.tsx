6ae3ff9d137ee84e6b365ea91989a97fdea53360
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-06-06 08:38:00 +0200

    removes secondary from menu item renderer checkbox

--- webapp/packages/core-ui/src/ContextMenu/MenuItemRenderer.tsx
+++ webapp/packages/core-ui/src/ContextMenu/MenuItemRenderer.tsx
@@ -89,7 +89,7 @@ export const MenuItemRenderer = observer<IMenuItemRendererProps>(function MenuIt
         checked={item.checked}
         onClick={() => onClick(false)}
       >
-        <MenuItemElement label={item.label} icon={<Checkbox checked={item.checked} size="small" variant="secondary" />} tooltip={item.tooltip} />
+        <MenuItemElement label={item.label} icon={<Checkbox checked={item.checked} size="small" />} tooltip={item.tooltip} />
       </MenuItemCheckbox>
     );
   }
