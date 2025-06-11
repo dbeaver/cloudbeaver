c20c93820f43db3c6804366603c874623d9ab690
naumov <iamemptyhuh@gmail.com>
2025-06-11 22:52:15 +0200

    dbeaver/pro#5538 add general delete message

--- webapp/packages/core-localization/src/locales/fr.ts
+++ webapp/packages/core-localization/src/locales/fr.ts
@@ -135,6 +135,7 @@ export default [
   ['ui_project', 'Project'],
   ['ui_type', 'Type'],
   ['ui_field_is_required', 'Please fill in this field'],
+  ['ui_delete_confirmation_message', 'You are about to delete "{arg:item}". Are you sure?'],
 
   ['root_permission_denied', "Vous n'avez pas les permissions"],
   ['root_permission_no_permission', "Vous n'avez pas la permission pour cette action"],
