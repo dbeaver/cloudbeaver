141b2ee4df10cbcf41186020e989246882ec31ca
Sychev Andrey <nukemoore@gmail.com>
2025-06-16 16:34:15 +0200

    dbeaver/pro#4745 refactor: remove async

--- webapp/packages/plugin-navigation-tree/src/TreeNew/useTree.ts
+++ webapp/packages/plugin-navigation-tree/src/TreeNew/useTree.ts
@@ -27,7 +27,7 @@ export interface ITree {
   openNode(id: string): Promise<void>;
   clickNode(id: string): Promise<void>;
   expandNode(id: string, state: boolean): Promise<void>;
-  selectNode(id: string, selected?: boolean): Promise<void>;
+  selectNode(id: string, selected?: boolean): void;
 }
 
 export function useTree(options: IOptions): ITree {
@@ -71,10 +71,10 @@ export function useTree(options: IOptions): ITree {
           if (state) {
             await options.data.load(id, true);
           }
-        } catch (exception) {}
+        } catch (exception) { }
       },
-      async selectNode(id: string, selected = true) {
-        options.data.updateState(id, { selected});
+      selectNode(id: string, selected = true) {
+        options.data.updateState(id, { selected });
       },
     }),
     {},
