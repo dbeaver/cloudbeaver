f4131be61256e6ee4b2617a1dd7727f27e3e5b23
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-07-07 12:34:58 +0200

    adds more proper setting description

--- webapp/packages/plugin-navigation-tabs/src/locales/en.ts
+++ webapp/packages/plugin-navigation-tabs/src/locales/en.ts
@@ -2,5 +2,5 @@ export default [
   ['plugin_navigation_tabs_welcome_start', 'Start'],
   ['plugin_navigation_tabs_navigation_tabs_group', 'Navigation Tabs'],
   ['plugin_navigation_tabs_multiple_rows', 'Multiple rows'],
-  ['plugin_navigation_tabs_multiple_rows_description', 'Enable multiple rows for navigation tabs.'],
+  ['plugin_navigation_tabs_multiple_rows_description', 'Display navigation tabs in multiple rows instead of scrolling.'],
 ];
--- webapp/packages/plugin-navigation-tabs/src/locales/ru.ts
+++ webapp/packages/plugin-navigation-tabs/src/locales/ru.ts
@@ -2,5 +2,5 @@ export default [
   ['plugin_navigation_tabs_welcome_start', 'Начать'],
   ['plugin_navigation_tabs_navigation_tabs_group', 'Навигационные вкладки'],
   ['plugin_navigation_tabs_multiple_rows', 'Мульти строчные вкладки'],
-  ['plugin_navigation_tabs_multiple_rows_description', 'Включить мульти строчные вкладки для навигации.'],
+  ['plugin_navigation_tabs_multiple_rows_description', 'Выключить прокрутку навигационных вкладок и отображать их в нескольких строках.'],
 ];
