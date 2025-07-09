631cbe2b3ef08bace706391baeb26a9fc17a4ddf
Sychev Andrey <nukemoore@gmail.com>
2025-07-09 10:37:21 +0200

    dbeaver/pro#4990 refactor: use proper product name

--- webapp/packages/plugin-connections/src/ConnectionForm/SSL/SSLDescription.tsx
+++ webapp/packages/plugin-connections/src/ConnectionForm/SSL/SSLDescription.tsx
@@ -6,17 +6,21 @@
  * you may not use this file except in compliance with the License.
  */
 
-import { IconOrImage, useTranslate } from '@cloudbeaver/core-blocks';
+import { IconOrImage, useResource, useTranslate } from '@cloudbeaver/core-blocks';
+import { ProductInfoResource } from '@cloudbeaver/core-root';
 import type { ReactNode } from 'react';
 
 export function SSLDescription(): ReactNode {
   const translate = useTranslate();
+  const productInfoResource = useResource(SSLDescription, ProductInfoResource, undefined);
+  const productName = productInfoResource.data?.name || 'CloudBeaver';
 
   return (
     <>
+      <div>{translate('plugin_connections_connection_ssl_optional')}</div>
       <div className="tw:text-balance">{translate('plugin_connections_connection_ssl_description')}</div>
       <div className="tw:flex tw:items-center tw:gap-4 tw:mt-2 tw:text-balance">
-        <IconOrImage icon="/icons/info_icon_sm.svg" /> {translate('plugin_connections_connection_ssl_note')}
+        <IconOrImage icon="/icons/info_icon_sm.svg" /> {translate('plugin_connections_connection_ssl_note', undefined, { productName })}
       </div>
     </>
   );
