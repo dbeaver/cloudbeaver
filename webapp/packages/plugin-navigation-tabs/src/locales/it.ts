e7c0f73a14069d32f333e496bd13922aae71a428
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-07-10 14:20:06 +0200

    removes extra dots

--- webapp/packages/plugin-navigation-tabs/src/locales/it.ts
+++ webapp/packages/plugin-navigation-tabs/src/locales/it.ts
@@ -2,5 +2,5 @@ export default [
   ['plugin_navigation_tabs_welcome_start', 'Inizia'],
   ['plugin_navigation_tabs_navigation_tabs_group', 'Navigation Tabs'],
   ['plugin_navigation_tabs_multiple_rows', 'Multiple rows'],
-  ['plugin_navigation_tabs_multiple_rows_description', 'Display navigation tabs in multiple rows instead of scrolling.'],
+  ['plugin_navigation_tabs_multiple_rows_description', 'Display navigation tabs in multiple rows instead of scrolling'],
 ];
--- webapp/packages/plugin-navigation-tabs/src/locales/vi.ts
+++ webapp/packages/plugin-navigation-tabs/src/locales/vi.ts
@@ -2,5 +2,5 @@ export default [
   ['plugin_navigation_tabs_welcome_start', 'Bắt đầu'],
   ['plugin_navigation_tabs_navigation_tabs_group', 'Navigation Tabs'],
   ['plugin_navigation_tabs_multiple_rows', 'Multiple rows'],
-  ['plugin_navigation_tabs_multiple_rows_description', 'Enable multiple rows for navigation tabs.'],
+  ['plugin_navigation_tabs_multiple_rows_description', 'Enable multiple rows for navigation tabs'],
 ];
