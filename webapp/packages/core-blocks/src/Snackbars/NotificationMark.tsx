2ddba85a78d2af3a4bbd6a35d6f92c94e77af190
Sychev Andrey <nukemoore@gmail.com>
2025-07-10 13:39:28 +0200

    dbeaver/pro#5241 refactor: change role for snackbar
    aria-live polite works with aria regions which initially empty and then dynamically populated with some data. If we want assistive technologies read initial content, we need to use alert role.
    
    also hide icon from assistive technologies (no additional information from that)

--- webapp/packages/core-blocks/src/Snackbars/NotificationMark.tsx
+++ webapp/packages/core-blocks/src/Snackbars/NotificationMark.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ type IconProps = HTMLProps<HTMLDivElement> & {
 
 export const NotificationMark: React.FC<IconProps> = function NotificationMark({ type, ...props }) {
   return (
-    <div {...props}>
+    <div aria-hidden {...props}>
       {type === ENotificationType.Info && <IconOrImage icon="/icons/info_icon.svg" />}
       {type === ENotificationType.Error && <IconOrImage icon="/icons/error_icon.svg" />}
       {type === ENotificationType.Success && <IconOrImage icon="/icons/success_icon.svg" />}
