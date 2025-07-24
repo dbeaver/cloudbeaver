ea30a7c553ad189a1e0ef81f4934fc03baf4efb0
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-07-24 13:25:42 +0200

    dbeaver/pro#5108 create plugin-data-editor

---
+++ webapp/packages/plugin-data-editor-settings/src/manifest.ts
@@ -0,0 +1,17 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
+import type { PluginManifest } from '@cloudbeaver/core-di';
+
+export const pluginDataEditorSettingsManifest: PluginManifest = {
+  info: {
+    name: 'Plugin data editor settings',
+  },
+
+  providers: [() => import('./PluginDataEditorSettingsServiceBootstrap.js').then(m => m.PluginDataEditorSettingsServiceBootstrap)],
+};
