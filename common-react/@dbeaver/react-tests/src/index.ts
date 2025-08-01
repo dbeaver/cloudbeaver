e8f8c3a8851fe3572769c4165a5fdf95ce9c0d29
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-08-01 12:54:29 +0200

    implements react-testing library into @dbeaver/react-tests

--- common-react/@dbeaver/react-tests/src/index.ts
+++ common-react/@dbeaver/react-tests/src/index.ts
@@ -1,2 +1,1 @@
 export { default as ReactVitestConfig } from './vitest.config.js';
-export * from './vitest.setup.js';
--- common-react/@dbeaver/react-tests/src/vitest.setup.ts
+++ common-react/@dbeaver/react-tests/src/vitest.setup.ts
@@ -5,8 +5,8 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-// TODO throws an error if uncomment
-// import '@testing-library/jest-dom/vitest';
+
+import '@testing-library/jest-dom/vitest';
 
 // different machine has its own timezones and some tests can fail because of it
 process.env.TZ = 'UTC';
