1245a7605730e81dfe776b4b4cae58813dddac63
Wroud <wrouds@gmail.com>
2025-08-20 16:04:43 +0800

    dbeaver/pro#6565 fix: prevent requests on session expire

--- webapp/packages/core-root/src/SessionExpireService.ts
+++ webapp/packages/core-root/src/SessionExpireService.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -9,6 +9,7 @@ import { Bootstrap, injectable } from '@cloudbeaver/core-di';
 import { Executor, type IExecutor } from '@cloudbeaver/core-executor';
 import { EServerErrorCode, GQLError, GraphQLService, SessionError } from '@cloudbeaver/core-sdk';
 import { errorOf } from '@cloudbeaver/core-utils';
+import { makeObservable, observable } from 'mobx';
 
 export const SESSION_EXPIRE_MIN_TIME = 5 * 1000 * 60;
 
@@ -23,9 +24,12 @@ export class SessionExpireService extends Bootstrap {
     this.sessionExpired = this.sessionExpired.bind(this);
 
     this.onSessionExpire = new Executor();
+    makeObservable<this, 'isExpired'>(this, {
+      isExpired: observable,
+    });
   }
 
-  get expired() {
+  get expired(): boolean {
     return this.isExpired;
   }
 
