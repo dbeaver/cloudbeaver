989651e89b896169b198a115bab155d25cf8d8dc
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-09-10 11:42:01 +0200

    handles empty json table edge case

--- webapp/packages/core-ui/src/locales/en.ts
+++ webapp/packages/core-ui/src/locales/en.ts
@@ -1,5 +1,6 @@
 export default [
   ['core_ui_empty', 'Empty'],
+  ['core_ui_no_data', 'No data available'],
   ['core_ui_form_save_error', 'Error occurred when saving the form'],
   ['core_ui_switch_tab_error', 'Cannot switch tab during form validation'],
 ];
--- webapp/packages/core-ui/src/locales/fr.ts
+++ webapp/packages/core-ui/src/locales/fr.ts
@@ -1,5 +1,6 @@
 export default [
   ['core_ui_empty', 'Vide'],
+  ['core_ui_no_data', 'No data available'],
   ['core_ui_form_save_error', 'Une erreur est survenue lors de la sauvegarde du formulaire'],
   ['core_ui_switch_tab_error', "Impossible de changer d'onglet pendant la validation du formulaire"],
 ];
