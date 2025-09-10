25bab91e981ec4517bb44b386fb6178dcaadda17
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-09-10 13:45:25 +0200

    removes redundant translations

--- webapp/packages/core-ui/src/locales/zh.ts
+++ webapp/packages/core-ui/src/locales/zh.ts
@@ -1,6 +1,5 @@
 export default [
   ['core_ui_empty', 'Empty'],
-  ['core_ui_no_data', 'No data available'],
   ['core_ui_form_save_error', '保存表单出错'],
   ['core_ui_switch_tab_error', '表单校验时无法切换标签'],
 ];
