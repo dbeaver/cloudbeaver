ded20a5192cb752497f64868454545c659520af0
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-09-19 12:54:21 +0200

    reverts object menu cell usage of new menu (problems with portal - infinite rerender)

--- webapp/packages/plugin-object-viewer/src/ObjectPropertiesPage/ObjectPropertyTable/Table/ObjectMenuCell.tsx
+++ webapp/packages/plugin-object-viewer/src/ObjectPropertiesPage/ObjectPropertyTable/Table/ObjectMenuCell.tsx
@@ -10,7 +10,7 @@ import { ConnectionInfoResource, DATA_CONTEXT_CONNECTION } from '@cloudbeaver/co
 import { useDataContextLink } from '@cloudbeaver/core-data-context';
 import { useService } from '@cloudbeaver/core-di';
 import { DATA_CONTEXT_NAV_NODE, NavNodeManagerService, type DBObject } from '@cloudbeaver/core-navigation-tree';
-import { ContextMenuAriaKit } from '@cloudbeaver/core-ui';
+import { ContextMenu } from '@cloudbeaver/core-ui';
 import { MENU_NAV_TREE, useNode } from '@cloudbeaver/plugin-navigation-tree';
 import { observer } from 'mobx-react-lite';
 import { useState } from 'react';
@@ -82,12 +82,17 @@ export const ObjectMenuCell = observer<Props>(function ObjectMenuCell({ object }
         {!menuEmpty && (
           <div className={s(styles, { menuBox: true })}>
             <Loader suspense small fullSize>
-              <ContextMenuAriaKit
+              <ContextMenu
                 contextMenuPosition={contextMenuPosition}
                 menu={menu}
-                buttonElement={<Icon className={s(styles, { menuIcon: true })} name="snack" viewBox="0 0 16 10" />}
-                onSwitch={switchState}
-              />
+                placement="auto-end"
+                hidden={menuEmpty}
+                modal
+                disclosure
+                onVisibleSwitch={switchState}
+              >
+                <Icon className={s(styles, { menuIcon: true })} name="snack" viewBox="0 0 16 10" />
+              </ContextMenu>
             </Loader>
           </div>
         )}
