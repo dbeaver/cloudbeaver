522c56660c87ad4f367e281bc0a9103b51ee19f4
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-10-13 11:37:24 +0200

    dbeaver/pro#7132 updates deps

--- webapp/packages/plugin-navigation-tree/src/NavigationTree/ElementsTree/NavigationTreeNode/NavigationNode/NavigationNodeControl.tsx
+++ webapp/packages/plugin-navigation-tree/src/NavigationTree/ElementsTree/NavigationTreeNode/NavigationNode/NavigationNodeControl.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -49,123 +49,130 @@ interface IEditingState {
   cancel(): void;
 }
 
-export const NavigationNodeControl: NavTreeControlComponent = observer(
-  forwardRef(function NavigationNodeControl({ node, nodeInfo, dndElement, dndPlaceholder, className, onClick }, ref) {
-    const styles = useS(style);
-    const contextMenuPosition = useContextMenuPosition();
-    const treeNodeContext = useContext(TreeNodeContext);
-    const elementsTreeContext = useContext(ElementsTreeContext);
-    const navNodeInfoResource = useService(NavNodeInfoResource);
-    const navTreeResource = useService(NavTreeResource);
-    const error = getComputed(() => !!navNodeInfoResource.getException(node.id) || !!navTreeResource.getException(node.id));
-    const selected = treeNodeContext.selected;
-    const editingState = useObservableRef<IEditingState>(
-      () => ({
-        saving: false,
-        editing: false,
-        resolve: () => Promise.resolve(false),
+export const NavigationNodeControl: NavTreeControlComponent = React.memo(
+  observer(
+    forwardRef(function NavigationNodeControl({ node, nodeInfo, dndElement, dndPlaceholder, className, onClick }, ref) {
+      const styles = useS(style);
+      const contextMenuPosition = useContextMenuPosition();
+      const treeNodeContext = useContext(TreeNodeContext);
+      const elementsTreeContext = useContext(ElementsTreeContext);
+      const navNodeInfoResource = useService(NavNodeInfoResource);
+      const navTreeResource = useService(NavTreeResource);
+      const error = getComputed(() => !!navNodeInfoResource.getException(node.id) || !!navTreeResource.getException(node.id));
+      const selected = treeNodeContext.selected;
+      const editingState = useObservableRef<IEditingState>(
+        () => ({
+          saving: false,
+          editing: false,
+          resolve: () => Promise.resolve(false),
 
-        startEditing(resolve) {
-          this.editing = true;
-          this.saving = false;
-          this.resolve = resolve;
-        },
+          startEditing(resolve) {
+            this.editing = true;
+            this.saving = false;
+            this.resolve = resolve;
+          },
 
-        async save(name: string) {
-          this.setSaveStatus(true);
-          try {
-            const saved = await this.resolve(name);
+          async save(name: string) {
+            this.setSaveStatus(true);
+            try {
+              const saved = await this.resolve(name);
 
-            if (saved) {
-              this.finish();
+              if (saved) {
+                this.finish();
+              }
+            } finally {
+              this.setSaveStatus(false);
             }
-          } finally {
-            this.setSaveStatus(false);
-          }
+          },
+          setSaveStatus(saving: boolean) {
+            this.saving = saving;
+          },
+          finish() {
+            this.editing = false;
+            this.saving = false;
+            this.resolve = () => Promise.resolve(false);
+          },
+          cancel() {
+            this.finish();
+          },
+        }),
+        {
+          saving: observable.ref,
+          editing: observable.ref,
+          startEditing: action.bound,
+          save: action.bound,
+          setSaveStatus: action.bound,
+          finish: action.bound,
+          cancel: action.bound,
         },
-        setSaveStatus(saving: boolean) {
-          this.saving = saving;
-        },
-        finish() {
-          this.editing = false;
-          this.saving = false;
-          this.resolve = () => Promise.resolve(false);
-        },
-        cancel() {
-          this.finish();
-        },
-      }),
-      {
-        saving: observable.ref,
-        editing: observable.ref,
-        startEditing: action.bound,
-        save: action.bound,
-        setSaveStatus: action.bound,
-        finish: action.bound,
-        cancel: action.bound,
-      },
-      false,
-    );
+        false,
+      );
 
-    const nodeActions = useObjectRef<INodeActions>({
-      rename: editingState.startEditing,
-    });
+      const nodeActions = useObjectRef<INodeActions>({
+        rename: editingState.startEditing,
+      });
 
-    let icon = nodeInfo.icon;
-    const name = nodeInfo.name;
-    const description = nodeInfo.description;
-    const title = nodeInfo.tooltip;
+      let icon = nodeInfo.icon;
+      const name = nodeInfo.name;
+      const description = nodeInfo.description;
+      const title = nodeInfo.tooltip;
 
-    if (error) {
-      icon = '/icons/error_icon_sm.svg';
-    }
+      if (error) {
+        icon = '/icons/error_icon_sm.svg';
+      }
 
-    function handlePortalClick(event: React.MouseEvent<HTMLDivElement>) {
-      EventContext.set(event, EventStopPropagationFlag);
-      treeNodeContext.select();
-    }
+      function handlePortalClick(event: React.MouseEvent<HTMLDivElement>) {
+        EventContext.set(event, EventStopPropagationFlag);
+        treeNodeContext.select();
+      }
 
-    function handleContextMenuOpen(event: React.MouseEvent<HTMLDivElement>) {
-      contextMenuPosition.handleContextMenuOpen(event);
-      treeNodeContext.select();
-    }
+      function handleContextMenuOpen(event: React.MouseEvent<HTMLDivElement>) {
+        contextMenuPosition.handleContextMenuOpen(event);
+        treeNodeContext.select();
+      }
 
-    const { editing, saving } = editingState;
+      const { editing, saving } = editingState;
 
-    const attributes = { [DATA_ATTRIBUTE_NODE_EDITING]: editing };
+      const attributes = { [DATA_ATTRIBUTE_NODE_EDITING]: editing };
 
-    return (
-      <TreeNodeControl
-        ref={ref}
-        {...attributes}
-        className={s(styles, { treeNodeControl: true, dragging: !!dndElement, editing }, className)}
-        onClick={onClick}
-        onContextMenu={handleContextMenuOpen}
-      >
-        <NavigationNodeExpand nodeId={node.id} />
-        <TreeNodeIcon icon={icon} />
-        <TreeNodeName title={title} className={s(styles, { treeNodeName: true })}>
-          <Loader suspense inline fullSize>
-            {editing ? (
-              <NavigationNodeEditorLoader name={getNodePlainName(node)} disabled={saving} onSave={editingState.save} onClose={editingState.cancel} />
-            ) : (
-              <div className={s(styles, { nameBox: true })}>
-                {name}
-                {elementsTreeContext?.tree.settings?.objectsDescription && description && (
-                  <TreeNodeDescription>{` - ${description}`}</TreeNodeDescription>
-                )}
-              </div>
-            )}
-          </Loader>
-        </TreeNodeName>
-        {!editing && !dndPlaceholder && (
-          <div className={s(styles, { portal: true })} onClick={handlePortalClick}>
-            <TreeNodeMenuLoader contextMenuPosition={contextMenuPosition} node={node} actions={nodeActions} selected={selected} />
-          </div>
-        )}
-      </TreeNodeControl>
-    );
-  }),
+      return (
+        <TreeNodeControl
+          ref={ref}
+          {...attributes}
+          className={s(styles, { treeNodeControl: true, dragging: !!dndElement, editing }, className)}
+          onClick={onClick}
+          onContextMenu={handleContextMenuOpen}
+        >
+          <NavigationNodeExpand nodeId={node.id} />
+          <TreeNodeIcon icon={icon} />
+          <TreeNodeName title={title} className={s(styles, { treeNodeName: true })}>
+            <Loader suspense inline fullSize>
+              {editing ? (
+                <NavigationNodeEditorLoader
+                  name={getNodePlainName(node)}
+                  disabled={saving}
+                  onSave={editingState.save}
+                  onClose={editingState.cancel}
+                />
+              ) : (
+                <div className={s(styles, { nameBox: true })}>
+                  {name}
+                  {elementsTreeContext?.tree.settings?.objectsDescription && description && (
+                    <TreeNodeDescription>{` - ${description}`}</TreeNodeDescription>
+                  )}
+                </div>
+              )}
+            </Loader>
+          </TreeNodeName>
+          {!editing && !dndPlaceholder && (
+            <div className={s(styles, { portal: true })} onClick={handlePortalClick}>
+              <TreeNodeMenuLoader contextMenuPosition={contextMenuPosition} node={node} actions={nodeActions} selected={selected} />
+            </div>
+          )}
+        </TreeNodeControl>
+      );
+    }),
+  ),
 );
 
 NavigationNodeControl.displayName = 'NavigationNodeControl';
