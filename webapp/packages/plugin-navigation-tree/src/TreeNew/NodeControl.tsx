42cd6696692a9dfb430169d671e618522b30ec56
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-10-13 14:29:22 +0200

    Revert "dbeaver/pro#7132 updates deps"
    This reverts commit 522c56660c87ad4f367e281bc0a9103b51ee19f4.

--- webapp/packages/plugin-navigation-tree/src/TreeNew/NodeControl.tsx
+++ webapp/packages/plugin-navigation-tree/src/TreeNew/NodeControl.tsx
@@ -1,12 +1,12 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2024 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
 import { observer } from 'mobx-react-lite';
-import { memo, forwardRef, useContext } from 'react';
+import { forwardRef, useContext } from 'react';
 
 import { TreeNodeControl, TreeNodeExpand, TreeNodeIcon, TreeNodeName } from '@cloudbeaver/core-blocks';
 
@@ -14,22 +14,20 @@ import { TreeContext } from './contexts/TreeContext.js';
 import { TreeDataContext } from './contexts/TreeDataContext.js';
 import type { NodeControlComponent } from './INodeRenderer.js';
 
-export const NodeControl: NodeControlComponent = memo(
-  observer(
-    forwardRef(function NodeControl({ nodeId }, ref) {
-      const data = useContext(TreeDataContext)!;
-      const tree = useContext(TreeContext)!;
+export const NodeControl: NodeControlComponent = observer(
+  forwardRef(function NodeControl({ nodeId }, ref) {
+    const data = useContext(TreeDataContext)!;
+    const tree = useContext(TreeContext)!;
 
-      const node = data.getNode(nodeId);
-      const height = tree.getNodeHeight(nodeId);
+    const node = data.getNode(nodeId);
+    const height = tree.getNodeHeight(nodeId);
 
-      return (
-        <TreeNodeControl ref={ref} style={{ height }}>
-          <TreeNodeExpand leaf={node.leaf} />
-          <TreeNodeIcon icon={node.icon} />
-          <TreeNodeName title={node.tooltip ?? node.name}>{node.name}</TreeNodeName>
-        </TreeNodeControl>
-      );
-    }),
-  ),
+    return (
+      <TreeNodeControl ref={ref} style={{ height }}>
+        <TreeNodeExpand leaf={node.leaf} />
+        <TreeNodeIcon icon={node.icon} />
+        <TreeNodeName title={node.tooltip ?? node.name}>{node.name}</TreeNodeName>
+      </TreeNodeControl>
+    );
+  }),
 );
