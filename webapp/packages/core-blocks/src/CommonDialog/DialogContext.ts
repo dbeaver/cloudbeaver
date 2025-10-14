f366aba00296afd3816ba6581bab68175bbcaf47
Wroud <wrouds@gmail.com>
2025-10-14 21:32:12 +0800

    fix: modify reject function signatures for consistency

--- webapp/packages/core-blocks/src/CommonDialog/DialogContext.ts
+++ webapp/packages/core-blocks/src/CommonDialog/DialogContext.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@ import type { DialogInternal } from '@cloudbeaver/core-dialogs';
 export interface IDialogContext {
   visible: boolean;
   dialog: DialogInternal<any>;
-  reject: () => void;
+  reject: (result?: any) => void;
 }
 
 export const DialogContext = createContext<IDialogContext>(undefined as any);
--- webapp/packages/core-blocks/src/CommonDialog/DialogsPortal.tsx
+++ webapp/packages/core-blocks/src/CommonDialog/DialogsPortal.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -33,9 +33,9 @@ export const DialogsPortal = observer(function DialogsPortal() {
 
   const state = useObjectRef(
     () => ({
-      reject() {
+      reject(result?: any) {
         if (this.dialog) {
-          commonDialogService.rejectDialog(this.dialog.promise);
+          commonDialogService.rejectDialog(this.dialog.promise, result);
         }
       },
       resolve(result: any) {
@@ -99,7 +99,7 @@ export const DialogsPortal = observer(function DialogsPortal() {
 interface NestedDialogType {
   dialog: DialogInternal<any>;
   resolveDialog: (result: any) => void;
-  rejectDialog: () => void;
+  rejectDialog: (result?: any) => void;
   visible: boolean;
 }
 
