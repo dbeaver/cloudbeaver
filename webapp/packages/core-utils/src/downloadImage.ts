8e642a486a21913e07b29fcff9c354099cc0354c
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-10-24 13:55:35 +0200

    fixes svg so it is not blur

---
+++ webapp/packages/core-utils/src/downloadImage.ts
@@ -0,0 +1,27 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
+import * as modernScreenshot from 'modern-screenshot';
+import type { Options } from 'modern-screenshot';
+import { download } from './download.js';
+
+export type { Options as IScreenshotOptions };
+
+export async function downloadSvg<T extends Node>(element: T, options: Options, fileName: string): Promise<void> {
+  const svg = await modernScreenshot.domToForeignObjectSvg<T>(element, options);
+  const svgData = new XMLSerializer().serializeToString(svg);
+  const blob = new Blob(['<?xml version="1.0" standalone="no"?>\r\n', svgData], { type: 'image/svg+xml;charset=utf-8' });
+
+  download(blob, `${fileName}.svg`);
+}
+
+export async function downloadPng<T extends Node>(element: T, options: Options, fileName: string): Promise<void> {
+  const png = await modernScreenshot.domToPng(element, options);
+
+  download(png, `${fileName}.png`);
+}
--- webapp/packages/core-utils/src/index.ts
+++ webapp/packages/core-utils/src/index.ts
@@ -89,4 +89,4 @@ export * from './bindFunctions.js';
 export * from './getDomainFromUrl.js';
 export * from './isNumber.js';
 export * from './getSubjectDifferences.js';
-export * from './modernScreenshot.js';
+export * from './downloadImage.js';
