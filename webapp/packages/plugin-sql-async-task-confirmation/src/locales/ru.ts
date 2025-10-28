a88ca770490a87ddee55db3d994ff1d340741d39
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-10-28 17:42:43 +0100

    dbeaver/pro#6363 adds settings for dangerous queries dialogs

--- webapp/packages/plugin-sql-async-task-confirmation/src/locales/ru.ts
+++ webapp/packages/plugin-sql-async-task-confirmation/src/locales/ru.ts
@@ -1,1 +1,6 @@
-export default [];
+export default [
+  ['plugin_sql_editor_execute_drop_table_queries', ' Подтверждение диалога для DROP TABLE запросов'],
+  ['plugin_sql_editor_execute_drop_table_queries_description', ' Показать диалог подтверждения перед выполнением DROP TABLE запросов'],
+  ['plugin_sql_editor_execute_dangerous_queries', ' Подтверждение диалога для опасных запросов'],
+  ['plugin_sql_editor_execute_dangerous_queries_description', ' Показать диалог подтверждения перед выполнением опасных запросов'],
+];
