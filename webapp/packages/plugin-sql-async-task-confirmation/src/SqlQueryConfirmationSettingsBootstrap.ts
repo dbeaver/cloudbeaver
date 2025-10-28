a1e16ae5a1a89ffa379be2bdbb5f0527087557c7
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-10-28 17:45:15 +0100

    cleanup

--- webapp/packages/plugin-sql-async-task-confirmation/src/SqlQueryConfirmationSettingsBootstrap.ts
+++ webapp/packages/plugin-sql-async-task-confirmation/src/SqlQueryConfirmationSettingsBootstrap.ts
@@ -6,22 +6,6 @@
  * you may not use this file except in compliance with the License.
  */
 
-/*
- * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2019-2025 DBeaver Corp
- *
- * All Rights Reserved
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of DBeaver Corp and its suppliers, if any.
- * The intellectual and technical concepts contained
- * herein are proprietary to DBeaver Corp and its suppliers
- * and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from DBeaver Corp.
- */
 import { Bootstrap, injectable } from '@cloudbeaver/core-di';
 import { ESettingsValueType, SettingsManagerService, SettingsProvider, SettingsProviderService } from '@cloudbeaver/core-settings';
 import { schema } from '@cloudbeaver/core-utils';
