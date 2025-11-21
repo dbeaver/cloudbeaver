b349f02fbe7e2f37a19156e4fcadc47fae39df43
Viktor Kirst <viktor.kirst@dbeaver.com>
2025-11-21 09:10:25 +0400

    dbeaver/pro#7442 Add copyright

--- server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/CBEventsLongPollingServletTest.java
+++ server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/CBEventsLongPollingServletTest.java
@@ -1,3 +1,19 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cloudbeaver.test.platform;
 
 import io.cloudbeaver.CloudbeaverMockTest;
@@ -11,15 +27,12 @@ import org.eclipse.jetty.http.BadMessageException;
 import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.model.websocket.WSConstants;
-import org.jkiss.junit.osgi.annotation.RunWithApplication;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class CBEventsLongPollingServletTest extends CloudbeaverMockTest {
 
