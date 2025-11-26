522c96c4e750e371e2ddefe935ae2a4d6169feb1
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-11-26 16:52:17 +0100

    uses input from core-blocks inside fetch size action

--- webapp/packages/plugin-data-viewer/src/TableViewer/TableFooter/TableFooterMenu/FetchSizeAction/FetchSizeAction.tsx
+++ webapp/packages/plugin-data-viewer/src/TableViewer/TableFooter/TableFooterMenu/FetchSizeAction/FetchSizeAction.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -8,13 +8,23 @@
 import { observer } from 'mobx-react-lite';
 import { useEffect, useRef, useState } from 'react';
 
-import { Container, Form, getComputed, s, useS } from '@cloudbeaver/core-blocks';
+import { Container, Form, getComputed, Input, InputStyles, s, SContext, useS, type StyleRegistry } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
 import { type ICustomMenuItemComponent } from '@cloudbeaver/core-view';
 
 import { DATA_CONTEXT_DV_DDM } from '../../../../DatabaseDataModel/DataContext/DATA_CONTEXT_DV_DDM.js';
 import { DataViewerSettingsService } from '../../../../DataViewerSettingsService.js';
 import styles from './FetchSizeAction.module.css';
+
+const styleRegistry: StyleRegistry = [
+  [
+    InputStyles,
+    {
+      mode: 'append',
+      styles: [styles],
+    },
+  ],
+];
 
 export const FetchSizeAction: ICustomMenuItemComponent = observer(function FetchSizeAction({ context }) {
   const model = context.get(DATA_CONTEXT_DV_DDM)!;
@@ -47,17 +57,21 @@ export const FetchSizeAction: ICustomMenuItemComponent = observer(function Fetch
   return (
     <Container className={s(style, { count: true })} keepSize noGrow center>
       <Form contents onSubmit={handleChange}>
-        <input
-          ref={ref}
-          className={s(style, { input: true })}
-          type="number"
-          value={limit}
-          disabled={disabled}
-          min={dataViewerSettingsService.minFetchSize}
-          max={dataViewerSettingsService.maxFetchSize}
-          onChange={e => setLimit(e.target.value)}
-          onBlur={handleChange}
-        />
+        <SContext registry={styleRegistry}>
+          <Input
+            ref={ref}
+            className={s(style, { input: true })}
+            type="number"
+            value={limit}
+            disabled={disabled}
+            min={dataViewerSettingsService.minFetchSize}
+            max={dataViewerSettingsService.maxFetchSize}
+            size="small"
+            tiny
+            onChange={value => setLimit(value)}
+            onBlur={handleChange}
+          />
+        </SContext>
       </Form>
     </Container>
   );
