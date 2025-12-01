35e2a20d214a754bd88d19dba92fc1d821a8fab7
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-12-01 12:55:25 +0100

    Revert "cleanup"
    This reverts commit 84ef0742420fbaf02afda8df99ab18fbb13c6f76.

--- webapp/packages/core-blocks/src/Cell.tsx
+++ webapp/packages/core-blocks/src/Cell.tsx
@@ -11,9 +11,9 @@ import style from './Cell.module.css';
 import { Container } from './Containers/Container.js';
 import { s } from './s.js';
 import { useS } from './useS.js';
-import { Focusable, type FocusableProps } from './Focusable.js';
+import { Focusable, type ClickableProps } from './Focusable.js';
 
-interface Props extends FocusableProps {
+interface Props extends ClickableProps {
   description?: React.ReactNode | string;
   before?: React.ReactElement;
   after?: React.ReactElement;
