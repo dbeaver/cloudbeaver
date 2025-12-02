b31f246b74f798341834a7ae8511725ce7b5f5ae
Sychev Andrey <nukemoore@gmail.com>
2025-12-02 11:53:37 +0100

    dbeaver/pro#5232 feat: add Storysource addon to Storybook configuration

--- webapp/packages/storybook/.storybook/main.ts
+++ webapp/packages/storybook/.storybook/main.ts
@@ -11,7 +11,7 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [getAbsolutePath('@storybook/addon-docs')],
+  addons: [getAbsolutePath('@storybook/addon-docs'), getAbsolutePath('@storybook/addon-storysource')],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
