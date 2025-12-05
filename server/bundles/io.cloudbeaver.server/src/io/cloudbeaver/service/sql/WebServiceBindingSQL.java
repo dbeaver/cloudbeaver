d6748677137b5c404e5afa1aef596aac2df244df
Ruslan <pupkinvasya7000@gmail.com>
2025-12-05 16:33:18 +0100

    dbeaver/pro#7073 add grouping endpoint grahpql

--- server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/sql/WebServiceBindingSQL.java
+++ server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/sql/WebServiceBindingSQL.java
@@ -105,6 +105,18 @@ public class WebServiceBindingSQL extends WebServiceBindingBase<DBWServiceSQL>
                 getArgument(env, "functions"),
                 getArgument(env, "showDuplicatesOnly"))
             )
+            .dataFetcher(
+                "sqlGropingResultSet", env ->
+                    getService(env).getGroupingSqlResultSet(
+                        getSQLContext(env),
+                        getArgumentVal(env, "resultsId"),
+                        getArgumentVal(env, "columnNames"),
+                        getArgument(env, "functions"),
+                        getArgument(env, "showDuplicatesOnly"),
+                        getDataFilter(env),
+                        getDataFormat(env)
+                    )
+            )
         ;
 
         model.getMutationType()
