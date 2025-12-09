98742e457a543ee8ff9e60a1d766fe0d307e0388
Ruslan <pupkinvasya7000@gmail.com>
2025-12-09 12:23:36 +0100

    dbeaver/pro#6612 ldap group mapping full dn

--- server/bundles/io.cloudbeaver.service.ldap.auth/src/io/cloudbeaver/service/ldap/auth/LdapAuthProvider.java
+++ server/bundles/io.cloudbeaver.service.ldap.auth/src/io/cloudbeaver/service/ldap/auth/LdapAuthProvider.java
@@ -533,7 +533,7 @@ public class LdapAuthProvider implements SMAuthProviderExternal<SMSession>, SMBr
     @NotNull
     private List<String> getGroupForMember(String fullDN, LdapSettings ldapSettings, Map<String, Object> authParameters) {
         DirContext context = null;
-        Set<String> result = new HashSet<>();
+        Set<String> result = new LinkedHashSet<>();
         try {
             Map<String, String> environment = creteAuthEnvironment(ldapSettings);
             if (CommonUtils.isEmpty(ldapSettings.getBindUserDN())) {
