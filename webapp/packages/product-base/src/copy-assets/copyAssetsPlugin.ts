c751f817d399cca2cb1c81a03c1f894bffb1606d
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-12-14 23:49:01 +0100

    dbeaver/pro#6985 preloads icons

--- webapp/packages/product-base/src/copy-assets/copyAssetsPlugin.ts
+++ webapp/packages/product-base/src/copy-assets/copyAssetsPlugin.ts
@@ -1,21 +1,17 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-// import fsExtra from 'fs-extra';
 import fs from 'node:path';
-// import { fileURLToPath } from 'node:url';
-import type { Plugin } from 'vite';
-// @ts-ignore
+import type { Plugin, PluginOption } from 'vite';
 import { DynamicPublicDirectory } from 'vite-multiple-assets';
 
 import { getAssets } from './getAssets.js';
 
-export function copyAssetsPlugin(): Plugin {
-  // let rootPath = fileURLToPath(import.meta.url);
+export function copyAssetsPlugin(): Plugin | PluginOption {
   const assets = getAssets(fs.join(process.cwd(), 'package.json'));
   const normalizedAssets = assets.map(asset =>
     fs
@@ -24,18 +20,11 @@ export function copyAssetsPlugin(): Plugin {
       .replace(/\\/g, '/'),
   );
 
-  return DynamicPublicDirectory(normalizedAssets) as any;
-  // return {
-  //   name: 'copy-assets',
-  //   configResolved(config) {
-  //     rootPath = config.root;
-  //   },
-  //   async buildStart() {
-  //     const assets = getAssets(fs.join(rootPath, '..', 'package.json'));
-
-  //     for (const asset of assets) {
-  //       fsExtra.copySync(asset, './src/public/', { overwrite: true });
-  //     }
-  //   },
-  // };
+  return DynamicPublicDirectory(
+    normalizedAssets.map(asset => ({
+      input: asset,
+      output: '/icons',
+      flatten: true,
+    })),
+  );
 }
