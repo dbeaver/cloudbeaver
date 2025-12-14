fd9813cdcf57cb665264c93310dc07d9e4c0f149
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-12-15 00:23:18 +0100

    simplifies the structure

--- webapp/packages/product-base/src/copy-assets/copyAssetsPlugin.ts
+++ webapp/packages/product-base/src/copy-assets/copyAssetsPlugin.ts
@@ -5,13 +5,17 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
+// import fsExtra from 'fs-extra';
 import fs from 'node:path';
-import type { Plugin, PluginOption } from 'vite';
+// import { fileURLToPath } from 'node:url';
+import type { Plugin } from 'vite';
+// @ts-ignore
 import { DynamicPublicDirectory } from 'vite-multiple-assets';
 
 import { getAssets } from './getAssets.js';
 
-export function copyAssetsPlugin(): Plugin | PluginOption {
+export function copyAssetsPlugin(): Plugin {
+  // let rootPath = fileURLToPath(import.meta.url);
   const assets = getAssets(fs.join(process.cwd(), 'package.json'));
   const normalizedAssets = assets.map(asset =>
     fs
@@ -20,11 +24,18 @@ export function copyAssetsPlugin(): Plugin | PluginOption {
       .replace(/\\/g, '/'),
   );
 
-  return DynamicPublicDirectory(
-    normalizedAssets.map(asset => ({
-      input: asset,
-      output: '/icons',
-      flatten: true,
-    })),
-  );
+  return DynamicPublicDirectory(normalizedAssets) as any;
+  // return {
+  //   name: 'copy-assets',
+  //   configResolved(config) {
+  //     rootPath = config.root;
+  //   },
+  //   async buildStart() {
+  //     const assets = getAssets(fs.join(rootPath, '..', 'package.json'));
+
+  //     for (const asset of assets) {
+  //       fsExtra.copySync(asset, './src/public/', { overwrite: true });
+  //     }
+  //   },
+  // };
 }
