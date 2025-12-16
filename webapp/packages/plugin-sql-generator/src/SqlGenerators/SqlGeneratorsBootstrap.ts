2cef1531f9c0ecf1a71f071fcb08a6ac2a7d8c2a
Sychev Andrey <nukemoore@gmail.com>
2025-12-16 17:32:05 +0100

    dbeaver/pro#7810 style: update copyright year in SqlGeneratorsBootstrap.ts

--- webapp/packages/plugin-sql-generator/src/SqlGenerators/SqlGeneratorsBootstrap.ts
+++ webapp/packages/plugin-sql-generator/src/SqlGenerators/SqlGeneratorsBootstrap.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,6 @@ export class SqlGeneratorsBootstrap extends Bootstrap {
   override register(): void {
     this.menuService.setHandler({
       id: 'node-sql-generators',
-      menus: [MENU_SQL_GENERATORS],
       contexts: [DATA_CONTEXT_NAV_NODE],
       isDisabled: context => {
         const node = context.get(DATA_CONTEXT_NAV_NODE)!;
