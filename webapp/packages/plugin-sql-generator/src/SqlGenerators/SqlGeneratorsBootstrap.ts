899cf6911fc3a2d66b64a2cb75cc90d80205bcf6
Sychev Andrey <nukemoore@gmail.com>
2025-12-17 16:40:01 +0100

    dbeaver/pro#7810 fix: return back isDisabled for handler

--- webapp/packages/plugin-sql-generator/src/SqlGenerators/SqlGeneratorsBootstrap.ts
+++ webapp/packages/plugin-sql-generator/src/SqlGenerators/SqlGeneratorsBootstrap.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,12 @@ export class SqlGeneratorsBootstrap extends Bootstrap {
       id: 'node-sql-generators',
       menus: [MENU_SQL_GENERATORS],
       contexts: [DATA_CONTEXT_NAV_NODE],
-      getLoader: context => {
+      isDisabled: context => {
+        const node = context.get(DATA_CONTEXT_NAV_NODE)!;
+
+        return this.sqlGeneratorsResource.get(node.id)?.length === 0;
+      },
+      getLoader: (context, action) => {
         const node = context.get(DATA_CONTEXT_NAV_NODE)!;
 
         return getCachedMapResourceLoaderState(this.sqlGeneratorsResource, () => node.id);
