95ff5ebb8f5982e08f882b4f7ccd55206632be66
Sychev Andrey <nukemoore@gmail.com>
2025-12-24 18:31:22 +0100

    dbeaver/pro#7855 feat: Add setting to use the user's OS formatting for numbers and dates

--- webapp/packages/plugin-data-spreadsheet-new/src/DataGridSettingsService.ts
+++ webapp/packages/plugin-data-spreadsheet-new/src/DataGridSettingsService.ts
@@ -21,6 +21,7 @@ import { DATA_EDITOR_SETTINGS_GROUP } from '@cloudbeaver/plugin-data-viewer';
 const defaultSettings = schema.object({
   'plugin.data-spreadsheet.hidden': schemaExtra.stringedBoolean().default(false),
   'plugin.data-spreadsheet.showDescriptionInHeader': schemaExtra.stringedBoolean().default(true),
+  'plugin.data-spreadsheet.useOSFormatting': schema.string().default('default'),
 });
 
 export type DataGridSettingsSchema = typeof defaultSettings;
@@ -36,7 +37,12 @@ export class DataGridSettingsService {
     return this.settings.getValue('plugin.data-spreadsheet.showDescriptionInHeader');
   }
 
+  get useOSFormatting(): string {
+    return this.settings.getValue('plugin.data-spreadsheet.useOSFormatting');
+  }
+
   readonly settings: SettingsProvider<DataGridSettingsSchema>;
+  readonly supportedLocales = this.getSupportedLocales();
 
   constructor(
     private readonly settingsProviderService: SettingsProviderService,
@@ -53,6 +59,33 @@ export class DataGridSettingsService {
     );
 
     this.registerSettings();
+  }
+
+  private getSupportedLocales() {
+    const codes = [];
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+
+    function isLanguageCodeSupported(code: string | Intl.Locale) {
+      const locale = new Intl.Locale(code);
+      return locale.maximize().region !== undefined;
+    }
+
+    for (let i = 0; i < letters.length; i++) {
+      for (let j = 0; j < letters.length; j++) {
+        const code = letters[i]! + letters[j]!;
+        if (isLanguageCodeSupported(code)) {
+          codes.push(code);
+        }
+      }
+    }
+
+    return codes;
+  }
+
+  private getLocaleName(localeCode: string) {
+    const languageNameInItsOwnLang = new Intl.DisplayNames(localeCode, { type: 'language' });
+    const name = languageNameInItsOwnLang.of(localeCode);
+    return name && name?.length > 2 ? name.slice(0, 1).toLocaleUpperCase() + name.slice(1) : localeCode;
   }
 
   private registerSettings() {
@@ -76,6 +109,24 @@ export class DataGridSettingsService {
         type: ESettingsValueType.Checkbox,
         name: 'plugin_data_spreadsheet_new_settings_description_label',
         description: 'plugin_data_spreadsheet_new_settings_description_label_description',
+      },
+      {
+        group: DATA_EDITOR_SETTINGS_GROUP,
+        key: 'plugin.data-spreadsheet.useOSFormatting',
+        access: {
+          scope: ['client'],
+        },
+        type: ESettingsValueType.Select,
+        options: [
+          { value: 'default', name: 'None' },
+          { value: 'os', name: 'plugin_data_spreadsheet_new_settings_use_locale_formatting_os' },
+          ...this.supportedLocales
+            .map(locale => ({ value: locale, name: this.getLocaleName(locale) }))
+            .filter(locale => locale.name.length > 2)
+            .sort((a, b) => a.name.localeCompare(b.name)),
+        ],
+        name: 'plugin_data_spreadsheet_new_settings_use_locale_formatting_title',
+        description: 'plugin_data_spreadsheet_new_settings_use_locale_formatting_description',
       },
     ]);
   }
