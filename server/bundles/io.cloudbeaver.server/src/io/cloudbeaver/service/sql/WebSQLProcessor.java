cf40178f3f434d824b651896a8e6d28eed9a5431
Dmitrii Barnukov <dmitrii.barnukov@dbeaver.com>
2025-12-26 15:59:26 +0100

    dbeaver/pro#7693 added fix for single row tables

--- server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/sql/WebSQLProcessor.java
+++ server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/sql/WebSQLProcessor.java
@@ -951,7 +951,7 @@ public class WebSQLProcessor implements WebSessionProvider {
         DBCExecutionContext executionContext = getExecutionContext(dataContainer);
         try (DBCSession session = executionContext.openSession(monitor, DBCExecutionPurpose.USER, "Generate data update batches")) {
             DBDDataFilter dataFilter = new DBDDataFilter();
-            addKeyAttributes(resultsInfo, row, dataContainer, session, dataFilter);
+            addKeyAttributes(monitor, resultsInfo, row, dataContainer, session, dataFilter);
             WebExecutionSource executionSource = new WebExecutionSource(dataContainer, executionContext, this);
             dataContainer.readData(
                 executionSource, session, dataReceiver, dataFilter,
@@ -960,12 +960,19 @@ public class WebSQLProcessor implements WebSessionProvider {
     }
 
     private void addKeyAttributes(
+        @NotNull DBRProgressMonitor monitor,
         @NotNull WebSQLResultsInfo resultsInfo,
         @NotNull WebSQLResultsRow row,
         @NotNull DBSDataContainer dataContainer,
         @NotNull DBCSession session,
         @NotNull DBDDataFilter dataFilter
     ) throws DBException {
+        if (resultsInfo.isSingleRow()) {
+            long rowCount = DBUtils.readRowCount(monitor, session.getExecutionContext(), dataContainer, null, this);
+            if (rowCount == 1) {
+                return;
+            }
+        }
         DBDRowIdentifier rowIdentifier = resultsInfo.getDefaultRowIdentifier();
         checkRowIdentifier(resultsInfo, rowIdentifier);
         DBDAttributeBinding[] keyAttributes = rowIdentifier.getAttributes().toArray(new DBDAttributeBinding[0]);
