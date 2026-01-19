4e20e3c4d0267f73bf1b0dfed3c3bf9de59a33fb
naumov <iamemptyhuh@gmail.com>
2026-01-19 18:21:42 +0100

    dbeaver/pro#4692 show spinner when loading new page

--- webapp/packages/plugin-navigation-tree/src/NavigationTree/ElementsTree/NavTreeLimitFilter/elementsTreeLimitRenderer.tsx
+++ webapp/packages/plugin-navigation-tree/src/NavigationTree/ElementsTree/NavTreeLimitFilter/elementsTreeLimitRenderer.tsx
@@ -1,20 +1,22 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
+
+import { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 
-import { Link, s, useTranslate } from '@cloudbeaver/core-blocks';
+import { Link, useTranslate } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
 import { NavTreeResource } from '@cloudbeaver/core-navigation-tree';
 import { CachedResourceOffsetPageKey, CachedResourceOffsetPageTargetKey, getNextPageOffset } from '@cloudbeaver/core-resource';
+import { Spinner } from '@dbeaver/ui-kit';
 
 import type { NavigationNodeRendererComponent } from '../NavigationNodeComponent.js';
 import { NAVIGATION_TREE_LIMIT } from './elementsTreeLimitFilter.js';
-import styles from './elementsTreeLimitRenderer.module.css';
 
 export function elementsTreeLimitRenderer(nodeId: string): NavigationNodeRendererComponent | undefined {
   if (nodeId === NAVIGATION_TREE_LIMIT.limit) {
@@ -29,20 +31,35 @@ const NavTreeLimitMessage: NavigationNodeRendererComponent = observer(function N
   const navTreeResource = useService(NavTreeResource);
   const limit = navTreeResource.childrenLimit;
 
-  function loadMore() {
+  const [loading, setLoading] = useState(false);
+
+  async function loadMore() {
     const parentNodeId = path[path.length - 1];
     const pageInfo = navTreeResource.offsetPagination.getPageInfo(
       CachedResourceOffsetPageKey(0, 0).setParent(CachedResourceOffsetPageTargetKey(parentNodeId)),
     );
     if (pageInfo) {
-      navTreeResource.load(
-        CachedResourceOffsetPageKey(getNextPageOffset(pageInfo), limit).setParent(CachedResourceOffsetPageTargetKey(parentNodeId)),
-      );
+      try {
+        setLoading(true);
+        await navTreeResource.load(
+          CachedResourceOffsetPageKey(getNextPageOffset(pageInfo), limit).setParent(CachedResourceOffsetPageTargetKey(parentNodeId)),
+        );
+      } finally {
+        setLoading(false);
+      }
     }
   }
 
+  if (loading) {
+    return (
+      <div className='tw:px-6 tw:py-1'>
+        <Spinner size='small' />
+      </div>
+    );
+  }
+
   return (
-    <div className={s(styles, { loadMoreBox: true })}>
+    <div className='tw:text-(--theme-text-hint-on-light) theme-typography--caption tw:px-6 tw:py-1'>
       <Link title={translate('app_navigationTree_limited', undefined, { limit: limit })} onClick={loadMore}>
         {translate('ui_load_more')}
       </Link>
