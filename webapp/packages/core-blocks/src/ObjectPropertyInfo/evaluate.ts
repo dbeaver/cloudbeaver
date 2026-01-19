27fda84a5d86cbd50a5ac2b9d86134b336df7fe5
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-01-19 16:32:06 +0100

    bumps subscript major version

--- webapp/packages/core-blocks/src/ObjectPropertyInfo/evaluate.ts
+++ webapp/packages/core-blocks/src/ObjectPropertyInfo/evaluate.ts
@@ -1,17 +1,14 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
 
-import subscript from 'subscript';
-import { err, token } from 'subscript/parse';
+import { err, token, type AST, compile } from 'subscript';
 
-import 'subscript/feature/bool.js';
-
-const UNDEFINED_MAPPER_VALUE = new Array(2).fill(undefined, 1);
+const UNDEFINED_MAPPER_VALUE = new Array(2).fill(undefined, 1) as AST;
 token('undefined', 20, a => (a ? err() : UNDEFINED_MAPPER_VALUE));
 token('null', 20, a => (a ? err() : UNDEFINED_MAPPER_VALUE));
 
@@ -20,10 +17,9 @@ interface IContext {
 }
 
 export function evaluate(expression: string, object: Record<string, any>): boolean {
-  const fn = subscript(expression);
+  const fn = compile(expression);
   const context: IContext = {
     object,
   };
-  const result = fn(context);
-  return result;
+  return fn(context);
 }
