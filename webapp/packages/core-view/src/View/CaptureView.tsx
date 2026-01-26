0c5d748a6bc341088a0e149232d6b91e6971ae75
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-01-26 19:51:58 +0100

    dbeaver/pro#8104 fixes shortcuts to execute only for its own view context

--- webapp/packages/core-view/src/View/CaptureView.tsx
+++ webapp/packages/core-view/src/View/CaptureView.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -8,7 +8,7 @@
 import { observer } from 'mobx-react-lite';
 import { useContext } from 'react';
 
-import { s, useFocus, useHotkeys, useS } from '@cloudbeaver/core-blocks';
+import { s, useFocus, useHotkeys, useMergeRefs, useS } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
 import { isObjectsEqual } from '@cloudbeaver/core-utils';
 
@@ -32,24 +32,20 @@ export const CaptureView = observer<React.PropsWithChildren<ICaptureViewProps>>(
   const viewContext = useViewContext(view, parentContext);
   const actionService = useService(ActionService);
   const activeView = useActiveView(view);
-  const [ref, state] = useFocus<HTMLDivElement>({ onFocus: activeView.focusView, onBlur: activeView.blurView });
+  const [focusRef] = useFocus<HTMLDivElement>({ onFocus: activeView.focusView, onBlur: activeView.blurView });
   const style = useS(styles);
+  const allActionItems = view.actions.map(action => actionService.getAction(viewContext, action)).filter(Boolean) as IActionItem[];
+  const enabledActionItems = allActionItems.filter(action => action.binding && !action.isDisabled());
+  const allKeys = allActionItems.filter(action => action.binding).flatMap(item => getCommonAndOSSpecificKeys(item.binding?.binding));
 
-  const actionItems = view.actions
-    .map(action => actionService.getAction(viewContext, action))
-    .filter(action => action?.binding && !action.isDisabled())
-    .filter(Boolean) as IActionItem[];
-
-  const keys = actionItems.map(item => getCommonAndOSSpecificKeys(item.binding?.binding)).flat();
-
-  useHotkeys(
-    keys,
+  const hotkeysRef = useHotkeys<HTMLDivElement>(
+    allKeys,
     (event, handler) => {
-      if (!event.isTrusted || !state.reference?.contains(document.activeElement)) {
+      if (!event.isTrusted) {
         return;
       }
 
-      const action = actionItems.find(action => {
+      const action = enabledActionItems.find(action => {
         const commonAndSpecificKeys = getCommonAndOSSpecificKeys(action.binding?.binding);
         return commonAndSpecificKeys.some(key => {
           const hotkey = parseHotkey(key);
@@ -58,13 +54,14 @@ export const CaptureView = observer<React.PropsWithChildren<ICaptureViewProps>>(
         });
       });
 
+      event.stopImmediatePropagation();
       action?.activate(true);
     },
     {
-      enabled: keys.length > 0,
+      enabled: allKeys.length > 0,
       enableOnFormTags: ['INPUT', 'SELECT', 'TEXTAREA'],
       preventDefault(event, handler) {
-        const action = actionItems.find(action => {
+        const action = enabledActionItems.find(action => {
           const commonAndSpecificKeys = getCommonAndOSSpecificKeys(action.binding?.binding);
           return commonAndSpecificKeys.some(key => {
             const hotkey = parseHotkey(key);
@@ -72,17 +69,18 @@ export const CaptureView = observer<React.PropsWithChildren<ICaptureViewProps>>(
             return isObjectsEqual(hotkey, handler);
           });
         });
-        const isAppliedInSelectedScope = Boolean(state.reference?.contains(document.activeElement));
 
-        return isAppliedInSelectedScope && action?.binding?.binding.preventDefault === true;
+        return action?.binding?.binding.preventDefault === true;
       },
       enableOnContentEditable: true,
     },
   );
 
+  const mergedRef = useMergeRefs(focusRef, hotkeysRef);
+
   return (
     <CaptureViewContext.Provider value={viewContext}>
-      <div ref={ref} className={s(style, { container: true }, className)} tabIndex={0}>
+      <div ref={mergedRef} className={s(style, { container: true }, className)} tabIndex={0}>
         {children}
       </div>
     </CaptureViewContext.Provider>
