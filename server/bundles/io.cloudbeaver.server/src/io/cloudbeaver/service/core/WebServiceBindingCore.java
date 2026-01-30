1c790a91a82e3c16690127fb41c327d00de7e68a
Ruslan <pupkinvasya7000@gmail.com>
2026-01-30 09:50:52 +0100

    dbeaver/pro#8044 add api for database user preference

--- server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/core/WebServiceBindingCore.java
+++ server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/core/WebServiceBindingCore.java
@@ -163,7 +163,7 @@ public class WebServiceBindingCore extends WebServiceBindingBase<DBWServiceCore>
                 getWebSession(env),
                 getProjectReference(env),
                 getArgumentVal(env, "objectType"),
-                getArgumentVal(env, "objectId"),
+                getArgumentVal(env, "id"),
                 getArgumentVal(env, "settings")
             ))
 
