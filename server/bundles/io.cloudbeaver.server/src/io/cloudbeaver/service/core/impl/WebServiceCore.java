40694f5cf868f32e8f19e58acaa470811481ca24
Ruslan <pupkinvasya7000@gmail.com>
2026-02-10 09:50:16 +0100

    dbeaver/pro#8044 refactor api for database preferences

--- server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/core/impl/WebServiceCore.java
+++ server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/core/impl/WebServiceCore.java
@@ -135,7 +135,6 @@ public class WebServiceCore implements DBWServiceCore {
     ) throws DBWebException {
         if (id != null) {
             WebConnectionInfo connectionInfo = getConnectionState(webSession, projectId, id);
-            Map<String, String> connectionPreferences = connectionInfo.getDefaultUserSettings();
             if (connectionInfo != null) {
                 return Collections.singletonList(connectionInfo);
             }
