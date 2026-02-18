59e1bc3daeacbd59a3de3053181bfd92f0c23a6b
Sychev Andrey <nukemoore@gmail.com>
2026-02-18 16:55:23 +0100

    dbeaver/pro#8279 feat: rename CloudBeaver CE to CloudBeaver Community across multiple files

--- webapp/packages/core-root/src/__custom_mocks__/data/defaultServerConfig.ts
+++ webapp/packages/core-root/src/__custom_mocks__/data/defaultServerConfig.ts
@@ -14,7 +14,7 @@ export const defaultServerConfig: (productConfiguration?: Record<string, any>) =
   productConfiguration = defaultProductConfiguration,
 ) => ({
   serverConfig: {
-    name: 'CloudBeaver CE Server',
+    name: 'CloudBeaver Community',
     version: '22.1.2.202207140640',
     workspaceId: 'D14DGNGYNPI71M',
     rootURI: '/',
@@ -116,7 +116,7 @@ export const defaultServerConfig: (productConfiguration?: Record<string, any>) =
       id: 'io.cloudbeaver.product.ce.product',
       version: '22.1.2.202207140640',
       latestVersionInfo: WEBSITE_LINKS.LATEST_COMMUNITY_VERSION_PAGE,
-      name: 'CloudBeaver CE Server',
+      name: 'CloudBeaver Community',
       description: 'Cloudbeaver Web UI Application',
       buildTime: 'July 14, 2022',
       releaseTime: 'July 11, 2022',
--- webapp/packages/plugin-app-logo/src/Logo.tsx
+++ webapp/packages/plugin-app-logo/src/Logo.tsx
@@ -11,23 +11,15 @@ import { AppLogo, useResource } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
 import { PermissionsService, ProductInfoResource } from '@cloudbeaver/core-root';
 import { ScreenService } from '@cloudbeaver/core-routing';
-import { useAppVersion } from '@cloudbeaver/core-version';
 import { HolidaysService } from '@cloudbeaver/plugin-holidays';
 
 export const Logo = observer(function Logo() {
   const productInfoResource = useResource(Logo, ProductInfoResource, undefined);
   const screenService = useService(ScreenService);
   const permissionsService = useService(PermissionsService);
-  const { backendVersion, frontendVersion } = useAppVersion(true);
   const { holiday } = useService(HolidaysService);
 
-  const isSameVersion = backendVersion === frontendVersion;
-
-  const productName = productInfoResource.data?.name || 'CloudBeaver';
-  const backendVersionTitle = `${productName} ver. ${backendVersion}`;
-  const commonVersionTitle = `${productName} ver. ${frontendVersion}(${backendVersion})`;
-
-  const title = isSameVersion ? backendVersionTitle : commonVersionTitle;
+  const title = productInfoResource.data?.name || 'CloudBeaver';
 
   return <AppLogo title={title} iconSrc={holiday?.logoSrc} onClick={permissionsService.publicDisabled ? undefined : screenService.navigateToRoot} />;
 });
