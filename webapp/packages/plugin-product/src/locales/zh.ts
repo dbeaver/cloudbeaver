3c1a1759b87efe9dc10697ac5b8cda3341a19d6b
Sychev Andrey <nukemoore@gmail.com>
2026-02-19 11:29:11 +0100

    dbeaver/pro#8279 feat: update copyright year and remove product description fields in multiple locale files

--- webapp/packages/plugin-product/src/locales/zh.ts
+++ webapp/packages/plugin-product/src/locales/zh.ts
@@ -1,7 +1,6 @@
 export default [
   ['app_product_info', '关于'],
   ['app_product_info_name', '产品名称'],
-  ['app_product_info_description', '产品描述'],
   ['app_product_info_contacts', '联系人'],
   ['app_product_info_placeholder', '没有可显示的信息'],
 ];
