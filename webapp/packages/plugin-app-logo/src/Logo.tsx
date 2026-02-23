a0861b1e610678b215b1332480535ce703fc9771
Sychev Andrey <nukemoore@gmail.com>
2026-02-23 10:49:17 +0100

    dbeaver/pro#8279 feat: return product version back to Logo component

--- webapp/packages/plugin-app-logo/src/Logo.tsx
+++ webapp/packages/plugin-app-logo/src/Logo.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -11,15 +11,23 @@ import { AppLogo, useResource } from '@cloudbeaver/core-blocks';
 import { useService } from '@cloudbeaver/core-di';
 import { PermissionsService, ProductInfoResource } from '@cloudbeaver/core-root';
 import { ScreenService } from '@cloudbeaver/core-routing';
+import { useAppVersion } from '@cloudbeaver/core-version';
 import { HolidaysService } from '@cloudbeaver/plugin-holidays';
 
 export const Logo = observer(function Logo() {
   const productInfoResource = useResource(Logo, ProductInfoResource, undefined);
   const screenService = useService(ScreenService);
   const permissionsService = useService(PermissionsService);
+  const { backendVersion, frontendVersion } = useAppVersion(true);
   const { holiday } = useService(HolidaysService);
 
-  const title = productInfoResource.data?.name || 'CloudBeaver';
+  const isSameVersion = backendVersion === frontendVersion;
+
+  const productName = productInfoResource.data?.name || 'CloudBeaver';
+  const backendVersionTitle = `${productName} ver. ${backendVersion}`;
+  const commonVersionTitle = `${productName} ver. ${frontendVersion}(${backendVersion})`;
+
+  const title = isSameVersion ? backendVersionTitle : commonVersionTitle;
 
   return <AppLogo title={title} iconSrc={holiday?.logoSrc} onClick={permissionsService.publicDisabled ? undefined : screenService.navigateToRoot} />;
 });
