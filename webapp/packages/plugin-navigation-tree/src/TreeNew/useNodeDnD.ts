7e6be5f22b59e26e2bccfe9474c877807018e560
Sychev Andrey <nukemoore@gmail.com>
2026-02-26 17:01:49 +0100

    dbeaver/pro#7565 feat: implement drag-and-drop functionality in the new tree component

--- webapp/packages/plugin-navigation-tree/src/TreeNew/useNodeDnD.ts
+++ webapp/packages/plugin-navigation-tree/src/TreeNew/useNodeDnD.ts
@@ -1,31 +1,77 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
 import { useContext } from 'react';
 
+import { useMergeRefs, useObjectRef, useObservableRef } from '@cloudbeaver/core-blocks';
 import { useDataContext } from '@cloudbeaver/core-data-context';
-import { useDNDData } from '@cloudbeaver/core-ui';
+import { useDNDBox, useDNDData } from '@cloudbeaver/core-ui';
 
 import { TreeDnDContext } from './contexts/TreeDnDContext.js';
 
-export function useNodeDnD(nodeId: string, onDragStart: () => void) {
-  const treeDnD = useContext(TreeDnDContext)!;
+export interface INodeDnD {
+  state: {
+    isDragging: boolean;
+    isOverCurrent: boolean;
+    canDrop: boolean;
+  };
+  setRef: React.RefCallback<Element>;
+}
+
+export function useNodeDnD(nodeId: string): INodeDnD {
+  const treeDnD = useContext(TreeDnDContext);
+  const treeDnDRef = useObjectRef({ treeDnD });
   const context = useDataContext();
 
   const dndData = useDNDData(context, {
-    canDrag: () => true,
+    canDrag: () => {
+      const dnd = treeDnDRef.treeDnD;
+      return dnd ? dnd.canDrag(nodeId) : false;
+    },
     onDragStart: () => {
-      treeDnD.getContext(nodeId, context);
-      onDragStart();
+      treeDnDRef.treeDnD?.getContext(nodeId, context);
     },
     onDragEnd: () => {
-      treeDnD.getContext(nodeId, context);
+      treeDnDRef.treeDnD?.getContext(nodeId, context);
     },
   });
 
-  return dndData;
+  const dndBox = useDNDBox({
+    canDrop: moveContext => {
+      const dnd = treeDnDRef.treeDnD;
+      return dnd ? dnd.canDrop(nodeId, moveContext) : false;
+    },
+    onDrop: moveContext => treeDnDRef.treeDnD?.onDrop(nodeId, moveContext),
+  });
+
+  const setRef = useMergeRefs<Element>(dndData.setTargetRef as React.RefCallback<Element>, dndBox.setRef as React.RefCallback<Element>);
+
+  const state = useObservableRef(
+    () => ({
+      get isDragging() {
+        return treeDnDRef.treeDnD ? dndData.state.isDragging : false;
+      },
+      get isOverCurrent() {
+        return treeDnDRef.treeDnD ? dndBox.state.isOverCurrent : false;
+      },
+      get canDrop() {
+        return treeDnDRef.treeDnD ? dndBox.state.canDrop : false;
+      },
+    }),
+    {},
+    false,
+  );
+
+  return useObservableRef(
+    () => ({
+      state,
+      setRef,
+    }),
+    {},
+    { state, setRef },
+  );
 }
