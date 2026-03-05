10d618204a58a49d20e19fa22a7ea7c623797d9a
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-03-05 14:26:46 +0100

    reverts subscript version

--- webapp/packages/core-blocks/src/ObjectPropertyInfo/evaluate.ts
+++ webapp/packages/core-blocks/src/ObjectPropertyInfo/evaluate.ts
@@ -5,19 +5,25 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-// @ts-nocheck
-import justin from 'subscript/justin';
+
+import subscript from 'subscript';
+import { err, token } from 'subscript/parse';
+
+import 'subscript/feature/bool.js';
+
+const UNDEFINED_MAPPER_VALUE = new Array(2).fill(undefined, 1);
+token('undefined', 20, a => (a ? err() : UNDEFINED_MAPPER_VALUE));
+token('null', 20, a => (a ? err() : UNDEFINED_MAPPER_VALUE));
 
 interface IContext {
   object: Record<string, any>;
 }
 
 export function evaluate(expression: string, object: Record<string, any>): boolean {
-  const fn = justin(expression);
+  const fn = subscript(expression);
   const context: IContext = {
     object,
   };
   const result = fn(context);
-  
   return !!result;
 }
