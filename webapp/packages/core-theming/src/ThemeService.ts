099922d8dcaa61109c0edb5c1caf9cc658842dac
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-03-13 15:01:57 +0100

    moves switch logic to module styles

--- webapp/packages/core-theming/src/ThemeService.ts
+++ webapp/packages/core-theming/src/ThemeService.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@ import './styles/main/fonts.pure.css';
 // TODO: important to keep normalize first
 import './styles/main/normalize.pure.css';
 import './styles/utilities/typography.css';
-import './styles/utilities/switch.css';
 import './styles/UiIconButton.css';
 import './styles/UiSpinner.css';
 import './styles/UiInput.css';
