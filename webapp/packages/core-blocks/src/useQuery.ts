1c1635033a61d28488b0bbfbc34dd5320510c97f
naumov <iamemptyhuh@gmail.com>
2026-03-26 18:32:11 +0100

    dbeaver/pro#8746 add language tokens

--- webapp/packages/core-blocks/src/useQuery.ts
+++ webapp/packages/core-blocks/src/useQuery.ts
@@ -1,3 +1,11 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2026 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
 import { useState, useEffect, useRef, useLayoutEffect } from 'react';
 
 interface QueryState<T> {
@@ -6,10 +14,11 @@ interface QueryState<T> {
   error: Error | null;
 }
 
+/** We can use this if we don’t have ILoadableState and we fetch data that is not supposed to be kept in the resource */
 export function useQuery<T>(queryFn: () => Promise<T>, deps: any[] = []): QueryState<T> {
   const [state, setState] = useState<QueryState<T>>({
     data: null,
-    loading: true, // start as true
+    loading: true,
     error: null,
   });
 
