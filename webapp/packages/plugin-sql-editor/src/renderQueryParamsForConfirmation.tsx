4724445fe3f6f2168939fecf6684b91ec34c4cc8
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-03-27 14:48:00 +0100

    adds gray message

--- webapp/packages/plugin-sql-editor/src/renderQueryParamsForConfirmation.tsx
+++ webapp/packages/plugin-sql-editor/src/renderQueryParamsForConfirmation.tsx
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  */
 
-import { Alert, PropertiesTable, useResource, useTranslate } from '@cloudbeaver/core-blocks';
+import { PropertiesTable, useResource, useTranslate } from '@cloudbeaver/core-blocks';
 import { SQLCodeEditor, useSqlDialectExtension } from '@cloudbeaver/plugin-sql-editor-codemirror';
 import { useCodemirrorExtensions } from '@cloudbeaver/plugin-codemirror6';
 import { observer } from 'mobx-react-lite';
@@ -99,7 +99,9 @@ const RenderParametersForm = observer(function RenderParametersForm({
       {!sqlEditorSettingsService.disabled && (
         <SQLCodeEditor value={query} extensions={extensions} className="tw:overflow-auto tw:flex-1/3" readonly />
       )}
-      <Alert title={translate('plugin_sql_editor_bind_parameters_dialog_alert_title')} />
+      <div className="theme-typography--body2 tw:p-2 tw:bg-(--theme-secondary)">
+        {translate('plugin_sql_editor_bind_parameters_dialog_alert_title')}
+      </div>
     </div>
   );
 });
