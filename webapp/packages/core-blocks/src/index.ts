765c7fc925228583a6cf0b33bce7b9ebacf88d7d
naumov <iamemptyhuh@gmail.com>
2026-03-30 17:18:06 +0200

    dbeaver/pro#8746 remove useQuery hook

--- webapp/packages/core-blocks/src/index.ts
+++ webapp/packages/core-blocks/src/index.ts
@@ -253,4 +253,3 @@ export * from './ObjectPropertyInfo/evaluate.js';
 export * from './ObjectPropertyInfo/getObjectPropertyDefaults.js';
 export * from './useVisible.js';
 export * from './SAVED_VALUE_INDICATOR.js';
-export * from './useQuery.js';
