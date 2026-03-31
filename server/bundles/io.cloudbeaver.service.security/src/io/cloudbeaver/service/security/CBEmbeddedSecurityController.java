9ca2ca49dcf77e054bb256534da4aa9c506fa888
Ainur <ainur.iagudin@dbeaver.com>
2026-03-31 19:22:57 +0200

    dbeaver/pro#7912 support external oauth in te desktop

--- server/bundles/io.cloudbeaver.service.security/src/io/cloudbeaver/service/security/CBEmbeddedSecurityController.java
+++ server/bundles/io.cloudbeaver.service.security/src/io/cloudbeaver/service/security/CBEmbeddedSecurityController.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -2385,7 +2385,8 @@ public class CBEmbeddedSecurityController<T extends ServletAuthApplication>
         }
     }
 
-    private SMCredentials getCurrentUserCreds() throws SMException {
+    @NotNull
+    protected SMCredentials getCurrentUserCreds() throws SMException {
         var currentUserCreds = credentialsProvider.getActiveUserCredentials();
         if (currentUserCreds == null) {
             throw new SMException("Unauthorized");
