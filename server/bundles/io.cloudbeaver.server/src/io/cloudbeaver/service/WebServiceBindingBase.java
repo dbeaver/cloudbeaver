178a049665c68becad710586443d1dd75a1be61f
Ruslan <pupkinvasya7000@gmail.com>
2026-04-03 17:30:54 +0200

    dbeaver/cloudbeaver#4120 support token header

--- server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/WebServiceBindingBase.java
+++ server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/WebServiceBindingBase.java
@@ -129,12 +129,10 @@ public abstract class WebServiceBindingBase<API_TYPE extends DBWService> impleme
         if (env.getGraphQlContext().getBoolean(CloudbeaverCliConstants.CLI_MODE)) {
             return getSessionFromContextOrThrow(env);
         }
-        //fixme как то получше написать
-        WebSession session = env.getGraphQlContext().get(WebSession.class.getName());
-        if (session != null) {
-            // retrieved session by token
-            return session;
-        }
+//        WebSession session = env.getGraphQlContext().get(WebSession.class.getName());
+//        if (session != null) {
+//            return session;
+//        }
         return WebAppUtils.getWebApplication().getSessionManager().getWebSession(
             GraphQLEndpoint.getServletRequestOrThrow(env), getServletResponse(env), errorOnNotFound);
     }
