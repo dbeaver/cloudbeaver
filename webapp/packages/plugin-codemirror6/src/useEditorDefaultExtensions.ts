f8a35fbe62951a14ca48dd01fcbf7b132f0b4b96
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-04-21 17:59:28 +0200

    adds EDITOR_INDENT_UNIT to unify indent for sql editor and vqb

--- webapp/packages/plugin-codemirror6/src/useEditorDefaultExtensions.ts
+++ webapp/packages/plugin-codemirror6/src/useEditorDefaultExtensions.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,7 @@ import { clsx } from '@dbeaver/ui-kit';
 
 // @TODO allow to configure bindings outside of the component
 const DEFAULT_KEY_MAP = defaultKeymap.filter(binding => binding.mac !== 'Ctrl-f' && binding.key !== 'Mod-Enter');
+export const EDITOR_INDENT_UNIT = '\t';
 
 DEFAULT_KEY_MAP.push(indentWithTab);
 DEFAULT_KEY_MAP.push({
@@ -111,7 +112,7 @@ const extensionMap = {
   rectangularSelection,
   keymap: () => keymap.of(DEFAULT_KEY_MAP),
   lineWrapping: () => EditorView.lineWrapping,
-  tabIndentation: () => indentUnit.of('\t'),
+  tabIndentation: () => indentUnit.of(EDITOR_INDENT_UNIT),
 };
 
 const DEFAULT_EXTENSIONS_COMPARTMENT = new Compartment();
