0bf927d1a6449eef40076a1d07c78d465fec7ea8
naumov <iamemptyhuh@gmail.com>
2026-04-22 19:25:16 +0200

    dbeaver/pro#8978 paste multiple values

--- webapp/packages/plugin-data-spreadsheet-new/src/DataGrid/useTableData.tsx
+++ webapp/packages/plugin-data-spreadsheet-new/src/DataGrid/useTableData.tsx
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-import { computed, observable } from 'mobx';
+import { action, computed, observable } from 'mobx';
 
 import { useObservableRef } from '@cloudbeaver/core-blocks';
 import {
@@ -179,6 +179,7 @@ export function useTableData(
       editor: observable.ref,
       view: observable.ref,
       gridDIVElement: observable.ref,
+      isCellEditable: action.bound,
     },
     {
       formatting,
