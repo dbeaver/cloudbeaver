14acf777b62549e28cc6ced1ba97744b9e96488f
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-05-07 18:41:44 +0200

    dbeaver/pro#9126 initial commit for PR message

--- webapp/packages/plugin-codemirror6/src/index.ts
+++ webapp/packages/plugin-codemirror6/src/index.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,8 @@ export * from '@codemirror/view';
 export * from '@codemirror/state';
 export * from '@codemirror/autocomplete';
 export * from './highlightNewLine.js';
+
+export { history } from '@codemirror/commands';
 
 export { html as HTML_EDITOR } from '@codemirror/lang-html';
 export { javascript as JAVASCRIPT_EDITOR } from '@codemirror/lang-javascript';
