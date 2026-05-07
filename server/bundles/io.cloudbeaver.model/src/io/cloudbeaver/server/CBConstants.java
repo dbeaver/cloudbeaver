a2292399a2586e3b1963e4aaf323ddd07529f949
Ainur <ainur.iagudin@dbeaver.com>
2026-05-07 10:35:03 +0200

    dbeaver/pro#9096 remove unused constant

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/server/CBConstants.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/server/CBConstants.java
@@ -82,6 +82,4 @@ public class CBConstants {
     public static final String DEFAULT_CLOUD_PROJECT_NAME = "GlobalConfiguration";
 
     public static final String SECURED_VALUE = "******";
-
-    public static final String SERVER_FEATURE_SSH = "ssh";
 }
