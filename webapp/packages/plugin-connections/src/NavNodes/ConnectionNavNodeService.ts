22503248e0ad7b6f5e790e2e182155143efab58f
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-05-13 15:01:47 +0200

    dbeaver/pro#8294 allows to refresh only connection root nodes for non-established connections

--- webapp/packages/plugin-connections/src/NavNodes/ConnectionNavNodeService.ts
+++ webapp/packages/plugin-connections/src/NavNodes/ConnectionNavNodeService.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -133,7 +133,13 @@ export class ConnectionNavNodeService {
     const notConnected = ResourceKeyUtils.some(key, key => {
       const connection = this.connectionInfoResource.getConnectionForNode(key);
 
-      return !!connection && !connection.connected;
+      if (!connection || connection.connected) {
+        return false;
+      }
+
+      // Allow refreshing the connection node itself; only block its children
+      const connectionNodeId = connection.nodePath ?? NodeManagerUtils.connectionIdToConnectionNodeId(connection.id);
+      return key !== connectionNodeId;
     });
 
     if (notConnected) {
