05dea0cc493dc87baf9e628ca010818b69b9719e
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-05-14 11:03:48 +0200

    adds node check instead of connection directly

--- webapp/packages/plugin-navigation-tree/src/NodesManager/NavNodeContextMenuService.ts
+++ webapp/packages/plugin-navigation-tree/src/NodesManager/NavNodeContextMenuService.ts
@@ -14,6 +14,7 @@ import { LocalizationService } from '@cloudbeaver/core-localization';
 import {
   DATA_CONTEXT_NAV_NODE,
   ENodeFeature,
+  EObjectFeature,
   getNodePlainName,
   type INodeActions,
   isConnectionFolder,
@@ -25,7 +26,7 @@ import {
   nodeDeleteContext,
   NodeManagerUtils,
 } from '@cloudbeaver/core-navigation-tree';
-import { ConnectionInfoResource } from '@cloudbeaver/core-connections';
+import { isConnectionNode } from '@cloudbeaver/core-connections';
 import { ResourceKeyUtils } from '@cloudbeaver/core-resource';
 import {
   ACTION_DELETE,
@@ -58,7 +59,6 @@ export interface INodeMenuData {
   LocalizationService,
   NavNodeInfoResource,
   NavTreeSettingsService,
-  ConnectionInfoResource,
 ])
 export class NavNodeContextMenuService extends Bootstrap {
   constructor(
@@ -71,7 +71,6 @@ export class NavNodeContextMenuService extends Bootstrap {
     private readonly localizationService: LocalizationService,
     private readonly navNodeInfoResource: NavNodeInfoResource,
     private readonly navTreeSettingsService: NavTreeSettingsService,
-    private readonly connectionInfoResource: ConnectionInfoResource,
   ) {
     super();
   }
@@ -204,10 +203,8 @@ export class NavNodeContextMenuService extends Bootstrap {
           }
           case ACTION_REFRESH: {
             try {
-              const connection = this.connectionInfoResource.getConnectionForNode(node.id);
-              if (connection && !connection.connected) {
-                const connectionNodeId = connection.nodePath ?? NodeManagerUtils.connectionIdToConnectionNodeId(connection.id);
-                await this.navNodeInfoResource.refresh(connectionNodeId);
+              if (isConnectionNode(node) && !node.objectFeatures.includes(EObjectFeature.dataSourceConnected)) {
+                await this.navNodeInfoResource.refresh(node.id);
               } else {
                 await this.navNodeManagerService.refreshTree(node.id);
               }
