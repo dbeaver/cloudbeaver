f27216250c9915ff466323358e6a19f1805a60f5
Sychev Andrey <nukemoore@gmail.com>
2026-05-28 15:02:07 +0200

    dbeaver/pro#9436 feat: add isNumber and replaceMiddle utility functions

--- webapp/common-typescript/@dbeaver/js-helpers/src/index.ts
+++ webapp/common-typescript/@dbeaver/js-helpers/src/index.ts
@@ -17,3 +17,5 @@ export * from './mutex.js';
 export * from './reorderArray.js';
 export * from './getLocalizedDisplayName.js';
 export * from './formatNumber.js';
+export * from './isNumber.js';
+export * from './replaceMiddle.js';
