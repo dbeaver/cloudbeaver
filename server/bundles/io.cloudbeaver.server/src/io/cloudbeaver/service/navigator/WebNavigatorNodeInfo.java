f6cf08a22b22c5398510ffa9503bacbaec81e991
Serge Rider <serge@dbeaver.com>
2026-05-28 16:44:31 +0200

    dbeaver/pro#7128 Return DS id in objectId

--- server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/navigator/WebNavigatorNodeInfo.java
+++ server/bundles/io.cloudbeaver.server/src/io/cloudbeaver/service/navigator/WebNavigatorNodeInfo.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -209,6 +209,8 @@ public class WebNavigatorNodeInfo {
             return DBFUtils.getUriFromPath(dbnPath.getPath()).toString();
         } else if (node instanceof DBNFileSystem dbnFs) {
             return WebFSUtils.makeUniqueFsId(dbnFs.getFileSystem());
+        } else if (node instanceof DBNDataSource ds) {
+            return ds.getDataSourceContainer().getId();
         }
         return null;
     }
