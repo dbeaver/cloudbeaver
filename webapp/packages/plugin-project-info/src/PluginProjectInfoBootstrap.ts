565b08d40a6174da339c932ae65a8bd8ee0eb3c8
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-06-05 13:07:53 +0200

    adds project info to the context menu

--- webapp/packages/plugin-project-info/src/PluginProjectInfoBootstrap.ts
+++ webapp/packages/plugin-project-info/src/PluginProjectInfoBootstrap.ts
@@ -7,14 +7,38 @@
  */
 
 import { Bootstrap, injectable } from '@cloudbeaver/core-di';
+import { DATA_CONTEXT_NAV_NODE } from '@cloudbeaver/core-navigation-tree';
+import { ActionService, MenuService } from '@cloudbeaver/core-view';
 
-@injectable()
+import { ACTION_PROJECT_INFO } from './actions/ACTION_PROJECT_INFO.js';
+
+@injectable(() => [ActionService, MenuService])
 export class PluginProjectInfoBootstrap extends Bootstrap {
-  constructor() {
+  constructor(
+    private readonly actionService: ActionService,
+    private readonly menuService: MenuService,
+  ) {
     super();
   }
 
   override register() {
-    console.log('plugin project info is registered');
+    this.menuService.addCreator({
+      root: true,
+      contexts: [DATA_CONTEXT_NAV_NODE],
+      getItems: (context, items) => [...items, ACTION_PROJECT_INFO],
+    });
+
+    this.actionService.addHandler({
+      id: 'action-project-info-handler',
+      actions: [ACTION_PROJECT_INFO],
+      contexts: [DATA_CONTEXT_NAV_NODE],
+      isActionApplicable: context => {
+        const node = context.get(DATA_CONTEXT_NAV_NODE);
+        return !!node?.projectId;
+      },
+      handler: (context, action) => {
+        // TODO: implement project info action handler
+      },
+    });
   }
 }
--- webapp/packages/plugin-project-info/src/index.ts
+++ webapp/packages/plugin-project-info/src/index.ts
@@ -1,1 +1,3 @@
 import './module.js';
+
+export { ACTION_PROJECT_INFO } from './actions/ACTION_PROJECT_INFO.js';
