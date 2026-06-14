8df9e2b37c13bb168a0025f2d3ab35e6e1da3d9c
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-06-14 15:30:33 +0200

    pr fixes

--- webapp/packages/plugin-connection-network-handlers/src/module.ts
+++ webapp/packages/plugin-connection-network-handlers/src/module.ts
@@ -1,13 +1,18 @@
+/*
+ * CloudBeaver - Cloud Database Manager
+ * Copyright (C) 2020-2026 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * you may not use this file except in compliance with the License.
+ */
+
 import { Bootstrap, ModuleRegistry } from '@cloudbeaver/core-di';
 import { LocaleService } from './LocaleService.js';
-import { PluginConnectionNetworkHandlersBootstrap } from './PluginConnectionNetworkHandlersBootstrap.js';
 
 export default ModuleRegistry.add({
   name: '@cloudbeaver/plugin-connection-network-handlers',
 
   configure: serviceCollection => {
-    serviceCollection
-      .addSingleton(Bootstrap, LocaleService)
-      .addSingleton(Bootstrap, PluginConnectionNetworkHandlersBootstrap);
+    serviceCollection.addSingleton(Bootstrap, LocaleService);
   },
 });
