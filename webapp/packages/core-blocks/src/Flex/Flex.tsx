38943a3803bb9020cfb53973917d2006963d3314
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-06-16 10:52:23 +0200

    dbeaver/pro#9526 small ui adjustments

--- webapp/packages/core-blocks/src/Flex/Flex.tsx
+++ webapp/packages/core-blocks/src/Flex/Flex.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@ import classes from './Flex.module.css';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   overflow?: boolean;
-  gap?: 'xs' | 'md' | 'lg';
+  gap?: 'xxs' | 'xs' | 'md' | 'lg';
   wrap?: React.CSSProperties['flexWrap'];
   direction?: React.CSSProperties['flexDirection'];
   align?: React.CSSProperties['alignItems'];
