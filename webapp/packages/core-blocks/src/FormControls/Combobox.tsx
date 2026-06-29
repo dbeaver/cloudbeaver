a34c2f73d73c853d8ff65978c2b812c3ab641ff2
Sychev Andrey <nukemoore@gmail.com>
2026-06-29 11:34:50 +0200

    dbeaver/pro#9704 refactor: add text overflow handling for combobox item values
    remove tw:truncate because it adds hidden text-sm font-size

--- webapp/packages/core-blocks/src/FormControls/Combobox.tsx
+++ webapp/packages/core-blocks/src/FormControls/Combobox.tsx
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -274,7 +274,7 @@ export const Combobox: ComboboxType = observer(function Combobox({
                         {itemIcon && typeof itemIcon === 'string' ? <IconOrImage icon={itemIcon} /> : itemIcon}
                       </div>
                     )}
-                    <div className="tw:truncate">{itemValue}</div>
+                    <div className="combobox__item-value">{itemValue}</div>
                   </ComboboxItem>
                 ))
               ) : (
