97d69b9b2fa54cf699832fedbcaeccb891102d81
daniil.grigorev <daniil.grigorev@dbeaver.com>
2026-07-13 16:27:38 +0200

    dbeaver/pro#9721 [CB] code style fix

--- server/bundles/io.cloudbeaver.service.auth/src/io/cloudbeaver/service/auth/ReverseProxyConfigurator.java
+++ server/bundles/io.cloudbeaver.service.auth/src/io/cloudbeaver/service/auth/ReverseProxyConfigurator.java
@@ -82,7 +82,7 @@ public class ReverseProxyConfigurator implements DBWServiceServerConfigurator {
             parameters.put(RPConstants.PARAM_FULL_NAME, RPAuthProvider.X_FULL_NAME);
             parameters.put(RPAuthProvider.PARAM_AUTO_USER_PROVISIONING, true);
             smReverseProxyProviderConfiguration.setParameters(parameters);
-            authApplication.getAuthConfiguration().addAuthProviderConfiguration(smReverseProxyProviderConfiguration );
+            authApplication.getAuthConfiguration().addAuthProviderConfiguration(smReverseProxyProviderConfiguration);
             try {
                 authApplication.flushConfiguration();
             } catch (Exception e) {
