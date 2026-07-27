7282eca45207dcd859a372f11657d88918692078
daniil.grigorev <daniil.grigorev@dbeaver.com>
2026-07-27 11:56:06 +0200

    dbeaver/pro#9822 comment fix

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/utils/WebDataSourceUtils.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/utils/WebDataSourceUtils.java
@@ -75,7 +75,6 @@ public class WebDataSourceUtils {
         @NotNull DBPConnectionConfiguration configuration
     ) {
         // Properties passed from web
-        // webConnectionInfo may be null in some cases (e.g. connection test when no actual connection exist yet)
         Map<String, Object> authProperties = webConnectionInfo.getSavedAuthProperties();
         if (authProperties != null) {
             authProperties.forEach((s, o) -> configuration.setAuthProperty(s, CommonUtils.toString(o)));
