cc304635107acef861170a385022cfb305c9898f
naumov <iamemptyhuh@gmail.com>
2026-07-27 13:03:27 +0200

    dbeaver/pro#4425 update notice

--- webapp/packages/plugin-connections/src/locales/zh.ts
+++ webapp/packages/plugin-connections/src/locales/zh.ts
@@ -13,8 +13,6 @@ export default [
   ['plugin_connections_connection_edit_menu_item_title', '编辑连接'],
   ['plugin_connections_connection_clone', '克隆'],
   ['plugin_connections_connection_clone_error', '克隆连接失败'],
-  ['plugin_connections_connection_edit_cancel_title', '取消确认'],
-  ['plugin_connections_connection_edit_cancel_message', '您将取消连接更改。未保存的更改将丢失。您确定吗？'],
   ['plugin_connections_connection_edit_reconnect_title', '连接已更新'],
   ['plugin_connections_connection_edit_reconnect_message', '连接已更新。您想重新连接吗？'],
   ['plugin_connections_connection_edit_reconnect_failed', '重新连接失败'],
