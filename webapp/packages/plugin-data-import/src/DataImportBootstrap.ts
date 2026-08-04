8c16f0fbf867e541fad1e079974a3e1d6f020181
naumov <iamemptyhuh@gmail.com>
2026-08-04 20:47:03 +0200

    dbeaver/pro#9637 support edit and import restriction

--- webapp/packages/plugin-data-import/src/DataImportBootstrap.ts
+++ webapp/packages/plugin-data-import/src/DataImportBootstrap.ts
@@ -8,6 +8,7 @@
 import { Bootstrap, injectable } from '@cloudbeaver/core-di';
 import { CommonDialogService, DialogueStateResult } from '@cloudbeaver/core-dialogs';
 import { ACTION_IMPORT, ActionService, menuExtractItems, MenuService } from '@cloudbeaver/core-view';
+import { ConnectionInfoResource, createConnectionParam, EConnectionFeature } from '@cloudbeaver/core-connections';
 import {
   DATA_CONTEXT_DV_DDM,
   DATA_CONTEXT_DV_DDM_RESULT_INDEX,
@@ -16,18 +17,21 @@ import {
   DatabaseDataFeature,
   DataViewerPresentationType,
   isResultSetDataModel,
+  ResultSetDataSource,
+  type IDatabaseDataModel,
 } from '@cloudbeaver/plugin-data-viewer';
 
 import { DataImportDialogLazy } from './DataImportDialog/DataImportDialogLazy.js';
 import { DataImportService } from './DataImportService.js';
 
-@injectable(() => [MenuService, ActionService, CommonDialogService, DataImportService])
+@injectable(() => [MenuService, ActionService, CommonDialogService, DataImportService, ConnectionInfoResource])
 export class DataImportBootstrap extends Bootstrap {
   constructor(
     private readonly menuService: MenuService,
     private readonly actionService: ActionService,
     private readonly commonDialogService: CommonDialogService,
     private readonly dataImportService: DataImportService,
+    private readonly connectionInfoResource: ConnectionInfoResource,
   ) {
     super();
   }
@@ -96,9 +100,21 @@ export class DataImportBootstrap extends Bootstrap {
       menus: [DATA_VIEWER_DATA_MODEL_ACTIONS_MENU],
       contexts: [DATA_CONTEXT_DV_DDM, DATA_CONTEXT_DV_DDM_RESULT_INDEX],
       isApplicable: context => {
-        const model = context.get(DATA_CONTEXT_DV_DDM)!;
+        const model = context.get(DATA_CONTEXT_DV_DDM)! as unknown as IDatabaseDataModel<ResultSetDataSource>;
         const presentation = context.get(DATA_CONTEXT_DV_PRESENTATION);
         const resultIndex = context.get(DATA_CONTEXT_DV_DDM_RESULT_INDEX)!;
+
+        const executionContext = model.source.executionContext?.context;
+
+        if (executionContext) {
+          const connectionKey = createConnectionParam(executionContext.projectId, executionContext.connectionId);
+          const connection = this.connectionInfoResource.get(connectionKey);
+
+          if (connection?.features.includes(EConnectionFeature.restrictDataImport)) {
+            return false;
+          }
+        }
+
         const allowedFeatures = [DatabaseDataFeature.DataEditor];
 
         return (
--- webapp/packages/plugin-data-viewer/src/DataViewerService.ts
+++ webapp/packages/plugin-data-viewer/src/DataViewerService.ts
@@ -1,11 +1,11 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-import type { Connection } from '@cloudbeaver/core-connections';
+import { EConnectionFeature, type Connection } from '@cloudbeaver/core-connections';
 import { injectable } from '@cloudbeaver/core-di';
 import { EAdminPermission, SessionPermissionsResource } from '@cloudbeaver/core-root';
 import { PlaceholderContainer } from '@cloudbeaver/core-blocks';
@@ -37,7 +37,7 @@ export class DataViewerService {
   }
 
   isDataEditable(connection: Connection) {
-    if (connection.readOnly) {
+    if (connection.readOnly || connection.features.includes(EConnectionFeature.restrictDataEdit)) {
       return false;
     }
 
