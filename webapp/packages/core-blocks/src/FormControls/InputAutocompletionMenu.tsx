4bea61ffd8906bc49fdabccea9e1c5d19a827538
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-08-07 12:43:43 +0200

    dbeaver/pro#9982 adds position prop to InputAutocompletionMenu for accurate placement

--- webapp/packages/core-blocks/src/FormControls/InputAutocompletionMenu.tsx
+++ webapp/packages/core-blocks/src/FormControls/InputAutocompletionMenu.tsx
@@ -10,22 +10,45 @@ import { observer } from 'mobx-react-lite';
 import { Composite, CompositeItem, Popover } from '@dbeaver/ui-kit';
 
 import { type InputAutocompleteProposal } from './useInputAutocomplete.js';
+import type { IContextMenuPositionCoords } from '../Menu/useContextMenuPosition.js';
 import { useS } from '../useS.js';
 import { BaseDropdownStyles, IconOrImage, s, Text } from '../index.js';
 import { forwardRef } from 'react';
 
 interface AutocompletionProps {
   proposals: InputAutocompleteProposal[];
+  position: IContextMenuPositionCoords;
   className?: string;
   onSelect?: (proposal: InputAutocompleteProposal) => void;
 }
 
 export const InputAutocompletionMenu = observer(
-  forwardRef<HTMLDivElement, AutocompletionProps>(function InputAutocompletionMenu({ proposals, onSelect }, ref) {
+  forwardRef<HTMLDivElement, AutocompletionProps>(function InputAutocompletionMenu({ proposals, position, onSelect }, ref) {
     const styles = useS(BaseDropdownStyles);
 
+    function getAnchorRect(anchor: HTMLElement | null) {
+      if (!anchor) {
+        return null;
+      }
+
+      const rect = anchor.getBoundingClientRect();
+
+      return {
+        x: rect.left + position.x,
+        y: rect.top + position.y,
+      };
+    }
+
     return (
-      <Popover.PopoverContent ref={ref} className={s(styles, { menu: true })} gutter={4} modal={false} autoFocusOnShow={false} unmountOnHide>
+      <Popover.PopoverContent
+        ref={ref}
+        className={s(styles, { menu: true })}
+        gutter={4}
+        modal={false}
+        autoFocusOnShow={false}
+        getAnchorRect={getAnchorRect}
+        unmountOnHide
+      >
         <Composite>
           {proposals.map(proposal => (
             <CompositeItem key={proposal.title} className={s(styles, { menuItem: true }, 'tw:w-full')} onClick={() => onSelect?.(proposal)}>
