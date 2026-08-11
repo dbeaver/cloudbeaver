bd1e926929461ee8613272edc77ee8cbb8bbad52
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-08-11 12:29:14 +0200

    dbeaver/pro#9981 adds export button for admin users page (#4531)
    * dbeaver/pro#9981 adds export button for admin users page
    
    * dbeaver/pro#9981 uses fetch to download csv instead of submitForm approach (backend issue)
    
    * dbeaver/pro#9981 makes users export to be available only for PRO products
    
    * dbeaver/pro#9981 reverts export logic
    
    * dbeaver/pro#9981 add actionButtonsPlaceholder
    
    * dbeaver/pro#9981 cleanup
    
    ---------
    
    Co-authored-by: Evgenia <139753579+EvgeniaBzzz@users.noreply.github.com>

--- webapp/packages/plugin-authentication-administration/src/index.ts
+++ webapp/packages/plugin-authentication-administration/src/index.ts
@@ -22,3 +22,5 @@ export * from './AdministrationUsersManagementService.js';
 export * from './externalUserProviderStatusContext.js';
 export * from './Administration/Users/Teams/TeamsForm/Options/getTeamOptionsFormPart.js';
 export * from './Administration/Users/UsersTable/UsersTableOptionsPanelService.js';
+export * from './Administration/Users/UsersTable/Filters/useUsersTableFilters.js';
+export * from './Administration/Users/UsersTable/Filters/FiltersActionButton.js';
