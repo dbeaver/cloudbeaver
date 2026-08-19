67df14033c81569ea81b76dd984c86aa1d5f6f6d
Sychev Andrey <44414066+SychevAndrey@users.noreply.github.com>
2026-08-19 12:29:30 +0200

    dbeaver/pro#9948 fix: show unsaved changes dialog on tab change (#4544)
    * dbeaver/pro#9948 fix: show unsaved changes dialog on tab change
    
    fixes bug with tabs fire executors twice
    
    * dbeaver/pro#9948 refactor: move logic to tab service
    
    * dbeaver/pro#9948 feat: add unsaved changes dialog for password change tab
    
    ---------
    
    Co-authored-by: Daria Marutkina <125263541+dariamarutkina@users.noreply.github.com>

--- webapp/packages/core-ui/src/Tabs/Tab/useTab.ts
+++ webapp/packages/core-ui/src/Tabs/Tab/useTab.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -77,7 +77,6 @@ export function useTab(
           return;
         }
         refObject.onClick?.(this.tabId);
-        this.state.open(this.tabId);
       },
       handleClose(e: React.MouseEvent<HTMLDivElement>) {
         EventContext.set(e, EventStopPropagationFlag); // TODO: probably should use special flag
