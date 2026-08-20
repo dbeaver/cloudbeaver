aeb6fe0e8b4af8df41367eecf5e8679b99cbc6a6
naumov <iamemptyhuh@gmail.com>
2026-08-20 18:20:21 +0200

    dbeaver/pro#10317 hide empty handlers from auth dialog

--- webapp/packages/plugin-connections/src/ConnectionAuthService.ts
+++ webapp/packages/plugin-connections/src/ConnectionAuthService.ts
@@ -21,7 +21,10 @@ import {
 import { injectable } from '@cloudbeaver/core-di';
 import { CommonDialogService, DialogueStateResult } from '@cloudbeaver/core-dialogs';
 import type { IExecutionContextProvider } from '@cloudbeaver/core-executor';
+import { CachedMapAllKey } from '@cloudbeaver/core-resource';
+import type { NetworkHandlerDescriptor } from '@cloudbeaver/core-sdk';
 import { AuthenticationService } from '@cloudbeaver/plugin-authentication';
+import { NetworkHandlerResource } from '@cloudbeaver/plugin-network-handlers';
 
 const DatabaseAuthDialog = importLazyComponent(() => import('./DatabaseAuthDialog/DatabaseAuthDialog.js').then(m => m.DatabaseAuthDialog));
 
@@ -34,6 +37,7 @@ const DatabaseAuthDialog = importLazyComponent(() => import('./DatabaseAuthDialo
   UserInfoResource,
   ConnectionsManagerService,
   AuthenticationService,
+  NetworkHandlerResource,
 ])
 export class ConnectionAuthService {
   constructor(
@@ -45,6 +49,7 @@ export class ConnectionAuthService {
     userInfoResource: UserInfoResource,
     private readonly connectionsManagerService: ConnectionsManagerService,
     private readonly authenticationService: AuthenticationService,
+    private readonly networkHandlerResource: NetworkHandlerResource,
   ) {
     connectionsManagerService.connectionExecutor.addHandler(this.connectionDialog.bind(this));
     this.authenticationService.onLogin.before(
@@ -82,6 +87,7 @@ export class ConnectionAuthService {
     }
 
     let connectionNetworkHandlers: ConnectionInfoNetworkHandlers | null = null;
+    let handlers: NetworkHandlerDescriptor[] = [];
     let connection = await this.connectionInfoResource.load(key);
     const isConnectedInitially = connection?.connected;
 
@@ -103,13 +109,22 @@ export class ConnectionAuthService {
       }
     }
 
-    [connectionNetworkHandlers, connection] = await Promise.all([
+    [connectionNetworkHandlers, connection, handlers] = await Promise.all([
       this.connectionInfoNetworkHandlersResource.load(key),
       this.connectionInfoResource.load(key),
+      this.networkHandlerResource.load(CachedMapAllKey),
     ]);
 
     const networkHandlers = connectionNetworkHandlers
-      .networkHandlersConfig!.filter(handler => handler.enabled && (!handler.savePassword || resetCredentials))
+      .networkHandlersConfig!.filter(handler => {
+        const target = handlers.find(h => h.id === handler.id);
+
+        if (target && target.properties.length === 0) {
+          return false;
+        }
+
+        return handler.enabled && (!handler.savePassword || resetCredentials);
+      })
       .map(handler => handler.id);
 
     if (connectionAuthProperties.authNeeded || (connectionAuthProperties.credentialsSaved && resetCredentials) || networkHandlers.length > 0) {
