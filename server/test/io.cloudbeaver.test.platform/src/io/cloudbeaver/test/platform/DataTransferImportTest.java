68042096f0a0c70135e4acbb7b6150ed0b3dc00d
daniil.grigorev <daniil.grigorev@dbeaver.com>
2026-09-06 21:07:05 +0200

    dbeaver/pro#10443 fix linter

--- server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/DataTransferImportTest.java
+++ server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/DataTransferImportTest.java
@@ -48,8 +48,8 @@ import org.jkiss.dbeaver.model.struct.DBSObject;
 import org.jkiss.dbeaver.model.struct.DBSObjectContainer;
 import org.jkiss.dbeaver.utils.GeneralUtils;
 import org.jkiss.utils.HttpConstants;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
