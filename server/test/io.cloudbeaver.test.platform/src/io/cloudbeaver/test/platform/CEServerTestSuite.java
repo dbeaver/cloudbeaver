a18b1bd0f4db9168bc71c8a483bb417468121288
Ruslan <pupkinvasya7000@gmail.com>
2026-09-07 17:29:25 +0200

    dbeaver/pro#10370 Deleted used Connection type remains applied

--- server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/CEServerTestSuite.java
+++ server/test/io.cloudbeaver.test.platform/src/io/cloudbeaver/test/platform/CEServerTestSuite.java
@@ -24,6 +24,7 @@ import io.cloudbeaver.model.rm.local.LocalResourceControllerTest;
 import io.cloudbeaver.model.rm.lock.RMLockTest;
 import io.cloudbeaver.model.session.WebSessionProjectTest;
 import io.cloudbeaver.model.session.WebSessionTest;
+import io.cloudbeaver.server.events.WSEventHandlerWorkspaceConfigUpdateTest;
 import io.cloudbeaver.test.platform.admin.AdminCreateUserTest;
 import io.cloudbeaver.test.platform.admin.AdminImportUsersTest;
 import io.cloudbeaver.test.platform.admin.AdminLastLoginTimeTest;
@@ -46,6 +47,7 @@ import org.junit.platform.suite.api.Suite;
         NoSessionTest.class,
         WebSessionTest.class,
         WebSessionProjectTest.class,
+        WSEventHandlerWorkspaceConfigUpdateTest.class,
         WebNavigatorNodeInfoTest.class,
         AdminCreateUserTest.class,
         AdminImportUsersTest.class,
