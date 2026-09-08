4ce9ca53571cc09d95cf1fd8b8f23bd4e02bfa7e
Ruslan Musaev <43766501+HocKu7@users.noreply.github.com>
2026-09-08 08:51:03 +0200

    dbeaver/pro#10264 fix security issues (#4596)
    Co-authored-by: mr-anton-t <42037741+mr-anton-t@users.noreply.github.com>

--- server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/fs/WebFSUtils.java
+++ server/bundles/io.cloudbeaver.model/src/io/cloudbeaver/model/fs/WebFSUtils.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,9 +26,40 @@ import org.jkiss.dbeaver.model.navigator.DBNNode;
 import org.jkiss.dbeaver.model.navigator.fs.DBNPathBase;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 
 public class WebFSUtils {
+    @NotNull
+    public static Path resolveSafeChild(@NotNull Path parent, @NotNull String childName) throws DBException {
+        if (childName.isBlank()
+            || ".".equals(childName)
+            || "..".equals(childName)
+            || childName.indexOf('/') >= 0
+            || childName.indexOf('\\') >= 0
+            || (childName.length() >= 2
+                && Character.isLetter(childName.charAt(0))
+                && childName.charAt(1) == ':')
+        ) {
+            throw new DBException("Invalid file name");
+        }
+
+        try {
+            Path child = Path.of(childName);
+            if (child.isAbsolute() || child.getNameCount() != 1) {
+                throw new DBException("Invalid file name");
+            }
+            Path normalizedParent = parent.normalize();
+            Path resolvedChild = normalizedParent.resolve(child).normalize();
+            if (!resolvedChild.startsWith(normalizedParent)) {
+                throw new DBException("Invalid file name");
+            }
+            return resolvedChild;
+        } catch (InvalidPathException e) {
+            throw new DBException("Invalid file name", e);
+        }
+    }
+
     @NotNull
     public static String makeUniqueFsId(@NotNull DBFVirtualFileSystem fileSystem) {
         return fileSystem.getType() + "://" + fileSystem.getId();
