34f50a6859dcf07d9b9ccc53a64f469330b606e8
naumov <iamemptyhuh@gmail.com>
2026-09-08 14:04:18 +0200

    dbeaver/pro#9243 remove deprecated settings

--- webapp/packages/core-authentication/src/AuthSettingsService.test.ts
+++ webapp/packages/core-authentication/src/AuthSettingsService.test.ts
@@ -1,11 +1,11 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
-import { beforeEach, describe, expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 import { ServerConfigResource } from '@cloudbeaver/core-root';
 import { createGQLEndpoint } from '@cloudbeaver/core-root/__custom_mocks__/createGQLEndpoint.js';
@@ -13,19 +13,11 @@ import '@cloudbeaver/core-root/__custom_mocks__/expectWebsocketClosedMessage.js'
 import { mockAppInit } from '@cloudbeaver/core-root/__custom_mocks__/mockAppInit.js';
 import { mockGraphQL } from '@cloudbeaver/core-root/__custom_mocks__/mockGraphQL.js';
 import { mockServerConfig } from '@cloudbeaver/core-root/__custom_mocks__/resolvers/mockServerConfig.js';
-import { expectNoDeprecatedSettingMessage } from '@cloudbeaver/core-settings/__custom_mocks__/expectDeprecatedSettingMessage.js';
 import { createApp, initKnownConsoleMessages } from '@cloudbeaver/tests-runner';
 
 import { mockAuthentication } from './__custom_mocks__/mockAuthentication.js';
 import { AuthSettingsService } from './AuthSettingsService.js';
 import './module.js';
-import { DEPRECATED_SETTINGS } from '@cloudbeaver/core-settings';
-
-function resetDeprecatedSettings() {
-  beforeEach(() => {
-    DEPRECATED_SETTINGS.clear();
-  });
-}
 
 describe.skip(() => {
   const endpoint = createGQLEndpoint();
@@ -37,7 +29,6 @@ describe.skip(() => {
   };
 
   initKnownConsoleMessages();
-  resetDeprecatedSettings();
 
   test('Read settings', async () => {
     const settings = app.serviceProvider.getService(AuthSettingsService);
@@ -48,6 +39,5 @@ describe.skip(() => {
     await config.refresh();
 
     expect(settings.disableAnonymousAccess).toBe(true);
-    expectNoDeprecatedSettingMessage();
   });
 });
--- webapp/packages/plugin-resource-manager/src/ResourceManagerSettingsService.ts
+++ webapp/packages/plugin-resource-manager/src/ResourceManagerSettingsService.ts
@@ -1,18 +1,12 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
 import { injectable } from '@cloudbeaver/core-di';
-import {
-  createSettingsAliasResolver,
-  ROOT_SETTINGS_LAYER,
-  SettingsProvider,
-  SettingsProviderService,
-  SettingsResolverService,
-} from '@cloudbeaver/core-settings';
+import { SettingsProvider, SettingsProviderService } from '@cloudbeaver/core-settings';
 import { schema, schemaExtra } from '@cloudbeaver/core-utils';
 
 const defaultSettings = schema.object({
@@ -22,24 +16,14 @@ const defaultSettings = schema.object({
 export type ResourceManagerSettingsSchema = typeof defaultSettings;
 export type ResourceManagerSettings = schema.infer<ResourceManagerSettingsSchema>;
 
-@injectable(() => [SettingsProviderService, SettingsResolverService])
+@injectable(() => [SettingsProviderService])
 export class ResourceManagerSettingsService {
   get disabled(): boolean {
     return this.settings.getValue('plugin.resource-manager.disabled');
   }
   readonly settings: SettingsProvider<ResourceManagerSettingsSchema>;
 
-  constructor(
-    private readonly settingsProviderService: SettingsProviderService,
-    private readonly settingsResolverService: SettingsResolverService,
-  ) {
+  constructor(private readonly settingsProviderService: SettingsProviderService) {
     this.settings = this.settingsProviderService.createSettings(defaultSettings);
-    this.settingsResolverService.addResolver(
-      ROOT_SETTINGS_LAYER,
-      /** @deprecated Use settings instead, will be removed in 23.0.0 */
-      createSettingsAliasResolver<ResourceManagerSettingsSchema>(this.settingsProviderService.settingsResolver, {
-        'plugin.resource-manager.disabled': 'plugin_resource_manager.disabled',
-      }),
-    );
   }
 }
