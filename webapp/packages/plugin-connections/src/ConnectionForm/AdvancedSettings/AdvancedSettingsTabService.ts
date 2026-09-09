7eb5df0783178822bbf94dd0b8cd56291b6acadc
alex <48489896+devnaumov@users.noreply.github.com>
2026-09-09 10:14:00 +0200

    dbeaver/pro#10012 redesign connection form (#4641)
    * dbeaver/pro#10012 redesign connection form
    
    * dbeaver/pro#10012 fixes build
    
    * dbeaver/pro#10012 icons moved to packages respectively, moves readonly logic to helper, fixes broken layout with Groups component
    
    * dbeaver/pro#10012 hide empty tabs
    
    ---------
    
    Co-authored-by: sergeyteleshev <sergei.teleshev@dbeaver.com>
    Co-authored-by: sergeyteleshev <iamsergeyteleshev@gmail.com>
    Co-authored-by: mr-anton-t <42037741+mr-anton-t@users.noreply.github.com>

--- webapp/packages/plugin-connections/src/ConnectionForm/AdvancedSettings/AdvancedSettingsTabService.ts
+++ webapp/packages/plugin-connections/src/ConnectionForm/AdvancedSettings/AdvancedSettingsTabService.ts
@@ -7,15 +7,22 @@
  */
 
 import { importLazyComponent } from '@cloudbeaver/core-blocks';
+import { DBDriverResource } from '@cloudbeaver/core-connections';
 import { Bootstrap, injectable } from '@cloudbeaver/core-di';
+import { getCachedMapResourceLoaderState } from '@cloudbeaver/core-resource';
 
 import { ConnectionFormService } from '../ConnectionFormService.js';
+import { getConnectionFormOptionsPart } from '../Options/getConnectionFormOptionsPart.js';
+import { isProviderPropertySupported } from './isProviderPropertySupported.js';
 
 const AdvancedSettings = importLazyComponent(() => import('./AdvancedSettings.js').then(m => m.AdvancedSettings));
 
-@injectable(() => [ConnectionFormService])
+@injectable(() => [ConnectionFormService, DBDriverResource])
 export class AdvancedSettingsTabService extends Bootstrap {
-  constructor(private readonly connectionFormService: ConnectionFormService) {
+  constructor(
+    private readonly connectionFormService: ConnectionFormService,
+    private readonly dbDriverResource: DBDriverResource,
+  ) {
     super();
   }
 
@@ -23,9 +30,38 @@ export class AdvancedSettingsTabService extends Bootstrap {
     this.connectionFormService.parts.add({
       key: 'advanced_settings',
       name: 'plugin_connections_connection_form_part_advanced',
+      title: 'plugin_connections_connection_form_part_advanced',
       icon: '/icons/plugin_connection_bulleted_list.svg',
       order: 9,
       panel: () => AdvancedSettings,
+      getLoader: (_, props) => {
+        const optionsPart = props?.formState ? getConnectionFormOptionsPart(props.formState) : null;
+
+        return getCachedMapResourceLoaderState(
+          this.dbDriverResource,
+          () => optionsPart?.state.driverId ?? null,
+          () => ['includeProviderProperties'],
+        );
+      },
+      isHidden: (_, props) => {
+        if (!props) {
+          return true;
+        }
+
+        if (this.connectionFormService.providerPropertiesContainer.getDisplayed(props).length > 0) {
+          return false;
+        }
+
+        const optionsPart = getConnectionFormOptionsPart(props.formState);
+        const driver = optionsPart.state.driverId ? this.dbDriverResource.get(optionsPart.state.driverId) : null;
+        const configurationType = optionsPart.state.configurationType;
+
+        if (configurationType === undefined) {
+          return true;
+        }
+
+        return !driver?.providerProperties?.some(property => isProviderPropertySupported(property, configurationType));
+      },
     });
   }
 }
