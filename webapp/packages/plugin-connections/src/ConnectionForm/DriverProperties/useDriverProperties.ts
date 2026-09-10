ef241a38afb7f12b5044525655fc18645b18984c
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-09-10 11:28:25 +0200

    Revert "dbeaver/pro#10480 fixes driver properties form state"
    This reverts commit 2022fdd705a6a3ecd225527c330b9961f63f5fc5.

--- webapp/packages/plugin-connections/src/ConnectionForm/DriverProperties/useDriverProperties.ts
+++ webapp/packages/plugin-connections/src/ConnectionForm/DriverProperties/useDriverProperties.ts
@@ -57,6 +57,10 @@ export function useDriverProperties(payload: Payload) {
         try {
           this.exception = null;
 
+          // We are executing a format task, so the config was fully updated with defaults.
+          // We need to guarantee that the config is the same as the one sent to the test connection function;
+          // otherwise, the backend will return only partial data.
+          await this.payload.formState.formatTask.execute(this.payload.formState);
           this.promise = this.connectionInfoResource.getConnectionDriverProperties(this.payload.formState.state.projectId, this.payload.config);
           const properties = await this.promise;
           this.properties = properties;
