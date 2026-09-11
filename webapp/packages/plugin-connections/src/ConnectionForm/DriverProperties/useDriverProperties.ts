909e0ec24858d8a59c385b571ec0d7fa45eb952d
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-09-11 17:07:47 +0200

    dbeaver/pro#10480 cleanup

--- webapp/packages/plugin-connections/src/ConnectionForm/DriverProperties/useDriverProperties.ts
+++ webapp/packages/plugin-connections/src/ConnectionForm/DriverProperties/useDriverProperties.ts
@@ -57,7 +57,7 @@ export function useDriverProperties(payload: Payload) {
         try {
           this.exception = null;
 
-          // Prepare the config with defaults before requesting config-dependent driver properties.
+          // We are executing a format task, so the config was fully updated with defaults.
           // We need to guarantee that the config is the same as the one sent to the test connection function;
           // otherwise, the backend will return only partial data.
           await this.payload.formState.formatTask.execute(this.payload.formState);
