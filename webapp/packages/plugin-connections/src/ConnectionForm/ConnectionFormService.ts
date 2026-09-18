2a1337e45c61affb6a5983d51bf078cecda9dac6
naumov <iamemptyhuh@gmail.com>
2026-09-18 12:51:17 +0200

    dbeaver/pro#10736 add multisource presentation

--- webapp/packages/plugin-connections/src/ConnectionForm/ConnectionFormService.ts
+++ webapp/packages/plugin-connections/src/ConnectionForm/ConnectionFormService.ts
@@ -5,12 +5,13 @@
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
  */
+import { importLazyComponent, PlaceholderContainer } from '@cloudbeaver/core-blocks';
 import { injectable } from '@cloudbeaver/core-di';
 import { NotificationService } from '@cloudbeaver/core-events';
 import { LocalizationService } from '@cloudbeaver/core-localization';
 import { FormBaseService, type IFormState } from '@cloudbeaver/core-ui';
+
 import type { IConnectionFormProps, IConnectionFormState } from './IConnectionFormState.js';
-import { importLazyComponent, PlaceholderContainer } from '@cloudbeaver/core-blocks';
 
 const ConnectionFormBaseActionsLoader = importLazyComponent(() => import('./ConnectionFormBaseActions.js').then(m => m.ConnectionFormBaseActions));
 
@@ -23,12 +24,14 @@ export class ConnectionFormService extends FormBaseService<IConnectionFormState,
   readonly mainPropertiesContainer: PlaceholderContainer<ConnectionFormContainerProps>;
   readonly providerPropertiesContainer: PlaceholderContainer<ConnectionFormContainerProps>;
   readonly connectionContainer: PlaceholderContainer<ConnectionFormContainerProps>;
+  readonly optionsPresentationContainer: PlaceholderContainer<ConnectionFormContainerProps>;
 
   constructor(localizationService: LocalizationService, notificationService: NotificationService) {
     super(localizationService, notificationService, 'Connection form');
     this.mainPropertiesContainer = new PlaceholderContainer<ConnectionFormContainerProps>();
     this.providerPropertiesContainer = new PlaceholderContainer<ConnectionFormContainerProps>();
     this.connectionContainer = new PlaceholderContainer<ConnectionFormContainerProps>();
+    this.optionsPresentationContainer = new PlaceholderContainer<ConnectionFormContainerProps>();
     this.actionsContainer.add(ConnectionFormBaseActionsLoader);
   }
 }
