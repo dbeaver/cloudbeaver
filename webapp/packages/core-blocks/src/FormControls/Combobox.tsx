7a66e3684235606d94ef555062635072bce1912d
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-09-22 17:10:07 +0200

    dbeaver/pro#10365 Revert previous custom combobox filtering
    This reverts commit 760f174c0685169566370a1a75732cd6764e9e9f.

--- webapp/packages/core-blocks/src/FormControls/Combobox.tsx
+++ webapp/packages/core-blocks/src/FormControls/Combobox.tsx
@@ -121,7 +121,8 @@ export const Combobox: IComboboxType = observer(function Combobox({
       const itemIcon = iconSelector?.(item);
       const itemDisabled = isDisabled?.(item);
 
-      const isVisible = inputValue === null || !inputValue.trim() || itemValue.toLowerCase().includes(inputValue.trim().toLowerCase());
+      const isVisible =
+        allowCustomValue || inputValue === null || !inputValue.trim() || itemValue.toLowerCase().includes(inputValue.trim().toLowerCase());
 
       return {
         item,
@@ -253,7 +254,7 @@ export const Combobox: IComboboxType = observer(function Combobox({
             </>
           )}
           {icon && <div className="tw:absolute tw:left-3 tw:w-4 tw:h-4">{typeof icon === 'string' ? <IconOrImage icon={icon} /> : icon}</div>}
-          {displayPopover && (!allowCustomValue || filteredItems.length > 0) && (
+          {displayPopover && (
             <ComboboxPopover className="theme-text-on-surface theme-background-surface">
               {filteredItems.length > 0 ? (
                 filteredItems.map(({ itemKey, itemValue, itemTitle, itemIcon, itemDisabled }) => (
